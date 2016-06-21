a23e687f91c1c8759646f4b8302543efed564b71
xichen2020 <xichen@uber.com>
2016-06-21 09:53:10 -0400

    test coverage for BaseBootstrapper

---
+++ bootstrap/bootstrapper/base_test.go
@@ -0,0 +1,252 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package bootstrapper
+
+import (
+	"testing"
+	"time"
+
+	"github.com/m3db/m3db/bootstrap"
+	"github.com/m3db/m3db/interfaces/m3db"
+	"github.com/m3db/m3db/mocks"
+	"github.com/m3db/m3db/storage"
+	xtime "github.com/m3db/m3db/x/time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	testTargetStart = time.Now()
+	testShard       = uint32(0)
+)
+
+type testBlockEntry struct {
+	id string
+	t  time.Time
+}
+
+func testDatabaseOptions() m3db.DatabaseOptions {
+	return storage.NewDatabaseOptions()
+}
+
+func testBaseBootstrapper(t *testing.T, ctrl *gomock.Controller) (*mocks.MockSource, *mocks.MockBootstrapper, *baseBootstrapper) {
+	source := mocks.NewMockSource(ctrl)
+	dbOpts := testDatabaseOptions()
+	next := mocks.NewMockBootstrapper(ctrl)
+	return source, next, NewBaseBootstrapper(source, dbOpts, next).(*baseBootstrapper)
+}
+
+func testTargetRanges() xtime.Ranges {
+	return xtime.NewRanges().AddRange(xtime.Range{Start: testTargetStart, End: testTargetStart.Add(2 * time.Hour)})
+}
+
+func testShardResult(entries ...testBlockEntry) m3db.ShardResult {
+	dbOpts := testDatabaseOptions()
+	res := bootstrap.NewShardResult(dbOpts)
+	for _, entry := range entries {
+		res.AddBlock(entry.id, storage.NewDatabaseBlock(entry.t, nil, dbOpts))
+	}
+	return res
+}
+
+func validateBlock(t *testing.T, expectedBlock, actualBlock m3db.DatabaseBlock) {
+	if expectedBlock == nil {
+		require.Nil(t, actualBlock)
+		return
+	}
+	require.Equal(t, expectedBlock.StartTime(), actualBlock.StartTime())
+}
+
+func validateSeries(t *testing.T, expectedSeries, actualSeries m3db.DatabaseSeriesBlocks) {
+	if expectedSeries == nil {
+		require.Nil(t, actualSeries)
+		return
+	}
+	eb := expectedSeries.GetAllBlocks()
+	ab := actualSeries.GetAllBlocks()
+	require.Equal(t, len(eb), len(ab))
+	for id, expectedBlock := range eb {
+		actualBlock, exists := ab[id]
+		require.True(t, exists)
+		validateBlock(t, expectedBlock, actualBlock)
+	}
+}
+
+func validateResult(t *testing.T, expectedResult, actualResult m3db.ShardResult) {
+	if expectedResult == nil {
+		require.Nil(t, actualResult)
+		return
+	}
+	es := expectedResult.GetAllSeries()
+	as := actualResult.GetAllSeries()
+	require.Equal(t, len(es), len(as))
+	for id, expectedSeries := range es {
+		actualSeries, exists := as[id]
+		require.True(t, exists)
+		validateSeries(t, expectedSeries, actualSeries)
+	}
+}
+
+func validateRanges(t *testing.T, expected, actual xtime.Ranges) {
+	require.Equal(t, expected.Len(), actual.Len())
+	eit := expected.Iter()
+	ait := actual.Iter()
+	for eit.Next() {
+		require.True(t, ait.Next())
+		require.Equal(t, eit.Value(), ait.Value())
+	}
+	require.False(t, ait.Next())
+}
+
+func TestBaseBootstrapperEmptyRange(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	_, _, base := testBaseBootstrapper(t, ctrl)
+
+	// Test non-nil empty range
+	targetRanges := xtime.NewRanges()
+	res, tr := base.Bootstrap(0, targetRanges)
+	require.Nil(t, res)
+	require.Nil(t, tr)
+
+	targetRanges = nil
+	res, tr = base.Bootstrap(0, targetRanges)
+	require.Nil(t, res)
+	require.Nil(t, tr)
+}
+
+func TestBaseBootstrapperCurrentNoUnfulfilled(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	source, next, base := testBaseBootstrapper(t, ctrl)
+
+	targetRanges := testTargetRanges()
+	remainingRanges := xtime.NewRanges()
+	curResult := testShardResult(testBlockEntry{"foo", testTargetStart})
+	curUnfulfilled := xtime.NewRanges()
+
+	source.EXPECT().GetAvailability(testShard, targetRanges).Return(targetRanges)
+	source.EXPECT().ReadData(testShard, targetRanges).Return(curResult, curUnfulfilled)
+	next.EXPECT().Bootstrap(testShard, remainingRanges).Return(nil, nil)
+
+	res, tr := base.Bootstrap(testShard, targetRanges)
+	validateResult(t, curResult, res)
+	validateRanges(t, curUnfulfilled, tr)
+}
+
+func TestBaseBootstrapperCurrentSomeUnfulfilled(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	source, next, base := testBaseBootstrapper(t, ctrl)
+
+	entries := []testBlockEntry{
+		{"foo", testTargetStart},
+		{"foo", testTargetStart.Add(time.Hour)},
+		{"bar", testTargetStart.Add(time.Hour)},
+	}
+	targetRanges := testTargetRanges()
+	remainingRanges := xtime.NewRanges()
+	curResult := testShardResult(entries[0])
+	curUnfulfilled := xtime.NewRanges().AddRange(xtime.Range{Start: testTargetStart, End: testTargetStart.Add(time.Hour)})
+	nextResult := testShardResult(entries[1:]...)
+
+	source.EXPECT().GetAvailability(testShard, targetRanges).Return(targetRanges)
+	source.EXPECT().ReadData(testShard, targetRanges).Return(curResult, curUnfulfilled)
+	next.EXPECT().Bootstrap(testShard, remainingRanges).Return(nil, nil)
+	next.EXPECT().Bootstrap(testShard, curUnfulfilled).Return(nextResult, nil)
+
+	res, tr := base.Bootstrap(testShard, targetRanges)
+	expectedRanges := xtime.NewRanges()
+	expectedResult := testShardResult(entries...)
+
+	validateResult(t, expectedResult, res)
+	validateRanges(t, expectedRanges, tr)
+}
+
+func testBasebootstrapperNext(t *testing.T, nextUnfulfilled xtime.Ranges) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	source, next, base := testBaseBootstrapper(t, ctrl)
+
+	targetRanges := testTargetRanges()
+	nextResult := testShardResult(testBlockEntry{"foo", testTargetStart})
+
+	source.EXPECT().GetAvailability(testShard, targetRanges).Return(nil)
+	source.EXPECT().ReadData(testShard, nil).Return(nil, nil)
+	next.EXPECT().Bootstrap(testShard, targetRanges).Return(nextResult, nextUnfulfilled)
+
+	res, tr := base.Bootstrap(testShard, targetRanges)
+	validateResult(t, nextResult, res)
+	validateRanges(t, nextUnfulfilled, tr)
+}
+
+func TestBaseBootstrapperNextNoUnfulfilled(t *testing.T) {
+	nextUnfulfilled := xtime.NewRanges()
+	testBasebootstrapperNext(t, nextUnfulfilled)
+}
+
+func TestBaseBootstrapperNextSomeUnfulfilled(t *testing.T) {
+	nextUnfulfilled := xtime.NewRanges().AddRange(xtime.Range{Start: testTargetStart, End: testTargetStart.Add(time.Hour)})
+	testBasebootstrapperNext(t, nextUnfulfilled)
+}
+
+func TestBaseBootstrapperBoth(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	source, next, base := testBaseBootstrapper(t, ctrl)
+
+	entries := []testBlockEntry{
+		{"foo", testTargetStart},
+		{"foo", testTargetStart.Add(time.Hour)},
+		{"bar", testTargetStart.Add(time.Hour)},
+		{"baz", testTargetStart},
+	}
+	ranges := []xtime.Range{
+		xtime.Range{Start: testTargetStart, End: testTargetStart.Add(time.Hour)},
+		xtime.Range{Start: testTargetStart.Add(time.Hour), End: testTargetStart.Add(2 * time.Hour)},
+		xtime.Range{Start: testTargetStart, End: testTargetStart.Add(30 * time.Minute)},
+		xtime.Range{Start: testTargetStart.Add(90 * time.Minute), End: testTargetStart.Add(100 * time.Minute)},
+		xtime.Range{Start: testTargetStart.Add(10 * time.Minute), End: testTargetStart.Add(20 * time.Minute)},
+	}
+	targetRanges := testTargetRanges()
+	availableRanges := xtime.NewRanges().AddRange(ranges[0])
+	remainingRanges := xtime.NewRanges().AddRange(ranges[1])
+
+	curResult := testShardResult(entries[0])
+	curUnfulfilled := xtime.NewRanges().AddRange(ranges[2])
+	nextResult := testShardResult(entries[1:3]...)
+	nextUnfulfilled := xtime.NewRanges().AddRange(ranges[3])
+	fallBackResult := testShardResult(entries[3])
+	fallBackUnfulfilled := xtime.NewRanges().AddRange(ranges[4])
+
+	source.EXPECT().GetAvailability(testShard, targetRanges).Return(availableRanges)
+	source.EXPECT().ReadData(testShard, availableRanges).Return(curResult, curUnfulfilled)
+	next.EXPECT().Bootstrap(testShard, remainingRanges).Return(nextResult, nextUnfulfilled)
+	next.EXPECT().Bootstrap(testShard, curUnfulfilled).Return(fallBackResult, fallBackUnfulfilled)
+
+	res, tr := base.Bootstrap(testShard, targetRanges)
+	expectedRanges := xtime.NewRanges().AddRange(ranges[3]).AddRange(ranges[4])
+	expectedResult := testShardResult(entries...)
+
+	validateResult(t, expectedResult, res)
+	validateRanges(t, expectedRanges, tr)
+}
