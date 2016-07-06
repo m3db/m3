5f33220b9ed66dcf5a8d4e67dff62ff9ea3f25cb
xichen2020 <xichen@uber.com>
2016-07-06 14:51:11 -0400

    Properly support encoding datapoints with different time units (#17)
    * Properly support encoding datapoints with different time units
    * Return timeUnitChanged instead of making it an internal field, parameterize the default time unit so we don't incur a changed time unit for the first data point in most cases

--- encoding/tsz/iterator.go
+++ encoding/tsz/iterator.go
@@ -48,8 +48,9 @@ type singleReaderIterator struct {
 	done bool          // has reached the end
 	err  error         // current error
 
-	ant m3db.Annotation // current annotation
-	tu  xtime.Unit      // current time unit
+	ant       m3db.Annotation // current annotation
+	tu        xtime.Unit      // current time unit
+	tuChanged bool            // whether we have a new time unit
 
 	closed bool
 }
@@ -70,6 +71,7 @@ func (it *singleReaderIterator) Next() bool {
 		return false
 	}
 	it.ant = nil
+	it.tuChanged = false
 	if it.t.IsZero() {
 		it.readFirstTimestamp()
 		it.readFirstValue()
@@ -77,13 +79,20 @@ func (it *singleReaderIterator) Next() bool {
 		it.readNextTimestamp()
 		it.readNextValue()
 	}
+	// NB(xichen): reset time delta to 0 when there is a time unit change to be
+	// consistent with the encoder.
+	if it.tuChanged {
+		it.dt = 0
+	}
 	return it.hasNext()
 }
 
 func (it *singleReaderIterator) readFirstTimestamp() {
 	nt := int64(it.readBits(64))
+	// NB(xichen): first time stamp is always normalized to nanoseconds.
+	st := xtime.FromNormalizedTime(nt, time.Nanosecond)
+	it.tu = initialTimeUnit(st, it.opts.GetDefaultTimeUnit())
 	it.readNextTimestamp()
-	st := xtime.FromNormalizedTime(nt, it.timeUnit())
 	it.t = st.Add(it.dt)
 }
 
@@ -93,12 +102,11 @@ func (it *singleReaderIterator) readFirstValue() {
 }
 
 func (it *singleReaderIterator) readNextTimestamp() {
-	dod := it.readMarkerOrDeltaOfDelta()
-	it.dt += xtime.FromNormalizedDuration(dod, it.timeUnit())
+	it.dt += it.readMarkerOrDeltaOfDelta()
 	it.t = it.t.Add(it.dt)
 }
 
-func (it *singleReaderIterator) tryReadMarker() (int64, bool) {
+func (it *singleReaderIterator) tryReadMarker() (time.Duration, bool) {
 	numBits := it.mes.NumOpcodeBits() + it.mes.NumValueBits()
 	opcodeAndValue, success := it.tryPeekBits(numBits)
 	if !success {
@@ -109,26 +117,26 @@ func (it *singleReaderIterator) tryReadMarker() (int64, bool) {
 		return 0, false
 	}
 	valueMask := (1 << uint(it.mes.NumValueBits())) - 1
-	value := int64(opcodeAndValue & uint64(valueMask))
-	switch Marker(value) {
+	markerValue := int64(opcodeAndValue & uint64(valueMask))
+	switch Marker(markerValue) {
 	case it.mes.EndOfStream():
 		it.readBits(numBits)
 		it.done = true
+		return 0, true
 	case it.mes.Annotation():
 		it.readBits(numBits)
 		it.readAnnotation()
-		value = it.readMarkerOrDeltaOfDelta()
+		return it.readMarkerOrDeltaOfDelta(), true
 	case it.mes.TimeUnit():
 		it.readBits(numBits)
 		it.readTimeUnit()
-		value = it.readMarkerOrDeltaOfDelta()
+		return it.readMarkerOrDeltaOfDelta(), true
 	default:
 		return 0, false
 	}
-	return value, true
 }
 
-func (it *singleReaderIterator) readMarkerOrDeltaOfDelta() int64 {
+func (it *singleReaderIterator) readMarkerOrDeltaOfDelta() time.Duration {
 	if dod, success := it.tryReadMarker(); success {
 		return dod
 	}
@@ -140,7 +148,13 @@ func (it *singleReaderIterator) readMarkerOrDeltaOfDelta() int64 {
 	return it.readDeltaOfDelta(tes)
 }
 
-func (it *singleReaderIterator) readDeltaOfDelta(tes TimeEncodingScheme) int64 {
+func (it *singleReaderIterator) readDeltaOfDelta(tes TimeEncodingScheme) (d time.Duration) {
+	if it.tuChanged {
+		// NB(xichen): if the time unit has changed, always read 64 bits as normalized
+		// dod in nanoseconds.
+		dod := signExtend(it.readBits(64), 64)
+		return time.Duration(dod)
+	}
 	cb := it.readBits(1)
 	if cb == tes.ZeroBucket().Opcode() {
 		return 0
@@ -149,11 +163,13 @@ func (it *singleReaderIterator) readDeltaOfDelta(tes TimeEncodingScheme) int64 {
 	for i := 0; i < len(buckets); i++ {
 		cb = (cb << 1) | it.readBits(1)
 		if cb == buckets[i].Opcode() {
-			return signExtend(it.readBits(buckets[i].NumValueBits()), buckets[i].NumValueBits())
+			dod := signExtend(it.readBits(buckets[i].NumValueBits()), buckets[i].NumValueBits())
+			return xtime.FromNormalizedDuration(dod, it.timeUnit())
 		}
 	}
 	numValueBits := tes.DefaultBucket().NumValueBits()
-	return signExtend(it.readBits(numValueBits), numValueBits)
+	dod := signExtend(it.readBits(numValueBits), numValueBits)
+	return xtime.FromNormalizedDuration(dod, it.timeUnit())
 }
 
 func (it *singleReaderIterator) readNextValue() {
@@ -180,7 +196,11 @@ func (it *singleReaderIterator) readAnnotation() {
 }
 
 func (it *singleReaderIterator) readTimeUnit() {
-	it.tu = xtime.Unit(it.readBits(8))
+	tu := xtime.Unit(it.readBits(8))
+	if tu.IsValid() && tu != it.tu {
+		it.tuChanged = true
+	}
+	it.tu = tu
 }
 
 func (it *singleReaderIterator) readXOR() uint64 {
