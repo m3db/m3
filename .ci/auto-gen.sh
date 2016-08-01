bb44fdd7d9c7103d270419f969fece4b50feff53
xichen2020 <xichen@uber.com>
2016-08-01 15:46:53 -0400

    link to open mockgen issue for more context

--- .ci/auto-gen.sh
+++ .ci/auto-gen.sh
@@ -5,10 +5,12 @@ cleanup() {
     cd $1
     for FILE in $(ls *.go);
     do
-        # NB(xichen): clean up vendored imports. Note that sed -i'' does not
-        # work with BSD sed shipped with OS X, whereas sed -i '' doesn't work
-        # with GNU sed, so work around it by redirecting to a temp file first
-        # and moving it back later.
+        # NB(xichen): there is an open issue (https://github.com/golang/mock/issues/30)
+        # with mockgen that causes the generated mock files to have vendored packages
+        # in the import list. For now we are working around it by removing the vendored
+        # path. Also sed -i'' does not work with BSD sed shipped with OS X, whereas
+        # sed -i '' doesn't work with GNU sed, so we work around it by redirecting to a
+        # temp file first and moving it back later.
         sed "s|$VENDOR_PATH/||" $FILE > $FILE.tmp && mv $FILE.tmp $FILE
         
         # Add uber license
