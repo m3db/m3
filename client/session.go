c1fa54900ceed53b3422d554d62bc23c25eb1006
Ben Lerner <bl@uber.com>
2016-11-30 19:20:50 -0500

    break out metrics

--- client/session.go
+++ client/session.go
@@ -111,37 +111,6 @@ type session struct {
 	metrics                          sessionMetrics
 }
 
-type sessionMetrics struct {
-	sync.RWMutex
-	writeSuccess               tally.Counter
-	writeErrors                tally.Counter
-	writeNodesRespondingErrors []tally.Counter
-	fetchSuccess               tally.Counter
-	fetchErrors                tally.Counter
-	fetchNodesRespondingErrors []tally.Counter
-	streamFromPeersMetrics     map[uint32]streamFromPeersMetrics
-}
-
-func newSessionMetrics(scope tally.Scope) sessionMetrics {
-	return sessionMetrics{
-		writeSuccess:           scope.Counter("write.success"),
-		writeErrors:            scope.Counter("write.errors"),
-		fetchSuccess:           scope.Counter("fetch.success"),
-		fetchErrors:            scope.Counter("fetch.errors"),
-		streamFromPeersMetrics: make(map[uint32]streamFromPeersMetrics),
-	}
-}
-
-type streamFromPeersMetrics struct {
-	fetchBlocksFromPeers      tally.Gauge
-	metadataFetches           tally.Gauge
-	metadataFetchBatchCall    tally.Counter
-	metadataFetchBatchSuccess tally.Counter
-	metadataFetchBatchError   tally.Counter
-	metadataReceived          tally.Counter
-	blocksEnqueueChannel      tally.Gauge
-}
-
 type newHostQueueFn func(
 	host topology.Host,
 	writeBatchRawRequestPool writeBatchRawRequestPool,
@@ -212,74 +181,6 @@ func (s *session) ShardID(id string) (uint32, error) {
 	value := s.topoMap.ShardSet().Lookup(ts.StringID(id))
 	s.RUnlock()
 	return value, nil
-}
-
-func (s *session) streamFromPeersMetricsForShard(shard uint32) *streamFromPeersMetrics {
-	s.metrics.RLock()
-	m, ok := s.metrics.streamFromPeersMetrics[shard]
-	s.metrics.RUnlock()
-
-	if ok {
-		return &m
-	}
-
-	s.metrics.Lock()
-	m, ok = s.metrics.streamFromPeersMetrics[shard]
-	if ok {
-		s.metrics.Unlock()
-		return &m
-	}
-
-	scope := s.opts.InstrumentOptions().MetricsScope().
-		SubScope("stream-from-peers").Tagged(map[string]string{
-		"shard": fmt.Sprintf("%d", shard),
-	})
-	m = streamFromPeersMetrics{
-		fetchBlocksFromPeers:      scope.Gauge("fetch-blocks-inprogress"),
-		metadataFetches:           scope.Gauge("fetch-metadata-peers-inprogress"),
-		metadataFetchBatchCall:    scope.Counter("fetch-metadata-peers-batch-call"),
-		metadataFetchBatchSuccess: scope.Counter("fetch-metadata-peers-batch-success"),
-		metadataFetchBatchError:   scope.Counter("fetch-metadata-peers-batch-error"),
-		metadataReceived:          scope.Counter("fetch-metadata-peers-received"),
-		blocksEnqueueChannel:      scope.Gauge("fetch-blocks-enqueue-channel-length"),
-	}
-	s.metrics.streamFromPeersMetrics[shard] = m
-	s.metrics.Unlock()
-	return &m
-}
-
-func (s *session) incWriteMetrics(consistencyResultErr error, respErrs int32) {
-	if idx := s.nodesRespondingErrorsMetricIndex(respErrs); idx >= 0 {
-		s.metrics.writeNodesRespondingErrors[idx].Inc(1)
-	}
-	if consistencyResultErr == nil {
-		s.metrics.writeSuccess.Inc(1)
-	} else {
-		s.metrics.writeErrors.Inc(1)
-	}
-}
-
-func (s *session) incFetchMetrics(consistencyResultErr error, respErrs int32) {
-	if idx := s.nodesRespondingErrorsMetricIndex(respErrs); idx >= 0 {
-		s.metrics.fetchNodesRespondingErrors[idx].Inc(1)
-	}
-	if consistencyResultErr == nil {
-		s.metrics.fetchSuccess.Inc(1)
-	} else {
-		s.metrics.fetchErrors.Inc(1)
-	}
-}
-
-func (s *session) nodesRespondingErrorsMetricIndex(respErrs int32) int32 {
-	idx := respErrs - 1
-	replicas := int32(s.Replicas())
-	if respErrs > replicas {
-		// Cap to the max replicas, we might get more errors
-		// when a node is initializing a shard causing replicas + 1
-		// nodes to respond to operations
-		idx = replicas - 1
-	}
-	return idx
 }
 
 func (s *session) Open() error {
