e029df6520c95532bc299377c51fcae155bf915b
Xi Chen <xichen@uber.com>
2016-12-20 15:58:29 -0500

    Add Markdown file to describe wire format for encoded metrics

--- src/metrics/protocol/msgpack/schema.go
+++ src/metrics/protocol/msgpack/schema.go
@@ -18,28 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-/*
-
-The message-pack based encoder uses the following schema for encoding objects:
-
-current encoder version
-encoded object
-
-When encoding an object, the encoder first writes the number of fields to be
-encoded for the object, and then writes each object field. If an object field
-is also an object, the encoder encodes that field recursively.
-
-Backward-compatible changes (e.g., adding an additional field to the end of
-the object) can be deployed or rolled back separately on the client-side and
-the server-side. It is unnecessary to increase the version for
-backward-compatible changes.
-
-Backward-incompatible changes (e.g., removing a field or changing a field type)
-must be deployed to the server-side first then to the client-side. It is REQUIRED
-to increase the version for backward-incompatible changes.
-
-*/
-
 package msgpack
 
 type objectType int
