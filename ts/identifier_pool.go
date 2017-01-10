1ed36fc16ec04d29fa161058b44eca78cc22a760
Rob Skillington <r@uber.com>
2017-01-09 19:41:45 -0500

    Use a simple objectpool to retain reference to the checked.Bytes for identifiers

--- ts/identifier_pool.go
+++ ts/identifier_pool.go
@@ -21,10 +21,6 @@
 package ts
 
 import (
-	"reflect"
-	"sync"
-	"unsafe"
-
 	"github.com/m3db/m3db/context"
 	"github.com/m3db/m3x/checked"
 	"github.com/m3db/m3x/pool"
@@ -82,36 +78,23 @@ func (p *simpleIdentifierPool) Clone(existing ID) ID {
 // NewNativeIdentifierPool constructs a new NativeIdentifierPool.
 func NewNativeIdentifierPool(
 	heap pool.CheckedBytesPool,
-	options pool.ObjectPoolOptions,
+	opts pool.ObjectPoolOptions,
 ) IdentifierPool {
-
-	if options == nil {
-		options = pool.NewObjectPoolOptions()
+	if opts == nil {
+		opts = pool.NewObjectPoolOptions()
 	}
 
-	size := options.Size()
-
+	iopts := opts.InstrumentOptions()
 	return &nativeIdentifierPool{
-		pool: pool.NewNativePool(pool.NativePoolOptions{
-			Type: reflect.TypeOf(id{}),
-			Size: uint(size),
-		}),
+		pool: pool.NewObjectPool(opts.SetInstrumentOptions(
+			iopts.SetMetricsScope(iopts.MetricsScope().SubScope("id-pool")))),
 		heap: configureHeap(heap),
-		refs: checkedBytesRefs{
-			idRefsToBytesRefs: make(map[uintptr]unsafe.Pointer, size),
-		},
 	}
 }
 
 type nativeIdentifierPool struct {
-	pool pool.NativePool
+	pool pool.ObjectPool
 	heap pool.CheckedBytesPool
-	refs checkedBytesRefs
-}
-
-type checkedBytesRefs struct {
-	sync.Mutex
-	idRefsToBytesRefs map[uintptr]unsafe.Pointer
 }
 
 func configureHeap(heap pool.CheckedBytesPool) pool.CheckedBytesPool {
@@ -138,7 +121,7 @@ func create() interface{} {
 
 // GetBinaryID returns a new ID based on a binary value.
 func (p *nativeIdentifierPool) GetBinaryID(ctx context.Context, v checked.Bytes) ID {
-	id := p.pool.GetOr(create).(*id)
+	id := p.pool.Get().(*id)
 	ctx.RegisterFinalizer(id)
 
 	v.IncRef()
@@ -149,10 +132,10 @@ func (p *nativeIdentifierPool) GetBinaryID(ctx context.Context, v checked.Bytes)
 
 // GetStringID returns a new ID based on a string value.
 func (p *nativeIdentifierPool) GetStringID(ctx context.Context, str string) ID {
-	id := p.pool.GetOr(create).(*id)
+	id := p.pool.Get().(*id)
 	ctx.RegisterFinalizer(id)
 
-	v := p.heapRetrieve(id, len(str))
+	v := p.heap.Get(len(str))
 	v.IncRef()
 	v.AppendAll([]byte(str))
 
@@ -163,19 +146,17 @@ func (p *nativeIdentifierPool) GetStringID(ctx context.Context, str string) ID {
 
 func (p *nativeIdentifierPool) Put(v ID) {
 	v.Reset()
-	id := v.(*id)
-	p.heapRelease(id)
-	p.pool.Put(id)
+	p.pool.Put(v.(*id))
 }
 
 // Clone replicates given ID into a new ID from the pool.
 func (p *nativeIdentifierPool) Clone(existing ID) ID {
-	id := p.pool.GetOr(create).(*id)
+	id := p.pool.Get().(*id)
 
 	data := existing.Data()
 	data.IncRef()
 
-	v := p.heapRetrieve(id, data.Len())
+	v := p.heap.Get(data.Len())
 	v.IncRef()
 	v.AppendAll(data.Get())
 
@@ -184,25 +165,4 @@ func (p *nativeIdentifierPool) Clone(existing ID) ID {
 	id.pool, id.data = p, v
 
 	return id
-}
-
-// heapRetrieve is a helper that retrieves some bytes and keeps a reference
-// to it considering all bytes retrieved from the heap are permanent.
-func (p *nativeIdentifierPool) heapRetrieve(v *id, length int) checked.Bytes {
-	bytes := p.heap.Get(length)
-	key := uintptr(unsafe.Pointer(&v))
-	value := unsafe.Pointer(&bytes)
-	p.refs.Lock()
-	p.refs.idRefsToBytesRefs[key] = value
-	p.refs.Unlock()
-	return bytes
-}
-
-// heapRelease will release any reference so some checked out heap bytes
-// the pool might have.
-func (p *nativeIdentifierPool) heapRelease(v *id) {
-	key := uintptr(unsafe.Pointer(&v))
-	p.refs.Lock()
-	delete(p.refs.idRefsToBytesRefs, key)
-	p.refs.Unlock()
 }
