3b40e39fc6c25cea4de4443b2b9cea5fad529baa
cw9 <chaow929@gmail.com>
2017-01-25 18:10:11 -0500

    Fix flaky test (#73)

--- src/cluster/services/client/services_test.go
+++ src/cluster/services/client/services_test.go
@@ -325,6 +325,9 @@ func TestWatchIncludeUnhealthy(t *testing.T) {
 	require.Error(t, err)
 	require.Equal(t, errWatchInitTimeout, err)
 
+	sd, err = NewServices(opts.SetInitTimeout(defaultInitTimeout))
+	require.NoError(t, err)
+
 	p := placement.NewPlacement().
 		SetInstances([]services.PlacementInstance{
 			placement.NewInstance().
@@ -422,6 +425,9 @@ func TestWatchNotIncludeUnhealthy(t *testing.T) {
 	_, err = sd.Watch(sid, qopts)
 	require.Error(t, err)
 	require.Equal(t, errWatchInitTimeout, err)
+
+	sd, err = NewServices(opts.SetInitTimeout(defaultInitTimeout))
+	require.NoError(t, err)
 
 	p := placement.NewPlacement().
 		SetInstances([]services.PlacementInstance{
