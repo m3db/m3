60aeaa34e279c67ddbe27fd89e27c00784cb827e
xichen2020 <xichen@uber.com>
2017-02-09 18:34:09 -0500

    Add more detailed error messages for peer streaming (#253)
    * Add more detailed error messages for peer streaming
    
    * Address feedback

--- client/session_fetch_bulk_blocks_test.go
+++ client/session_fetch_bulk_blocks_test.go
@@ -1056,9 +1056,15 @@ func TestStreamBlocksBatchFromPeerReenqueuesOnFailCall(t *testing.T) {
 	s, err := newSession(opts)
 	assert.NoError(t, err)
 	session := s.(*session)
-	session.reattemptStreamBlocksFromPeersFn = func(blocks []blockMetadata, enqueueCh *enqueueChannel, _ reason, _ *streamFromPeersMetrics) {
+	session.reattemptStreamBlocksFromPeersFn = func(
+		blocks []blockMetadata,
+		enqueueCh *enqueueChannel,
+		attemptErr error,
+		_ reason,
+		_ *streamFromPeersMetrics,
+	) {
 		enqueue := enqueueCh.enqueueDelayed(len(blocks))
-		session.streamBlocksReattemptFromPeersEnqueue(blocks, enqueue)
+		session.streamBlocksReattemptFromPeersEnqueue(blocks, attemptErr, enqueue)
 	}
 
 	mockHostQueues, mockClients := mockHostQueuesAndClientsForFetchBootstrapBlocks(ctrl, opts)
@@ -1130,9 +1136,15 @@ func TestStreamBlocksBatchFromPeerVerifiesBlockErr(t *testing.T) {
 	s, err := newSession(opts)
 	assert.NoError(t, err)
 	session := s.(*session)
-	session.reattemptStreamBlocksFromPeersFn = func(blocks []blockMetadata, enqueueCh *enqueueChannel, _ reason, _ *streamFromPeersMetrics) {
+	session.reattemptStreamBlocksFromPeersFn = func(
+		blocks []blockMetadata,
+		enqueueCh *enqueueChannel,
+		attemptErr error,
+		_ reason,
+		_ *streamFromPeersMetrics,
+	) {
 		enqueue := enqueueCh.enqueueDelayed(len(blocks))
-		session.streamBlocksReattemptFromPeersEnqueue(blocks, enqueue)
+		session.streamBlocksReattemptFromPeersEnqueue(blocks, attemptErr, enqueue)
 	}
 
 	mockHostQueues, mockClients := mockHostQueuesAndClientsForFetchBootstrapBlocks(ctrl, opts)
@@ -1254,9 +1266,15 @@ func TestStreamBlocksBatchFromPeerVerifiesBlockChecksum(t *testing.T) {
 	s, err := newSession(opts)
 	assert.NoError(t, err)
 	session := s.(*session)
-	session.reattemptStreamBlocksFromPeersFn = func(blocks []blockMetadata, enqueueCh *enqueueChannel, _ reason, _ *streamFromPeersMetrics) {
+	session.reattemptStreamBlocksFromPeersFn = func(
+		blocks []blockMetadata,
+		enqueueCh *enqueueChannel,
+		attemptErr error,
+		_ reason,
+		_ *streamFromPeersMetrics,
+	) {
 		enqueue := enqueueCh.enqueueDelayed(len(blocks))
-		session.streamBlocksReattemptFromPeersEnqueue(blocks, enqueue)
+		session.streamBlocksReattemptFromPeersEnqueue(blocks, attemptErr, enqueue)
 	}
 
 	mockHostQueues, mockClients := mockHostQueuesAndClientsForFetchBootstrapBlocks(ctrl, opts)
