0b26ce183a008b18d05516f0d9d2bd7e0fe01e29
Rob Skillington <r@uber.com>
2017-05-01 10:57:05 -0400

    Use less aggressive health check intervals

--- client/options.go
+++ client/options.go
@@ -99,13 +99,13 @@ const (
 	defaultBackgroundConnectInterval = 4 * time.Second
 
 	// defaultBackgroundConnectStutter is the default background connect stutter
-	defaultBackgroundConnectStutter = 1 * time.Second
+	defaultBackgroundConnectStutter = 2 * time.Second
 
 	// defaultBackgroundHealthCheckInterval is the default background health check interval
-	defaultBackgroundHealthCheckInterval = 1 * time.Second
+	defaultBackgroundHealthCheckInterval = 4 * time.Second
 
 	// defaultBackgroundHealthCheckStutter is the default background health check stutter
-	defaultBackgroundHealthCheckStutter = 1 * time.Second
+	defaultBackgroundHealthCheckStutter = 2 * time.Second
 
 	// defaultBackgroundHealthCheckFailLimit is the default background health failure
 	// limit before connection is deemed unhealth
