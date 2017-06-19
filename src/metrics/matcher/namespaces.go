4709fef9dd47006e2475c89a9572752bbe3d0252
xichen2020 <xichen@uber.com>
2017-06-19 11:47:48 -0400

    Rule matcher API (#57)
    * Rule matcher API
    
    * Add metrics
    
    * Cleaner APIs
    
    * Address feedback

---
+++ src/metrics/matcher/namespaces.go
@@ -0,0 +1,260 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package matcher
+
+import (
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/m3db/m3cluster/kv"
+	"github.com/m3db/m3cluster/kv/util/runtime"
+	"github.com/m3db/m3metrics/generated/proto/schema"
+	"github.com/m3db/m3metrics/rules"
+	"github.com/m3db/m3x/clock"
+	"github.com/m3db/m3x/id"
+	"github.com/m3db/m3x/log"
+
+	"github.com/uber-go/tally"
+)
+
+var (
+	emptyNamespaces rules.Namespaces
+	errNilValue     = errors.New("nil value received")
+)
+
+// Namespaces manages runtime updates to registered namespaces and provides
+// API to match metic ids against rules in the corresponding namespaces.
+type Namespaces interface {
+	runtime.Value
+
+	// Version returns the current version for a give namespace.
+	Version(namespace []byte) int
+
+	// Match returns the matching policies for a given id in a given namespace
+	// between [fromNanos, toNanos).
+	Match(namespace, id []byte, fromNanos, toNanos int64) rules.MatchResult
+
+	// Close closes the namespaces.
+	Close()
+}
+
+type namespacesMetrics struct {
+	notExists   tally.Counter
+	added       tally.Counter
+	removed     tally.Counter
+	watched     tally.Counter
+	watchErrors tally.Counter
+	unwatched   tally.Counter
+}
+
+func newNamespacesMetrics(scope tally.Scope) namespacesMetrics {
+	return namespacesMetrics{
+		notExists:   scope.Counter("not-exists"),
+		added:       scope.Counter("added"),
+		removed:     scope.Counter("removed"),
+		watched:     scope.Counter("watched"),
+		watchErrors: scope.Counter("watch-errors"),
+		unwatched:   scope.Counter("unwatched"),
+	}
+}
+
+// namespaces contains the list of namespace users have defined rules for.
+type namespaces struct {
+	sync.RWMutex
+	runtime.Value
+
+	key                  string
+	store                kv.Store
+	opts                 Options
+	nowFn                clock.NowFn
+	log                  xlog.Logger
+	ruleSetKeyFn         RuleSetKeyFn
+	matchRangePast       time.Duration
+	onNamespaceAddedFn   OnNamespaceAddedFn
+	onNamespaceRemovedFn OnNamespaceRemovedFn
+
+	proto   *schema.Namespaces
+	rules   map[xid.Hash]RuleSet
+	metrics namespacesMetrics
+}
+
+// NewNamespaces creates a new namespaces object.
+func NewNamespaces(key string, opts Options) Namespaces {
+	instrumentOpts := opts.InstrumentOptions()
+	n := &namespaces{
+		key:                  key,
+		store:                opts.KVStore(),
+		opts:                 opts,
+		nowFn:                opts.ClockOptions().NowFn(),
+		log:                  instrumentOpts.Logger(),
+		ruleSetKeyFn:         opts.RuleSetKeyFn(),
+		matchRangePast:       opts.MatchRangePast(),
+		onNamespaceAddedFn:   opts.OnNamespaceAddedFn(),
+		onNamespaceRemovedFn: opts.OnNamespaceRemovedFn(),
+		proto:                &schema.Namespaces{},
+		rules:                make(map[xid.Hash]RuleSet),
+		metrics:              newNamespacesMetrics(instrumentOpts.MetricsScope()),
+	}
+	valueOpts := runtime.NewOptions().
+		SetInstrumentOptions(instrumentOpts).
+		SetInitWatchTimeout(opts.InitWatchTimeout()).
+		SetKVStore(n.store).
+		SetUnmarshalFn(n.toNamespaces).
+		SetProcessFn(n.process)
+	n.Value = runtime.NewValue(key, valueOpts)
+	return n
+}
+
+func (n *namespaces) Version(namespace []byte) int {
+	nsHash := xid.HashFn(namespace)
+	n.RLock()
+	ruleSet, exists := n.rules[nsHash]
+	n.RUnlock()
+	if !exists {
+		return kv.UninitializedVersion
+	}
+	return ruleSet.Version()
+}
+
+func (n *namespaces) Match(namespace, id []byte, fromNanos, toNanos int64) rules.MatchResult {
+	var (
+		res    = rules.EmptyMatchResult
+		nsHash = xid.HashFn(namespace)
+	)
+	n.RLock()
+	ruleSet, exists := n.rules[nsHash]
+	n.RUnlock()
+	if !exists {
+		n.metrics.notExists.Inc(1)
+		return res
+	}
+	return ruleSet.Match(id, fromNanos, toNanos)
+}
+
+func (n *namespaces) Close() {
+	// NB(xichen): we stop watching the value outside lock because otherwise we might
+	// be holding the namespace lock while attempting to acquire the value lock, and
+	// the updating goroutine might be holding the value lock and attempting to
+	// acquire the namespace lock, causing a deadlock.
+	n.Value.Unwatch()
+
+	n.RLock()
+	for _, rs := range n.rules {
+		rs.Unwatch()
+	}
+	n.RUnlock()
+}
+
+func (n *namespaces) toNamespaces(value kv.Value) (interface{}, error) {
+	n.Lock()
+	defer n.Unlock()
+
+	if value == nil {
+		return emptyNamespaces, errNilValue
+	}
+	n.proto.Reset()
+	if err := value.Unmarshal(n.proto); err != nil {
+		return emptyNamespaces, err
+	}
+	return rules.NewNamespaces(value.Version(), n.proto)
+}
+
+func (n *namespaces) process(value interface{}) error {
+	var (
+		nss        = value.(rules.Namespaces)
+		version    = nss.Version()
+		namespaces = nss.Namespaces()
+		incoming   = make(map[xid.Hash]rules.Namespace, len(namespaces))
+	)
+	for _, ns := range namespaces {
+		incoming[xid.HashFn(ns.Name())] = ns
+	}
+
+	n.Lock()
+	defer n.Unlock()
+
+	for nsHash, ns := range incoming {
+		nsName, snapshots := ns.Name(), ns.Snapshots()
+		ruleSet, exists := n.rules[nsHash]
+		if !exists {
+			instrumentOpts := n.opts.InstrumentOptions()
+			ruleSetScope := instrumentOpts.MetricsScope().SubScope("ruleset")
+			ruleSetOpts := n.opts.SetInstrumentOptions(instrumentOpts.SetMetricsScope(ruleSetScope))
+			ruleSetKey := n.ruleSetKeyFn(ns.Name())
+			ruleSet = newRuleSet(nsName, ruleSetKey, ruleSetOpts)
+			n.rules[nsHash] = ruleSet
+			n.metrics.added.Inc(1)
+		}
+
+		shouldWatch := true
+		// This should never happen but just to be on the defensive side.
+		if len(snapshots) == 0 {
+			n.log.WithFields(
+				xlog.NewLogField("version", version),
+			).Warn("namespace updates have no snapshots")
+		} else {
+			latestSnapshot := snapshots[len(snapshots)-1]
+			// If the latest update shows the namespace is tombstoned, and we
+			// have received the corresponding ruleset update, we can stop watching
+			// the ruleset updates.
+			if latestSnapshot.Tombstoned() && latestSnapshot.ForRuleSetVersion() == ruleSet.Version() {
+				shouldWatch = false
+			}
+		}
+		if !shouldWatch {
+			n.metrics.unwatched.Inc(1)
+			ruleSet.Unwatch()
+		} else {
+			n.metrics.watched.Inc(1)
+			if err := ruleSet.Watch(); err != nil {
+				n.metrics.watchErrors.Inc(1)
+				n.log.WithFields(
+					xlog.NewLogField("ruleSetKey", ruleSet.Key()),
+					xlog.NewLogErrField(err),
+				).Error("failed to watch ruleset updates")
+			}
+		}
+
+		if !exists && n.onNamespaceAddedFn != nil {
+			n.onNamespaceAddedFn(nsName, ruleSet)
+		}
+	}
+
+	for nsHash, ruleSet := range n.rules {
+		_, exists := incoming[nsHash]
+		if exists {
+			continue
+		}
+		// Process the namespaces not in the incoming update.
+		earliest := n.nowFn().Add(-n.matchRangePast)
+		if ruleSet.Tombstoned() && ruleSet.CutoverNanos() <= earliest.UnixNano() {
+			if n.onNamespaceRemovedFn != nil {
+				n.onNamespaceRemovedFn(ruleSet.Namespace())
+			}
+			delete(n.rules, nsHash)
+			ruleSet.Unwatch()
+			n.metrics.unwatched.Inc(1)
+		}
+	}
+
+	return nil
+}
---
+++ src/metrics/matcher/options.go
@@ -0,0 +1,305 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package matcher
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/m3db/m3cluster/kv"
+	"github.com/m3db/m3cluster/kv/mem"
+	"github.com/m3db/m3metrics/rules"
+	"github.com/m3db/m3x/clock"
+	"github.com/m3db/m3x/instrument"
+)
+
+const (
+	defaultInitWatchTimeout = 10 * time.Second
+	defaultValueRetryPeriod = 10 * time.Second
+	defaultValueRetryExpiry = 3 * time.Hour
+	defaultNamespacesKey    = "/namespaces"
+	defaultRuleSetKeyFormat = "/ruleset/%s"
+	defaultMatchRangePast   = 0
+)
+
+var (
+	defaultNamespaceTag     = []byte("namespace")
+	defaultDefaultNamespace = []byte("defaultNamespace")
+)
+
+// RuleSetKeyFn generates the ruleset key for a given namespace.
+type RuleSetKeyFn func(namespace []byte) string
+
+// OnNamespaceAddedFn is called when a namespace is added.
+type OnNamespaceAddedFn func(namespace []byte, ruleSet RuleSet)
+
+// OnNamespaceRemovedFn is called when a namespace is removed.
+type OnNamespaceRemovedFn func(namespace []byte)
+
+// OnRuleSetUpdatedFn is called when a ruleset is updated.
+type OnRuleSetUpdatedFn func(namespace []byte, ruleSet RuleSet)
+
+// Options provide a set of options for the msgpack-based reporter.
+type Options interface {
+	// SetClockOptions sets the clock options.
+	SetClockOptions(value clock.Options) Options
+
+	// ClockOptions returns the clock options.
+	ClockOptions() clock.Options
+
+	// SetInstrumentOptions sets the instrument options.
+	SetInstrumentOptions(value instrument.Options) Options
+
+	// InstrumentOptions returns the instrument options.
+	InstrumentOptions() instrument.Options
+
+	// SetRuleSetOptions sets the ruleset options.
+	SetRuleSetOptions(value rules.Options) Options
+
+	// RuleSetOptions returns the ruleset options.
+	RuleSetOptions() rules.Options
+
+	// SetInitWatchTimeout sets the initial watch timeout.
+	SetInitWatchTimeout(value time.Duration) Options
+
+	// InitWatchTimeout returns the initial watch timeout.
+	InitWatchTimeout() time.Duration
+
+	// SetKVStore sets the kv store.
+	SetKVStore(value kv.Store) Options
+
+	// KVStore returns the kv store.
+	KVStore() kv.Store
+
+	// SetNamespacesKey sets the key for the full list of namespaces.
+	SetNamespacesKey(value string) Options
+
+	// NamespacesKey returns the key for the full list of namespaces.
+	NamespacesKey() string
+
+	// SetRuleSetKeyFn sets the function to generate ruleset keys.
+	SetRuleSetKeyFn(value RuleSetKeyFn) Options
+
+	// RuleSetKeyFn returns the function to generate ruleset keys.
+	RuleSetKeyFn() RuleSetKeyFn
+
+	// SetNamespaceTag sets the namespace tag.
+	SetNamespaceTag(value []byte) Options
+
+	// NamespaceTag returns the namespace tag.
+	NamespaceTag() []byte
+
+	// SetDefaultNamespace sets the default namespace for ids without a namespace.
+	SetDefaultNamespace(value []byte) Options
+
+	// DefaultNamespace returns the default namespace for ids without a namespace.
+	DefaultNamespace() []byte
+
+	// SetMatchRangePast sets the limit on the earliest time eligible for rule matching.
+	SetMatchRangePast(value time.Duration) Options
+
+	// MatchRangePast returns the limit on the earliest time eligible for rule matching.
+	MatchRangePast() time.Duration
+
+	// SetOnNamespaceAddedFn sets the function to be called when a namespace is added.
+	SetOnNamespaceAddedFn(value OnNamespaceAddedFn) Options
+
+	// OnNamespaceAddedFn returns the function to be called when a namespace is added.
+	OnNamespaceAddedFn() OnNamespaceAddedFn
+
+	// SetOnNamespaceRemovedFn sets the function to be called when a namespace is removed.
+	SetOnNamespaceRemovedFn(value OnNamespaceRemovedFn) Options
+
+	// OnNamespaceRemovedFn returns the function to be called when a namespace is removed.
+	OnNamespaceRemovedFn() OnNamespaceRemovedFn
+
+	// SetOnRuleSetUpdatedFn sets the function to be called when a ruleset is updated.
+	SetOnRuleSetUpdatedFn(value OnRuleSetUpdatedFn) Options
+
+	// OnRuleSetUpdatedFn returns the function to be called when a ruleset is updated.
+	OnRuleSetUpdatedFn() OnRuleSetUpdatedFn
+}
+
+type options struct {
+	clockOpts            clock.Options
+	instrumentOpts       instrument.Options
+	ruleSetOpts          rules.Options
+	initWatchTimeout     time.Duration
+	kvStore              kv.Store
+	namespacesKey        string
+	ruleSetKeyFn         RuleSetKeyFn
+	namespaceTag         []byte
+	defaultNamespace     []byte
+	matchRangePast       time.Duration
+	onNamespaceAddedFn   OnNamespaceAddedFn
+	onNamespaceRemovedFn OnNamespaceRemovedFn
+	onRuleSetUpdatedFn   OnRuleSetUpdatedFn
+}
+
+// NewOptions creates a new set of options.
+func NewOptions() Options {
+	return &options{
+		clockOpts:        clock.NewOptions(),
+		instrumentOpts:   instrument.NewOptions(),
+		ruleSetOpts:      rules.NewOptions(),
+		initWatchTimeout: defaultInitWatchTimeout,
+		kvStore:          mem.NewStore(),
+		namespacesKey:    defaultNamespacesKey,
+		ruleSetKeyFn:     defaultRuleSetKeyFn,
+		namespaceTag:     defaultNamespaceTag,
+		defaultNamespace: defaultDefaultNamespace,
+		matchRangePast:   defaultMatchRangePast,
+	}
+}
+
+func (o *options) SetClockOptions(value clock.Options) Options {
+	opts := *o
+	opts.clockOpts = value
+	return &opts
+}
+
+func (o *options) ClockOptions() clock.Options {
+	return o.clockOpts
+}
+
+func (o *options) SetInstrumentOptions(value instrument.Options) Options {
+	opts := *o
+	opts.instrumentOpts = value
+	return &opts
+}
+
+func (o *options) InstrumentOptions() instrument.Options {
+	return o.instrumentOpts
+}
+
+func (o *options) SetRuleSetOptions(value rules.Options) Options {
+	opts := *o
+	opts.ruleSetOpts = value
+	return &opts
+}
+
+func (o *options) RuleSetOptions() rules.Options {
+	return o.ruleSetOpts
+}
+
+func (o *options) SetInitWatchTimeout(value time.Duration) Options {
+	opts := *o
+	opts.initWatchTimeout = value
+	return &opts
+}
+
+func (o *options) InitWatchTimeout() time.Duration {
+	return o.initWatchTimeout
+}
+
+func (o *options) SetKVStore(value kv.Store) Options {
+	opts := *o
+	opts.kvStore = value
+	return &opts
+}
+
+func (o *options) KVStore() kv.Store {
+	return o.kvStore
+}
+
+func (o *options) SetNamespacesKey(value string) Options {
+	opts := *o
+	opts.namespacesKey = value
+	return &opts
+}
+
+func (o *options) NamespacesKey() string {
+	return o.namespacesKey
+}
+
+func (o *options) SetRuleSetKeyFn(value RuleSetKeyFn) Options {
+	opts := *o
+	opts.ruleSetKeyFn = value
+	return &opts
+}
+
+func (o *options) RuleSetKeyFn() RuleSetKeyFn {
+	return o.ruleSetKeyFn
+}
+
+func (o *options) SetNamespaceTag(value []byte) Options {
+	opts := *o
+	opts.namespaceTag = value
+	return &opts
+}
+
+func (o *options) NamespaceTag() []byte {
+	return o.namespaceTag
+}
+
+func (o *options) SetDefaultNamespace(value []byte) Options {
+	opts := *o
+	opts.defaultNamespace = value
+	return &opts
+}
+
+func (o *options) DefaultNamespace() []byte {
+	return o.defaultNamespace
+}
+
+func (o *options) SetMatchRangePast(value time.Duration) Options {
+	opts := *o
+	opts.matchRangePast = value
+	return &opts
+}
+
+func (o *options) MatchRangePast() time.Duration {
+	return o.matchRangePast
+}
+
+func (o *options) SetOnNamespaceAddedFn(value OnNamespaceAddedFn) Options {
+	opts := *o
+	opts.onNamespaceAddedFn = value
+	return &opts
+}
+
+func (o *options) OnNamespaceAddedFn() OnNamespaceAddedFn {
+	return o.onNamespaceAddedFn
+}
+
+func (o *options) SetOnNamespaceRemovedFn(value OnNamespaceRemovedFn) Options {
+	opts := *o
+	opts.onNamespaceRemovedFn = value
+	return &opts
+}
+
+func (o *options) OnNamespaceRemovedFn() OnNamespaceRemovedFn {
+	return o.onNamespaceRemovedFn
+}
+
+func (o *options) SetOnRuleSetUpdatedFn(value OnRuleSetUpdatedFn) Options {
+	opts := *o
+	opts.onRuleSetUpdatedFn = value
+	return &opts
+}
+
+func (o *options) OnRuleSetUpdatedFn() OnRuleSetUpdatedFn {
+	return o.onRuleSetUpdatedFn
+}
+
+func defaultRuleSetKeyFn(namespace []byte) string {
+	return fmt.Sprintf(defaultRuleSetKeyFormat, namespace)
+}
