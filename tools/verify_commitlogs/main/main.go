77acd68e0f4e8a26f9383bba043bbc9e29757e06
Richard Artoul <rartoul@uber.com>
2017-10-18 11:33:17 -0400

    Rename to verify_commitlogs + fix nit

--- tools/verify_commitlogs/main/main.go
+++ tools/verify_commitlogs/main/main.go
@@ -29,37 +29,36 @@ import (
 	xtime "github.com/m3db/m3x/time"
 )
 
-var flagParser = flag.NewFlagSet("Verify Commitlogs", flag.ExitOnError)
-
 var (
-	pathPrefixArg         = flagParser.String("path-prefix", "/var/lib/m3db", "Path prefix - must contain a folder called 'commitlogs'")
-	namespaceArg          = flagParser.String("namespace", "metrics", "Namespace")
-	blockSizeArg          = flagParser.Duration("block-size", 10*time.Minute, "Block size")
-	flushSizeArg          = flagParser.Int("flush-size", 524288, "Flush size of commit log")
-	bootstrapRetentionArg = flagParser.Duration("retention", 48*time.Hour, "Retention")
-	shardsCountArg        = flagParser.Int("shards-count", 8192, "Shards count - set number too bootstrap all shards in range")
-	shardsArg             = flagParser.String("shards", "", "Shards - set comma separated list of shards")
-	debugListenAddressArg = flagParser.String("debug-listen-address", "", "Debug listen address - if set will expose pprof, i.e. ':8080'")
-	startUnixTimestampArg = flagParser.Int64("start-unix-timestramp", 0, "Start unix timestamp (Seconds) - If set will bootstrap all data after this timestamp up to end-unix-timestamp, defaults to reading from the beginning of the first commitlog")
-	// 1<<63-62135596801 is the largest possible time.Time that can be represented
-	// without causing overflow when passed to functions in the time package
-	endUnixTimestampArg = flagParser.Int64("end-unix-timestramp", 1<<63-62135596801, "End unix timestamp (Seconds) - If set will bootstrap all data from start-unix-timestamp up to this timestamp, defaults to reading up to the end of the last commitlog")
+	pathPrefixArg           = flag.String("path-prefix", "/var/lib/m3db", "Path prefix - must contain a folder called 'commitlogs'")
+	namespaceArg            = flag.String("namespace", "metrics", "Namespace")
+	blockSizeArg            = flag.Duration("block-size", 10*time.Minute, "Block size")
+	flushSizeArg            = flag.Int("flush-size", 524288, "Flush size of commit log")
+	bootstrapRetentionArg   = flag.Duration("retention", 48*time.Hour, "Retention")
+	shardsCountArg          = flag.Int("shards-count", 8192, "Shards count - set number too bootstrap all shards in range")
+	shardsArg               = flag.String("shards", "", "Shards - set comma separated list of shards")
+	debugListenAddressArg   = flag.String("debug-listen-address", "", "Debug listen address - if set will expose pprof, i.e. ':8080'")
+	currentUnixTimestampArg = flag.Int64("current-unix-timestamp", time.Now().Unix(), "Current unix timestamp (Seconds) - If set will perform the bootstrap as if this was the current time, defaults to current time")
 )
 
 func main() {
-	flagParser.Parse(os.Args[1:])
+	flag.Parse()
+	if *pathPrefixArg == "" ||
+		*namespaceArg == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	var (
-		pathPrefix         = *pathPrefixArg
-		namespaceStr       = *namespaceArg
-		blockSize          = *blockSizeArg
-		flushSize          = *flushSizeArg
-		bootstrapRetention = *bootstrapRetentionArg
-		shardsCount        = *shardsCountArg
-		shards             = *shardsArg
-		debugListenAddress = *debugListenAddressArg
-		startUnixTimestamp = *startUnixTimestampArg
-		endUnixTimestamp   = *endUnixTimestampArg
+		pathPrefix           = *pathPrefixArg
+		namespaceStr         = *namespaceArg
+		blockSize            = *blockSizeArg
+		flushSize            = *flushSizeArg
+		bootstrapRetention   = *bootstrapRetentionArg
+		shardsCount          = *shardsCountArg
+		shards               = *shardsArg
+		debugListenAddress   = *debugListenAddressArg
+		currentUnixTimestamp = *currentUnixTimestampArg
 	)
 
 	log := xlog.NewLogger(os.Stderr)
@@ -76,8 +75,11 @@ func main() {
 
 	shardTimeRanges := result.ShardTimeRanges{}
 
-	start := time.Unix(startUnixTimestamp, 0)
-	end := time.Unix(endUnixTimestamp, 0)
+	now := time.Unix(currentUnixTimestamp, 0)
+	// Round current time down to nearest blocksize (2h) and then decrease blocksize (2h)
+	startInclusive := now.Truncate(blockSize).Add(-blockSize)
+	// Round current time down to nearest blocksize (2h) and then add blocksize (2h)
+	endExclusive := now.Truncate(blockSize).Add(blockSize * 2)
 
 	// Ony used for logging
 	var shardsAll []uint32
@@ -93,14 +95,14 @@ func main() {
 			if err != nil {
 				log.Fatalf("could not parse shard '%s': %v", shard, err)
 			}
-			rng := xtime.Range{Start: start, End: end}
+			rng := xtime.Range{Start: startInclusive, End: endExclusive}
 			shardTimeRanges[uint32(value)] = xtime.NewRanges().AddRange(rng)
 			shardsAll = append(shardsAll, uint32(value))
 		}
 		// Or just handled up to N (shard-count) shards
 	} else if shardsCount > 0 {
 		for i := uint32(0); i < uint32(shardsCount); i++ {
-			rng := xtime.Range{Start: start, End: end}
+			rng := xtime.Range{Start: startInclusive, End: endExclusive}
 			shardTimeRanges[i] = xtime.NewRanges().AddRange(rng)
 			shardsAll = append(shardsAll, i)
 		}
@@ -206,7 +208,6 @@ func main() {
 		iter.Reset(r)
 		return iter
 	})
-	bytesPool.Init()
 
 	segmentReaderPool.Init()
 
