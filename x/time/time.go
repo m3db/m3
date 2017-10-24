5313a288b6c7c6e82542c3273cc47bef0f7b81d9
Richard Artoul <rartoul@uber.com>
2017-10-24 10:36:42 -0400

    Fix docstring

--- x/time/time.go
+++ x/time/time.go
@@ -1,5 +1,5 @@
 package time
 
-// UnixNano is used to indicate that an in64 stores a unix timestamp at
+// UnixNano is used to indicate that an int64 stores a unix timestamp at
 // nanosecond resolution
 type UnixNano int64
