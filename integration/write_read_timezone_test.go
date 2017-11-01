514a9bea55424db85bb003fed658087f712a9fa9
Richard Artoul <rartoul@uber.com>
2017-11-01 11:30:18 -0400

    Add integration test for write/read in a timezone other than that of the server

---
+++ integration/write_read_timezone_test.go
@@ -0,0 +1,119 @@
+package integration
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	xtime "github.com/m3db/m3x/time"
+	"github.com/stretchr/testify/require"
+)
+
+type readWriteTZCase struct {
+	namespace  string
+	id         string
+	datapoints []readWriteTZDP
+}
+
+type readWriteTZDP struct {
+	value     float64
+	timestamp time.Time
+}
+
+// Make sure that everythin works properly end-to-end even if the client issues
+// a write in a timezone other than that of the server.
+func TestWriteReadTimezone(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow() // Just skip if we're doing a short run
+	}
+
+	// Ensure that the test is running with the local timezone set to US/Pacific
+	os.Setenv("TZ", "US/Pacific")
+	name, offset := time.Now().Zone()
+	require.Equal(t, "PDT", name)
+	require.Equal(t, offset, -25200)
+
+	// Setup / start server
+	opts := newTestOptions(t)
+	setup, err := newTestSetup(t, opts)
+	require.NoError(t, err)
+	defer setup.close()
+	require.NoError(t, setup.startServer())
+	require.NoError(t, setup.waitUntilServerIsUp())
+
+	start := setup.getNowFn()
+
+	// Instantiate a client
+	client := setup.m3dbClient
+	session, err := client.DefaultSession()
+	require.NoError(t, err)
+	defer session.Close()
+
+	// Load NY timezone
+	nyLocation, err := time.LoadLocation("America/New_York")
+	require.NoError(t, err)
+
+	// Generate test datapoints
+	namespace := opts.Namespaces()[0].ID().String()
+	startNy := start.In(nyLocation)
+	writeSeries := []readWriteTZCase{
+		readWriteTZCase{
+			namespace: namespace,
+			id:        "some-id-1",
+			datapoints: []readWriteTZDP{
+				readWriteTZDP{
+					value:     20.0,
+					timestamp: startNy,
+				},
+				readWriteTZDP{
+					value:     20.0,
+					timestamp: startNy.Add(1 * time.Second),
+				},
+				readWriteTZDP{
+					value:     20.0,
+					timestamp: startNy.Add(2 * time.Second),
+				},
+			},
+		},
+		readWriteTZCase{
+			namespace: namespace,
+			id:        "some-id-2",
+			datapoints: []readWriteTZDP{
+				readWriteTZDP{
+					value:     30.0,
+					timestamp: startNy,
+				},
+				readWriteTZDP{
+					value:     30.0,
+					timestamp: startNy.Add(1 * time.Second),
+				},
+				readWriteTZDP{
+					value:     30.0,
+					timestamp: startNy.Add(2 * time.Second),
+				},
+			},
+		},
+	}
+
+	// Write datapoint
+	for _, series := range writeSeries {
+		for _, write := range series.datapoints {
+			err = session.Write(series.namespace, series.id, write.timestamp, write.value, xtime.Second, nil)
+			require.NoError(t, err)
+		}
+	}
+
+	// Read datapoints back
+	iters, err := session.FetchAll(opts.Namespaces()[0].ID().String(), []string{"some-id-1", "some-id-2"}, startNy, startNy.Add(1*time.Hour))
+	require.NoError(t, err)
+
+	// Assert datapoints match what we wrote
+	for i, iter := range iters.Iters() {
+		for j := 0; iter.Next(); j++ {
+			dp, _, _ := iter.Current()
+			expectedDatapoint := writeSeries[i].datapoints[j]
+			require.True(t, expectedDatapoint.timestamp.Equal(dp.Timestamp))
+			require.Equal(t, expectedDatapoint.value, dp.Value)
+		}
+	}
+}
