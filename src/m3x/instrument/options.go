aeb62fa709a9f9b0572e785a5722281e27f64b05
Jerome Froelich <jeromefroelich@hotmail.com>
2017-11-11 17:34:25 -0800

    [instrument] Add zap logger to options interface (#93)

--- src/m3x/instrument/options.go
+++ src/m3x/instrument/options.go
@@ -24,7 +24,9 @@ import (
 	"time"
 
 	"github.com/m3db/m3x/log"
+
 	"github.com/uber-go/tally"
+	"go.uber.org/zap"
 )
 
 const (
@@ -34,6 +36,7 @@ const (
 
 type options struct {
 	logger         log.Logger
+	zap            *zap.Logger
 	scope          tally.Scope
 	samplingRate   float64
 	reportInterval time.Duration
@@ -44,6 +47,7 @@ func NewOptions() Options {
 	logger := log.NewLevelLogger(log.SimpleLogger, log.LevelInfo)
 	return &options{
 		logger:         logger,
+		zap:            zap.L(),
 		scope:          tally.NoopScope,
 		samplingRate:   defaultSamplingRate,
 		reportInterval: defaultReportingInterval,
@@ -58,6 +62,16 @@ func (o *options) SetLogger(value log.Logger) Options {
 
 func (o *options) Logger() log.Logger {
 	return o.logger
+}
+
+func (o *options) SetZapLogger(value *zap.Logger) Options {
+	opts := *o
+	opts.zap = value
+	return &opts
+}
+
+func (o *options) ZapLogger() *zap.Logger {
+	return o.zap
 }
 
 func (o *options) SetMetricsScope(value tally.Scope) Options {
