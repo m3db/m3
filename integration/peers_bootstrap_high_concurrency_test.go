ac6bd8c9f6ea40af8d76c251ffb87c50f0b697bd
Richard Artoul <rartoul@uber.com>
2017-11-14 18:00:32 -0500

    Disable slow tests

--- integration/peers_bootstrap_high_concurrency_test.go
+++ integration/peers_bootstrap_high_concurrency_test.go
@@ -1,4 +1,4 @@
-// +build integration
+// +build integration_disabled
 
 // Copyright (c) 2016 Uber Technologies, Inc.
 //
--- integration/peers_bootstrap_v2_high_concurrency_test.go
+++ integration/peers_bootstrap_v2_high_concurrency_test.go
@@ -1,4 +1,4 @@
-// +build integration
+// +build integration_disabled
 
 // Copyright (c) 2017 Uber Technologies, Inc.
 //
