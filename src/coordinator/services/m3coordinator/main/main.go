bfa1816a906b60dea63571212d5453f9b1ff9516
Nikunj Aggarwal <nikunj@uber.com>
2017-12-07 17:21:50 -0500

    Address comments

--- src/coordinator/services/m3coordinator/main/main.go
+++ src/coordinator/services/m3coordinator/main/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/m3db/m3coordinator/services/m3coordinator/httpd"
 	"net/http"
+	"fmt"
+	"os"
 )
 
 func main() {
-	handler := httpd.NewHandler()
+	handler, err := httpd.NewHandler()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to set up handlers, got error %v\n", err)
+		os.Exit(1)
+	}
+
 	handler.Logger.Info("Starting server")
 	handler.RegisterRoutes()
 	http.ListenAndServe(":1234", handler.Router)
