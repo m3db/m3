71ad819f701ec8d7187773ddc0f944946815add6
Nikunj Aggarwal <nikunj@uber.com>
2017-12-11 11:58:41 -0500

    Integrate with zap logging

--- src/coordinator/services/m3coordinator/main/main.go
+++ src/coordinator/services/m3coordinator/main/main.go
@@ -5,17 +5,20 @@ import (
 	"net/http"
 	"fmt"
 	"os"
+	"github.com/m3db/m3coordinator/util/logging"
+	"context"
 )
 
 func main() {
+	logging.InitWithCores(nil)
 	handler, err := httpd.NewHandler()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to set up handlers, got error %v\n", err)
 		os.Exit(1)
 	}
-
-	handler.Logger.Info("Starting server")
 	handler.RegisterRoutes()
+
+	logging.WithContext(context.TODO()).Info("Starting server")
 	http.ListenAndServe(":1234", handler.Router)
 
 }
---
+++ src/coordinator/util/logging/log.go
@@ -0,0 +1,60 @@
+package logging
+
+import (
+	"context"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type loggerKeyType int
+
+const loggerKey loggerKeyType = iota
+
+var logger zap.Logger
+
+// InitWithCores is used to set up a new logger
+func InitWithCores(cores []zapcore.Core) {
+
+	consoleEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
+
+	consoleErrors := zapcore.Lock(os.Stderr)
+	consoleDebugging := zapcore.Lock(os.Stdout)
+
+	if cores == nil {
+		cores = make([]zapcore.Core, 0, 1)
+	}
+	cores = append(cores, zapcore.NewCore(consoleEncoder, consoleErrors, highPriority))
+	cores = append(cores, zapcore.NewCore(consoleEncoder, consoleDebugging, lowPriority))
+
+	core := zapcore.NewTee(cores...)
+
+	logger := zap.New(core)
+	defer logger.Sync()
+	logger.Info("constructed a logger")
+}
+
+// NewContext returns a context has a zap logger with the extra fields added
+func NewContext(ctx context.Context, fields ...zapcore.Field) context.Context {
+	return context.WithValue(ctx, loggerKey, WithContext(ctx).With(fields...))
+}
+
+// WithContext returns a zap logger with as much context as possible
+func WithContext(ctx context.Context) *zap.Logger {
+	if ctx == nil {
+		return &logger
+	}
+	if ctxLogger, ok := ctx.Value(loggerKey).(zap.Logger); ok {
+		return &ctxLogger
+	}
+
+	return &logger
+}
