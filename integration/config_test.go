88db65c6f1e60640e38b28824e3b79b398a6df74
Benjamin Raskin <braskin@uber.com>
2018-01-16 14:58:32 -0500

    Fix integration tests

--- integration/config_test.go
+++ integration/config_test.go
@@ -193,7 +193,7 @@ func TestConfig(t *testing.T) {
 	// NB(r): Make sure client config points to the root config
 	// service since we're going to instantiate the client configuration
 	// just by itself.
-	cfg.Client.EnvironmentConfig.Service = &cfg.EnvironmentConfig.Service
+	cfg.Client.EnvironmentConfig.Service = cfg.EnvironmentConfig.Service
 
 	cli, err := cfg.Client.NewClient(client.ConfigurationParameters{})
 	require.NoError(t, err)
@@ -404,14 +404,15 @@ pooling:
             - capacity: 4096
               size: 128
 
-configService:
-    env: {{.ServiceEnv}}
-    zone: {{.ServiceZone}}
-    service: {{.ServiceName}}
-    cacheDir: {{.ConfigServiceCacheDir}}
-    etcdClusters:
-        - zone: {{.ServiceZone}}
-          endpoints: {{.EtcdEndpoints}}
+config:
+    service:
+        env: {{.ServiceEnv}}
+        zone: {{.ServiceZone}}
+        service: {{.ServiceName}}
+        cacheDir: {{.ConfigServiceCacheDir}}
+        etcdClusters:
+            - zone: {{.ServiceZone}}
+              endpoints: {{.EtcdEndpoints}}
 `
 
 type cleanup func()
