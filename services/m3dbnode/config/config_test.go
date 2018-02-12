941778b8b511e7de14b9b1852410d85fcbc3adfb
Richard Artoul <rartoul@uber.com>
2018-02-11 19:45:41 -0500

    tweak test

--- services/m3dbnode/config/config_test.go
+++ services/m3dbnode/config/config_test.go
@@ -82,7 +82,6 @@ client:
 
 gcPercentage: 100
 
-writeNewSeriesAsync: true
 writeNewSeriesLimitPerSecond: 1048576
 writeNewSeriesBackoffDuration: 2ms
 
@@ -239,6 +238,7 @@ config:
                 - etcd05-us-west1:2379
 hashing:
   seed: 42
+writeNewSeriesAsync: true
 `
 
 	fd, err := ioutil.TempFile("", "config.yaml")
