849e98e0646be55a3ffeb782dbadce707de4d3b3
Nikunj Aggarwal <nikunj@uber.com>
2018-02-20 14:28:21 -0500

    Add fanout storage which can query multiple storages in parallel (#20)
    * Add fanout storage which can query multiple storages in parallel
    
    * Fix test
    
    * Address commentS
    
    * Separate filters, initialize in main
    
    * Use error group
    
    * Review comments
    
    * Review comments
    
    * Address comments
    
    * Test for cancel
    
    * Simpler asserts

---
+++ src/coordinator/util/execution/parallel.go
@@ -0,0 +1,44 @@
+package execution
+
+import (
+	"context"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// Request is input for parallel execution
+type Request interface {
+	Process(ctx context.Context) error
+}
+
+// RequestResponse is used to combine both request and response in output
+type RequestResponse struct {
+	Request  Request
+	Response *Response
+}
+
+// Response is returned from parallel execution
+type Response struct {
+	Value interface{}
+	Err   error
+}
+
+// ExecuteParallel executes a slice of requests in parallel and returns unordered results
+func ExecuteParallel(ctx context.Context, requests []Request) error {
+	return processParallel(ctx, requests)
+}
+
+// Process the requests in parallel and stop on first error
+func processParallel(ctx context.Context, requests []Request) error {
+	g, ctx := errgroup.WithContext(ctx)
+	for _, req := range requests {
+		// Need to use a separate func since g.Go doesn't take input
+		func (req Request) {
+			g.Go(func() error {
+				return req.Process(ctx)
+			})
+		}(req)
+	}
+
+	return g.Wait()
+}
