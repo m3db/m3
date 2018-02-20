bd35608b69011c4cbcf0f70aa7becdd6dbb2bbf3
Nikunj Aggarwal <nikunj@uber.com>
2018-02-20 14:28:21 -0500

    Add fanout storage which can query multiple storages in parallel (#20)
    * Add fanout storage which can query multiple storages in parallel
    
    * Fix test
    
    * Address commentS
    
    * Separate filters, initialize in main
    
    * Use error group
    
    * Review comments
    
    * Review comments
    
    * Address comments
    
    * Test for cancel
    
    * Simpler asserts

--- src/coordinator/policy/filter/storage.go
+++ src/coordinator/policy/filter/storage.go
@@ -4,3 +4,8 @@ import "github.com/m3db/m3coordinator/storage"
 
 // Storage determines whether storage can fulfil the read query
 type Storage func(query storage.Query, store storage.Storage) bool
+
+// LocalOnly filters out all remote storages
+func LocalOnly(query storage.Query, store storage.Storage) bool {
+	return store.Type() == storage.TypeLocalDC
+}
