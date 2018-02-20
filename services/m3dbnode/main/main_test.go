7b08502f5c8c6b2c9b92a6033824294c807e5b75
Prateek Rungta <rungta@uber.com>
2018-02-20 13:51:39 -0500

    migrate unit test

--- services/m3dbnode/main/main_test.go
+++ services/m3dbnode/main/main_test.go
@@ -18,9 +18,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-// +build integration
+// +build big
 
-package integration
+package main_test
 
 import (
 	"bytes"
