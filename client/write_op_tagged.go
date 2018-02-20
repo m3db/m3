huydx/m3db

Add rpc WriteTaggedBatch(...)

client/write_op_tagged.go
@@ -1,0 +1,101 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package client
+
+import (
+	"math"
+
+	"github.com/m3db/m3db/generated/thrift/rpc"
+	"github.com/m3db/m3x/ident"
+	"github.com/m3db/m3x/pool"
+)
+
+var (
+	// NB(bl): use an invalid shardID for the zerod op
+	writeOpTaggedZeroed = writeOpTagged{shardID: math.MaxUint32}
+)
+
+type writeOpTagged struct {
+	namespace    ident.ID
+	shardID      uint32
+	request      rpc.WriteTaggedBatchRawRequestElement
+	datapoint    rpc.Datapoint
+	completionFn completionFn
+	pool         *writeOpTaggedPool
+}
+
+func (w *writeOpTagged) reset() {
+	*w = writeOpTaggedZeroed
+	w.request.Datapoint = &w.datapoint
+}
+
+func (w *writeOpTagged) Close() {
+	p := w.pool
+	w.reset()
+	if p != nil {
+		p.Put(w)
+	}
+}
+
+func (w *writeOpTagged) Size() int {
+	// Writes always represent a single write
+	return 1
+}
+
+func (w *writeOpTagged) CompletionFn() completionFn {
+	return w.completionFn
+}
+
+func (w *writeOpTagged) SetCompletionFn(fn completionFn) {
+	w.completionFn = fn
+}
+
+func (w *writeOpTagged) ShardID() uint32 {
+	return w.shardID
+}
+
+type writeOpTaggedPool struct {
+	pool pool.ObjectPool
+}
+
+func newWriteOpTaggedPool(opts pool.ObjectPoolOptions) *writeOpTaggedPool {
+	p := pool.NewObjectPool(opts)
+	return &writeOpTaggedPool{pool: p}
+}
+
+func (p *writeOpTaggedPool) Init() {
+	p.pool.Init(func() interface{} {
+		w := &writeOpTagged{}
+		w.reset()
+		return w
+	})
+}
+
+func (p *writeOpTaggedPool) Get() *writeOpTagged {
+	w := p.pool.Get().(*writeOpTagged)
+	w.pool = p
+	return w
+}
+
+func (p *writeOpTaggedPool) Put(w *writeOpTagged) {
+	w.reset()
+	p.pool.Put(w)
+}

client/write_state.go
@@ -22,69 +22,24 @@ package client
 
 import (
 	"fmt"
-	"math"
 	"sync"
 
 	"github.com/m3db/m3cluster/shard"
-	"github.com/m3db/m3db/generated/thrift/rpc"
 	"github.com/m3db/m3db/topology"
 	xerrors "github.com/m3db/m3x/errors"
 	"github.com/m3db/m3x/ident"
 	"github.com/m3db/m3x/pool"
 )
 
-var (
-	writeOpZeroed = writeOp{shardID: math.MaxUint32}
-	// NB(bl): use an invalid shardID for the zerod op
-)
+// wOp represents a generic write operation
+type wOp interface {
+	op
 
-type writeOp struct {
-	namespace    ident.ID
-	shardID      uint32
-	request      rpc.WriteBatchRawRequestElement
-	datapoint    rpc.Datapoint
-	completionFn completionFn
-}
+	ShardID() uint32
 
-func (w *writeOp) reset() {
-	*w = writeOpZeroed
-	w.request.Datapoint = &w.datapoint
-}
+	SetCompletionFn(fn completionFn)
 
-func (w *writeOp) Size() int {
-	// Writes always represent a single write
-	return 1
-}
-
-func (w *writeOp) CompletionFn() completionFn {
-	return w.completionFn
-}
-
-type writeOpPool struct {
-	pool pool.ObjectPool
-}
-
-func newWriteOpPool(opts pool.ObjectPoolOptions) *writeOpPool {
-	p := pool.NewObjectPool(opts)
-	return &writeOpPool{pool: p}
-}
-
-func (p *writeOpPool) Init() {
-	p.pool.Init(func() interface{} {
-		w := &writeOp{}
-		w.reset()
-		return w
-	})
-}
-
-func (p *writeOpPool) Get() *writeOp {
-	w := p.pool.Get().(*writeOp)
-	return w
-}
-
-func (p *writeOpPool) Put(w *writeOp) {
-	w.reset()
-	p.pool.Put(w)
+	Close()
 }
 
 type writeState struct {
@@ -94,9 +49,10 @@ type writeState struct {
 
 	session           *session
 	topoMap           topology.Map
-	op                *writeOp
+	op                wOp
 	nsID              ident.ID
 	tsID              ident.ID
+	tags              ident.Tags
 	majority, pending int32
 	success           int32
 	errors            []error
@@ -112,11 +68,11 @@ func (w *writeState) reset() {
 }
 
 func (w *writeState) close() {
-	w.op.reset()
-	w.session.writeOpPool.Put(w.op)
+	w.op.Close()
 
 	w.nsID.Finalize()
 	w.tsID.Finalize()
+	w.tags.Finalize()
 
 	w.op, w.majority, w.pending, w.success = nil, 0, 0, 0
 	w.nsID, w.tsID = nil, nil
@@ -148,9 +104,9 @@ func (w *writeState) completionFn(result interface{}, err error) {
 	} else if hostShardSet, ok := w.topoMap.LookupHostShardSet(hostID); !ok {
 		errStr := "missing host shard in writeState completionFn: %s"
 		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, hostID))
-	} else if shardState, err := hostShardSet.ShardSet().LookupStateByID(w.op.shardID); err != nil {
+	} else if shardState, err := hostShardSet.ShardSet().LookupStateByID(w.op.ShardID()); err != nil {
 		errStr := "missing shard %d in host %s"
-		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.shardID, hostID))
+		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.ShardID(), hostID))
 	} else if shardState != shard.Available {
 		// NB(bl): only count writes to available shards towards success
 		var errStr string
@@ -162,7 +118,7 @@ func (w *writeState) completionFn(result interface{}, err error) {
 		default:
 			errStr = "shard %d in host %s not available (unknown state)"
 		}
-		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.shardID, hostID))
+		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.ShardID(), hostID))
 	} else {
 		w.success++
 	}

m3db/m3

client/write_op_tagged.go
@@ -1,0 +1,105 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package client
+
+import (
+	"math"
+
+	"github.com/m3db/m3db/generated/thrift/rpc"
+	"github.com/m3db/m3x/ident"
+	"github.com/m3db/m3x/pool"
+)
+
+var (
+	// NB(bl): use an invalid shardID for the zerod op
+	writeTaggedOpZeroed = writeTaggedOp{shardID: math.MaxUint32}
+)
+
+type writeTaggedOp struct {
+	namespace    ident.ID
+	shardID      uint32
+	request      rpc.WriteTaggedBatchRawRequestElement
+	datapoint    rpc.Datapoint
+	completionFn completionFn
+	pool         *writeTaggedOpPool
+}
+
+func (w *writeTaggedOp) reset() {
+	*w = writeTaggedOpZeroed
+	w.request.Datapoint = &w.datapoint
+	// TODO(prateek): need to test if this works; further if we can actually
+	// use the pooled parts of the arrays
+	w.request.TagNames = w.request.TagNames[:0]
+	w.request.TagValues = w.request.TagValues[:0]
+}
+
+func (w *writeTaggedOp) Close() {
+	p := w.pool
+	w.reset()
+	if p != nil {
+		p.Put(w)
+	}
+}
+
+func (w *writeTaggedOp) Size() int {
+	// Writes always represent a single write
+	return 1
+}
+
+func (w *writeTaggedOp) CompletionFn() completionFn {
+	return w.completionFn
+}
+
+func (w *writeTaggedOp) SetCompletionFn(fn completionFn) {
+	w.completionFn = fn
+}
+
+func (w *writeTaggedOp) ShardID() uint32 {
+	return w.shardID
+}
+
+type writeTaggedOpPool struct {
+	pool pool.ObjectPool
+}
+
+func newWriteTaggedOpPool(opts pool.ObjectPoolOptions) *writeTaggedOpPool {
+	p := pool.NewObjectPool(opts)
+	return &writeTaggedOpPool{pool: p}
+}
+
+func (p *writeTaggedOpPool) Init() {
+	p.pool.Init(func() interface{} {
+		w := &writeTaggedOp{}
+		w.reset()
+		return w
+	})
+}
+
+func (p *writeTaggedOpPool) Get() *writeTaggedOp {
+	w := p.pool.Get().(*writeTaggedOp)
+	w.pool = p
+	return w
+}
+
+func (p *writeTaggedOpPool) Put(w *writeTaggedOp) {
+	w.reset()
+	p.pool.Put(w)
+}

client/write_state.go
@@ -22,69 +22,24 @@ package client
 
 import (
 	"fmt"
-	"math"
 	"sync"
 
 	"github.com/m3db/m3cluster/shard"
-	"github.com/m3db/m3db/generated/thrift/rpc"
 	"github.com/m3db/m3db/topology"
 	xerrors "github.com/m3db/m3x/errors"
 	"github.com/m3db/m3x/ident"
 	"github.com/m3db/m3x/pool"
 )
 
-var (
-	writeOpZeroed = writeOp{shardID: math.MaxUint32}
-	// NB(bl): use an invalid shardID for the zerod op
-)
+// writableOp represents a generic write operation
+type writableOp interface {
+	op
 
-type writeOp struct {
-	namespace    ident.ID
-	shardID      uint32
-	request      rpc.WriteBatchRawRequestElement
-	datapoint    rpc.Datapoint
-	completionFn completionFn
-}
+	ShardID() uint32
 
-func (w *writeOp) reset() {
-	*w = writeOpZeroed
-	w.request.Datapoint = &w.datapoint
-}
+	SetCompletionFn(fn completionFn)
 
-func (w *writeOp) Size() int {
-	// Writes always represent a single write
-	return 1
-}
-
-func (w *writeOp) CompletionFn() completionFn {
-	return w.completionFn
-}
-
-type writeOpPool struct {
-	pool pool.ObjectPool
-}
-
-func newWriteOpPool(opts pool.ObjectPoolOptions) *writeOpPool {
-	p := pool.NewObjectPool(opts)
-	return &writeOpPool{pool: p}
-}
-
-func (p *writeOpPool) Init() {
-	p.pool.Init(func() interface{} {
-		w := &writeOp{}
-		w.reset()
-		return w
-	})
-}
-
-func (p *writeOpPool) Get() *writeOp {
-	w := p.pool.Get().(*writeOp)
-	return w
-}
-
-func (p *writeOpPool) Put(w *writeOp) {
-	w.reset()
-	p.pool.Put(w)
+	Close()
 }
 
 type writeState struct {
@@ -94,9 +49,10 @@ type writeState struct {
 
 	session           *session
 	topoMap           topology.Map
-	op                *writeOp
+	op                writableOp
 	nsID              ident.ID
 	tsID              ident.ID
+	tags              ident.Tags
 	majority, pending int32
 	success           int32
 	errors            []error
@@ -112,11 +68,11 @@ func (w *writeState) reset() {
 }
 
 func (w *writeState) close() {
-	w.op.reset()
-	w.session.writeOpPool.Put(w.op)
+	w.op.Close()
 
 	w.nsID.Finalize()
 	w.tsID.Finalize()
+	w.tags.Finalize()
 
 	w.op, w.majority, w.pending, w.success = nil, 0, 0, 0
 	w.nsID, w.tsID = nil, nil
@@ -148,9 +104,9 @@ func (w *writeState) completionFn(result interface{}, err error) {
 	} else if hostShardSet, ok := w.topoMap.LookupHostShardSet(hostID); !ok {
 		errStr := "missing host shard in writeState completionFn: %s"
 		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, hostID))
-	} else if shardState, err := hostShardSet.ShardSet().LookupStateByID(w.op.shardID); err != nil {
+	} else if shardState, err := hostShardSet.ShardSet().LookupStateByID(w.op.ShardID()); err != nil {
 		errStr := "missing shard %d in host %s"
-		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.shardID, hostID))
+		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.ShardID(), hostID))
 	} else if shardState != shard.Available {
 		// NB(bl): only count writes to available shards towards success
 		var errStr string
@@ -162,7 +118,7 @@ func (w *writeState) completionFn(result interface{}, err error) {
 		default:
 			errStr = "shard %d in host %s not available (unknown state)"
 		}
-		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.shardID, hostID))
+		wErr = xerrors.NewRetryableError(fmt.Errorf(errStr, w.op.ShardID(), hostID))
 	} else {
 		w.success++
 	}