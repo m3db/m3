986e025647a5903cf484bbaf68ab9623f041075d
Nikunj Aggarwal <nikunj@uber.com>
2018-02-20 14:28:21 -0500

    Add fanout storage which can query multiple storages in parallel (#20)
    * Add fanout storage which can query multiple storages in parallel
    
    * Fix test
    
    * Address commentS
    
    * Separate filters, initialize in main
    
    * Use error group
    
    * Review comments
    
    * Review comments
    
    * Address comments
    
    * Test for cancel
    
    * Simpler asserts

---
+++ src/coordinator/errors/storage.go
@@ -0,0 +1,19 @@
+package errors
+
+import (
+	"errors"
+)
+
+var (
+	// ErrInvalidFetchResponse is returned when fetch fails from storage.
+	ErrInvalidFetchResponse = errors.New("invalid response from fetch")
+
+	// ErrFetchResponseOrder is returned fetch responses are not in order.
+	ErrFetchResponseOrder = errors.New("responses out of order for fetch")
+
+	// ErrFetchRequestType is an error returned when response from fetch has invalid type.
+	ErrFetchRequestType = errors.New("invalid request type")
+
+	// ErrInvalidFetchResult is an error returned when fetch result is invalid.
+	ErrInvalidFetchResult = errors.New("invalid fetch result")
+)
