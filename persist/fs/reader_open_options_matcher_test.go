cd0d670e728f88087ef7a223296eddbd1cd36d62
Richard Artoul <rartoul@uber.com>
2018-03-23 11:42:17 -0400

    Move matcher to its own file

---
+++ persist/fs/reader_open_options_matcher_test.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package fs
+
+// ReaderOpenOptionsMatcher is a matcher for the ReaderOpenOptions struct
+import (
+	"fmt"
+	"time"
+
+	"github.com/m3db/m3x/ident"
+)
+
+type ReaderOpenOptionsMatcher struct {
+	Namespace  ident.ID
+	BlockStart time.Time
+	Shard      uint32
+	IsSnapshot bool
+}
+
+// Matches determine whether m matches a WriterOpenOptions
+func (m ReaderOpenOptionsMatcher) Matches(x interface{}) bool {
+	readerOpenOptions, ok := x.(ReaderOpenOptions)
+	if !ok {
+		return false
+	}
+
+	if !m.Namespace.Equal(readerOpenOptions.Namespace) {
+		return false
+	}
+	if m.Shard != readerOpenOptions.Shard {
+		return false
+	}
+	if !m.BlockStart.Equal(readerOpenOptions.BlockStart) {
+		return false
+	}
+	if !m.IsSnapshot == readerOpenOptions.IsSnapshot {
+		return false
+	}
+
+	return true
+}
+
+func (m ReaderOpenOptionsMatcher) String() string {
+	return fmt.Sprintf(
+		"namespace: %s, shard: %d, blockstart: %d, isSnapshot: %t",
+		m.Namespace.String(), m.Shard, m.BlockStart.Unix(), m.IsSnapshot,
+	)
+}
