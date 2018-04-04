de26d6a23d110c7cb0f219718e2305e4b1102dfc
Jake Skelcy <jskelcy@uber.com>
2018-04-04 14:14:13 -0400

    ruleData had incorrect json struct tag (#145)
    * ruleData had incorrect json struct tag
    
    * deserialize changes tests

--- src/metrics/rules/models/changes/mapping_test.go
+++ src/metrics/rules/models/changes/mapping_test.go
@@ -1,9 +1,11 @@
 package changes
 
 import (
+	"encoding/json"
 	"sort"
 	"testing"
 
+	"github.com/m3db/m3metrics/policy"
 	"github.com/m3db/m3metrics/rules/models"
 
 	"github.com/stretchr/testify/require"
@@ -129,4 +131,49 @@ func TestSortMappingRuleChanges(t *testing.T) {
 
 	sort.Sort(mappingRuleChangesByOpAscNameAscIDAsc(ruleChanges))
 	require.Equal(t, expected, ruleChanges)
+}
+
+func TestMappingRuleJSONDeserialization(t *testing.T) {
+	fixtureBytes := []byte(`{
+		"op": "change",
+		"ruleData": {
+			"id": "validID",
+			"name": "valid rule name",
+			"cutoverMillis": 61522,
+			"filter": "name:servers.* service:servers",
+			"policies": [
+					"1m:2d",
+					"1m:40d"
+			],
+			"lastUpdatedBy": "valid user name",
+			"lastUpdatedAtMillis": 1522
+		},
+		"ruleID": "validID"
+	}`)
+
+	ruleChange := &MappingRuleChange{}
+	err := json.Unmarshal(fixtureBytes, ruleChange)
+	require.NoError(t, err)
+
+	policy1M2D, err := policy.ParsePolicy("1m:2d")
+	require.NoError(t, err)
+	policy1m40d, err := policy.ParsePolicy("1m:40d")
+	require.NoError(t, err)
+	expected := MappingRuleChange{
+		Op: "change",
+		RuleData: &models.MappingRule{
+			ID:            "validID",
+			Name:          "valid rule name",
+			CutoverMillis: 61522,
+			Filter:        "name:servers.* service:servers",
+			Policies: []policy.Policy{
+				policy1M2D,
+				policy1m40d,
+			},
+			LastUpdatedBy:       "valid user name",
+			LastUpdatedAtMillis: 1522,
+		},
+		RuleID: ptr("validID"),
+	}
+	require.Equal(t, expected, *ruleChange)
 }
