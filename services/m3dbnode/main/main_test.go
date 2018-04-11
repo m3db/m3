a0c2b937a277e1ceab37c3bb1ad3325b2f06605f
Richard Artoul <rartoul@uber.com>
2018-04-11 16:07:22 -0400

    Remove duplicate decls

--- services/m3dbnode/main/main_test.go
+++ services/m3dbnode/main/main_test.go
@@ -23,28 +23,20 @@
 package main_test
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"strconv"
-	"strings"
 	"sync"
 	"testing"
 	"text/template"
 	"time"
 
-	"github.com/gogo/protobuf/proto"
 	"github.com/m3db/m3cluster/integration/etcd"
 	"github.com/m3db/m3cluster/placement"
 	"github.com/m3db/m3cluster/services"
 	"github.com/m3db/m3db/client"
 	"github.com/m3db/m3db/kvconfig"
-	"github.com/m3db/m3db/retention"
 	"github.com/m3db/m3db/services/m3dbnode/config"
 	"github.com/m3db/m3db/services/m3dbnode/server"
-	"github.com/m3db/m3db/storage/namespace"
 	xconfig "github.com/m3db/m3x/config"
 	"github.com/m3db/m3x/ident"
 	"github.com/m3db/m3x/instrument"
@@ -621,70 +613,3 @@ config:
               endpoint: {{.InitialClusterEndpoint}}
 `
 )
-
-type cleanup func()
-
-func tempFile(t *testing.T, name string) (*os.File, cleanup) {
-	fd, err := ioutil.TempFile("", name)
-	require.NoError(t, err)
-
-	fname := fd.Name()
-	return fd, func() {
-		assert.NoError(t, fd.Close())
-		assert.NoError(t, os.Remove(fname))
-	}
-}
-
-func tempFileTouch(t *testing.T, name string) (string, cleanup) {
-	fd, err := ioutil.TempFile("", name)
-	require.NoError(t, err)
-
-	fname := fd.Name()
-	require.NoError(t, fd.Close())
-	return fname, func() {
-		assert.NoError(t, os.Remove(fname))
-	}
-}
-
-func tempDir(t *testing.T, name string) (string, cleanup) {
-	dir, err := ioutil.TempDir("", name)
-	require.NoError(t, err)
-	return dir, func() {
-		assert.NoError(t, os.RemoveAll(dir))
-	}
-}
-
-// yamlArray returns a JSON array which is valid YAML, hehe..
-func yamlArray(t *testing.T, values []string) string {
-	buff := bytes.NewBuffer(nil)
-	err := json.NewEncoder(buff).Encode(values)
-	require.NoError(t, err)
-	return strings.TrimSpace(buff.String())
-}
-
-func endpoint(ip string, port uint32) string {
-	return fmt.Sprintf("%s:%d", ip, port)
-}
-
-func newNamespaceProtoValue(id string) (proto.Message, error) {
-	md, err := namespace.NewMetadata(
-		ident.StringID(id),
-		namespace.NewOptions().
-			SetBootstrapEnabled(true).
-			SetCleanupEnabled(true).
-			SetFlushEnabled(true).
-			SetRepairEnabled(true).
-			SetWritesToCommitLog(true).
-			SetRetentionOptions(
-				retention.NewOptions().
-					SetBlockSize(1*time.Hour).
-					SetRetentionPeriod(24*time.Hour)))
-	if err != nil {
-		return nil, err
-	}
-	nsMap, err := namespace.NewMap([]namespace.Metadata{md})
-	if err != nil {
-		return nil, err
-	}
-	return namespace.ToProto(nsMap), nil
-}
