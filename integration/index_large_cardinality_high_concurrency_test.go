8039b270201048ba293dfd37a0354aafed492f46
Prateek Rungta <rungta@uber.com>
2018-04-11 00:07:50 -0400

    reduce numbers further

--- integration/index_large_cardinality_high_concurrency_test.go
+++ integration/index_large_cardinality_high_concurrency_test.go
@@ -43,7 +43,7 @@ func TestIndexLargeCardinalityHighConcurrency(t *testing.T) {
 	}
 
 	concurrency := 4
-	writeEach := 8192
+	writeEach := 1024
 	maxNumTags := 10
 
 	genIDTags := func(i int, j int) (ident.ID, ident.TagIterator) {
