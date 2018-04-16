0ed5876657764a33929cbdc33672bc95f1e6062a
Nikunj Aggarwal <nikunj@uber.com>
2018-04-16 13:28:10 -0400

    Add physical plan generator and result node (#37)
    * Add physical plan generator and result node
    
    * Stack optimization, remove interfaces

--- src/coordinator/plan/logical.go
+++ src/coordinator/plan/logical.go
@@ -8,67 +8,95 @@ import (
 
 // LogicalPlan converts a DAG into a list of steps to be executed in order
 type LogicalPlan struct {
-	Transforms map[parser.TransformID]*LogicalStep
-	Pipeline   []parser.TransformID // Ordered list of steps to be performed
+	Steps    map[parser.TransformID]LogicalStep
+	Pipeline []parser.TransformID // Ordered list of steps to be performed
 }
 
 // LogicalStep is a single step in a logical plan
 type LogicalStep struct {
 	Parents   []parser.TransformID
 	Children  []parser.TransformID
-	Transform *parser.Transform
+	Transform parser.Transform
 }
 
-// NewLogicalPlan returns an empty logical plan
-func NewLogicalPlan() *LogicalPlan {
-	return &LogicalPlan{
-		Transforms: make(map[parser.TransformID]*LogicalStep),
-		Pipeline:   make([]parser.TransformID, 0),
+// NewLogicalPlan creates a plan from the DAG structure
+func NewLogicalPlan(transforms parser.Transforms, edges parser.Edges) (LogicalPlan, error) {
+	lp := LogicalPlan{
+		Steps:    make(map[parser.TransformID]LogicalStep),
+		Pipeline: make([]parser.TransformID, 0, len(transforms)),
 	}
-}
-
-// NewLogicalStep returns an empty plan step
-func NewLogicalStep(Transform *parser.Transform) *LogicalStep {
-	return &LogicalStep{
-		Transform: Transform,
-		Parents:   make([]parser.TransformID, 0),
-		Children:  make([]parser.TransformID, 0),
-	}
-}
-
-func (l *LogicalPlan) String() string {
-	return fmt.Sprintf("Transforms: %s, Pipeline: %s", l.Transforms, l.Pipeline)
-}
-
-func (l *LogicalStep) String() string {
-	return fmt.Sprintf("Parents: %s, Children: %s, Transform: %s", l.Parents, l.Children, l.Transform)
-}
-
-// GenerateLogicalPlan creates a plan from the DAG structure
-func GenerateLogicalPlan(transforms parser.Transforms, edges parser.Edges) (*LogicalPlan, error) {
-	lp := NewLogicalPlan()
 
 	// Create all steps
 	for _, transform := range transforms {
-		lp.Transforms[transform.ID] = NewLogicalStep(transform)
+		lp.Steps[transform.ID] = LogicalStep{
+			Transform: transform,
+			Parents:   make([]parser.TransformID, 0, 1),
+			Children:  make([]parser.TransformID, 0, 1),
+		}
 		lp.Pipeline = append(lp.Pipeline, transform.ID)
 	}
 
 	// Link all parent/children
 	for _, edge := range edges {
-		parent, ok := lp.Transforms[edge.ParentID]
+		parent, ok := lp.Steps[edge.ParentID]
 		if !ok {
-			return nil, fmt.Errorf("invalid DAG found, parent %s not found", edge.ParentID)
+			return LogicalPlan{}, fmt.Errorf("invalid DAG found, parent %s not found for child %s", edge.ParentID, edge.ChildID)
 		}
 
-		child, ok := lp.Transforms[edge.ChildID]
+		child, ok := lp.Steps[edge.ChildID]
 		if !ok {
-			return nil, fmt.Errorf("invalid DAG found, child %s not found", edge.ChildID)
+			return LogicalPlan{}, fmt.Errorf("invalid DAG found, child %s not found for parent %s", edge.ChildID, edge.ParentID)
 		}
 
-		parent.Children = append(parent.Children, child.Transform.ID)
-		child.Parents = append(child.Parents, parent.Transform.ID)
+		parent.Children = append(parent.Children, child.ID())
+		child.Parents = append(child.Parents, parent.ID())
+		// Write back since we are doing copy instead reference
+		lp.Steps[edge.ParentID] = parent
+		lp.Steps[edge.ChildID] = child
 	}
 
 	return lp, nil
+}
+
+func (l LogicalPlan) String() string {
+	return fmt.Sprintf("Steps: %s, Pipeline: %s", l.Steps, l.Pipeline)
+}
+
+// Clone the plan
+func (l LogicalPlan) Clone() LogicalPlan {
+	steps := make(map[parser.TransformID]LogicalStep)
+	for id, step := range l.Steps {
+		steps[id] = step.Clone()
+	}
+
+	pipeline := make([]parser.TransformID, len(l.Pipeline))
+	copy(pipeline, l.Pipeline)
+	return LogicalPlan{
+		Steps:    steps,
+		Pipeline: pipeline,
+	}
+}
+
+func (l LogicalStep) String() string {
+	return fmt.Sprintf("Parents: %s, Children: %s, Transform: %s", l.Parents, l.Children, l.Transform)
+}
+
+// ID is a convenience method to expose the inner transforms' ID
+func (l LogicalStep) ID() parser.TransformID {
+	return l.Transform.ID
+}
+
+// Clone the step, the transform is immutable so its left as is
+func (l LogicalStep) Clone() LogicalStep {
+	parents := make([]parser.TransformID, len(l.Parents))
+	copy(parents, l.Parents)
+
+	children := make([]parser.TransformID, len(l.Children))
+	copy(children, l.Children)
+
+	return LogicalStep{
+		Transform: l.Transform,
+		Parents:   parents,
+		Children:  children,
+	}
 }
