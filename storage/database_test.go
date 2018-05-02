72763ff8c133950fd41e0abf39a739beb14f97bf
Richard Artoul <rartoul@uber.com>
2018-05-02 17:06:35 -0400

    Fix test timeout

--- storage/database_test.go
+++ storage/database_test.go
@@ -698,6 +698,7 @@ func TestDatabaseIndexEnabled(t *testing.T) {
 	ns := dbAddNewMockNamespace(ctrl, d, "testns")
 	ns.EXPECT().GetOwnedShards().Return([]databaseShard{}).AnyTimes()
 	ns.EXPECT().Tick(gomock.Any()).Return(nil).AnyTimes()
+	ns.EXPECT().BootstrapState().Return(ShardBootstrapStates{}).AnyTimes()
 	require.NoError(t, d.Open())
 
 	ctx := context.NewContext()
