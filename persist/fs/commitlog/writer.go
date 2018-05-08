32c5736ad5f9119ccd8cea10baa0286af63febb3
Richard Artoul <rartoul@uber.com>
2018-05-08 15:57:31 -0400

    Reorder imports

--- persist/fs/commitlog/writer.go
+++ persist/fs/commitlog/writer.go
@@ -27,9 +27,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/m3db/m3x/ident"
-
-	"github.com/m3db/bitset"
 	"github.com/m3db/m3db/clock"
 	"github.com/m3db/m3db/digest"
 	"github.com/m3db/m3db/persist/fs"
@@ -37,6 +34,9 @@ import (
 	"github.com/m3db/m3db/persist/schema"
 	"github.com/m3db/m3db/serialize"
 	"github.com/m3db/m3db/ts"
+
+	"github.com/m3db/bitset"
+	"github.com/m3db/m3x/ident"
 	xtime "github.com/m3db/m3x/time"
 )
 
