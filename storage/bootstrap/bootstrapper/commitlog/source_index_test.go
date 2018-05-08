41a3869b20ca7b4d69b53bbdb10438bd66ecc47b
Richard Artoul <rartoul@uber.com>
2018-05-08 19:27:02 -0400

    Working test

--- storage/bootstrap/bootstrapper/commitlog/source_index_test.go
+++ storage/bootstrap/bootstrapper/commitlog/source_index_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/m3db/m3db/persist/fs"
 	"github.com/m3db/m3db/persist/fs/commitlog"
 	"github.com/m3db/m3db/storage/bootstrap/result"
+	"github.com/m3db/m3db/storage/namespace"
 	"github.com/m3db/m3x/ident"
 	xtime "github.com/m3db/m3x/time"
 
@@ -34,22 +35,69 @@ import (
 )
 
 func TestBootstrapIndex(t *testing.T) {
-	opts := testOptions()
-	src := newCommitLogSource(opts, fs.Inspection{})
+	var (
+		opts             = testOptions()
+		src              = newCommitLogSource(opts, fs.Inspection{}).(*commitLogSource)
+		dataBlockSize    = 2 * time.Hour
+		indexBlockSize   = 4 * time.Hour
+		namespaceOptions = namespace.NewOptions().
+					SetRetentionOptions(
+				namespace.NewOptions().
+					RetentionOptions().
+					SetBlockSize(dataBlockSize),
+			).
+			SetIndexOptions(
+				namespace.NewOptions().
+					IndexOptions().
+					SetBlockSize(indexBlockSize),
+			)
+	)
+	md, err := namespace.NewMetadata(testNamespaceID, namespaceOptions)
+	require.NoError(t, err)
 
-	md := testNsMetadata(t)
-	blockSize := md.Options().RetentionOptions().BlockSize()
 	now := time.Now()
-	start := now.Truncate(blockSize).Add(-2 * blockSize)
+	start := now.Truncate(dataBlockSize).Add(-2 * dataBlockSize)
 
-	foo := commitlog.Series{Namespace: testNamespaceID, Shard: 0, ID: ident.StringID("foo")}
-	bar := commitlog.Series{Namespace: testNamespaceID, Shard: 1, ID: ident.StringID("bar")}
-	baz := commitlog.Series{Namespace: testNamespaceID, Shard: 2, ID: ident.StringID("baz")}
+	fooTags := ident.Tags{ident.StringTag("city", "ny"), ident.StringTag("conference", "monitoroma")}
+	barTags := ident.Tags{ident.StringTag("city", "sf")}
+	bazTags := ident.Tags{ident.StringTag("city", "oakland")}
+	foo := commitlog.Series{Namespace: testNamespaceID, Shard: 0, ID: ident.StringID("foo"), Tags: fooTags}
+	bar := commitlog.Series{Namespace: testNamespaceID, Shard: 1, ID: ident.StringID("bar"), Tags: barTags}
+	baz := commitlog.Series{Namespace: testNamespaceID, Shard: 2, ID: ident.StringID("baz"), Tags: bazTags}
 
 	values := []testValue{
 		{foo, start, 1.0, xtime.Second, nil},
+		{foo, start, 2.0, xtime.Second, nil},
+		{bar, start.Add(dataBlockSize), 1.0, xtime.Second, nil},
+		{bar, start.Add(dataBlockSize), 2.0, xtime.Second, nil},
+		{baz, start.Add(2 * dataBlockSize), 1.0, xtime.Second, nil},
+		{baz, start.Add(2 * dataBlockSize), 2.0, xtime.Second, nil},
 	}
 
-	_, err := src.ReadIndex(md, result.ShardTimeRanges{}, testDefaultRunOpts)
+	src.newIteratorFn = func(_ commitlog.IteratorOpts) (commitlog.Iterator, error) {
+		return newTestCommitLogIterator(values, nil), nil
+	}
+
+	ranges := xtime.Ranges{}
+	ranges = ranges.AddRange(xtime.Range{
+		Start: start.Add(-2 * dataBlockSize),
+		End:   start.Add(-dataBlockSize),
+	})
+	ranges = ranges.AddRange(xtime.Range{
+		Start: start.Add(-dataBlockSize),
+		End:   start,
+	})
+	ranges = ranges.AddRange(xtime.Range{
+		Start: start,
+		End:   start.Add(dataBlockSize),
+	})
+
+	targetRanges := result.ShardTimeRanges{0: ranges, 1: ranges, 2: ranges}
+	res, err := src.ReadIndex(md, targetRanges, testDefaultRunOpts)
 	require.NoError(t, err)
+
+	// Data blockSize is 2 hours and index blockSize is four hours so the data blocks
+	// will span two different index blocks.
+	require.Equal(t, 2, len(res.IndexResults()))
+	require.Equal(t, 0, len(res.Unfulfilled()))
 }
