8bc72af55da56691e5aebe1eba6f678f9865dc4a
Prateek Rungta <prateek@users.noreply.github.com>
2018-05-12 11:47:57 -0400

    Allow overrides on key/value type (#163)

--- src/m3x/generated-source-files.mk
+++ src/m3x/generated-source-files.mk
@@ -69,6 +69,8 @@ hashmap-gen-rename-helper:
 	[ "$(rename_constructor_options)" = "" ] || \
 	gorename -from '"$(target_package)$(temp_suffix)".MapOptions' -to '$(rename_constructor_options)'
 
+key_type_alias   ?= $(key_type)
+value_type_alias ?= $(value_type)
 .PHONY: hashmap-gen-rename
 hashmap-gen-rename: install-gorename
 	$(eval out_dir=$(gopath_prefix)/$(target_package))
@@ -77,18 +79,25 @@ hashmap-gen-rename: install-gorename
 	@if [ -d $(temp_outdir) ] ; then echo "temp directory $(temp_outdir) exists, failing" ; exit 1 ; fi
 	mkdir -p $(temp_outdir)
 	[ ! -f $(out_dir)/new_map_gen.go ] || mv $(out_dir)/new_map_gen.go $(temp_outdir)/new_map_gen.go
-ifeq ($(rename_nogen_types),)
-	# allow users to short circuit the generation of types.go if they don't need it.
-	echo 'package $(pkg)' > $(temp_outdir)/types.go
-	echo '' >> $(temp_outdir)/types.go
-	[[ $(value_type) = struct* ]] || echo 'type $(value_type) interface{}' >> $(temp_outdir)/types.go
-	[ "$(key_type)" == "" ] || echo "type $(key_type) interface{}" >> $(temp_outdir)/types.go
+ifeq ($(rename_nogen_key),)
+	# allow users to short circuit the generation of key.go if they don't need it.
+	echo 'package $(pkg)' > $(temp_outdir)/key.go
+	echo '' >> $(temp_outdir)/key.go
+	[ "$(key_type_alias)" == "" ] || echo "type $(key_type_alias) interface{}" >> $(temp_outdir)/key.go
+endif
+ifeq ($(rename_nogen_value),)
+	# allow users to short circuit the generation of key.go if they don't need it.
+	echo 'package $(pkg)' > $(temp_outdir)/value.go
+	echo '' >> $(temp_outdir)/value.go
+	[[ $(value_type_alias) = struct* ]] || echo 'type $(value_type_alias) interface{}' >> $(temp_outdir)/value.go
+	# allow users to short circuit the generation of value.go if they don't need it.
 endif
 	mv $(out_dir)/map_gen.go $(temp_outdir)/map_gen.go
 	make hashmap-gen-rename-helper
 	mv $(temp_outdir)/map_gen.go $(out_dir)/map_gen.go
 	[ ! -f $(temp_outdir)/new_map_gen.go ] || mv $(temp_outdir)/new_map_gen.go $(out_dir)/new_map_gen.go
-	rm -f $(temp_outdir)/types.go
+	rm -f $(temp_outdir)/key.go
+	rm -f $(temp_outdir)/value.go
 	rmdir $(temp_outdir)
 
 .PHONY: genny-arraypool-all
