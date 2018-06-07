6dccb6e66717fcb94a0db43f70e625564d2fdbc7
xichen2020 <xichen@uber.com>
2018-06-07 14:00:50 -0400

    Add metric and pipeline metadata (#140) (#165)

---
+++ src/metrics/op/applied/type.go
@@ -0,0 +1,152 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package applied
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/m3db/m3metrics/aggregation"
+	"github.com/m3db/m3metrics/op"
+)
+
+var (
+	// DefaultPipeline is a default pipeline.
+	DefaultPipeline Pipeline
+)
+
+// Rollup captures the rollup metadata after the operation is applied against a metric ID.
+type Rollup struct {
+	// Metric ID generated as a result of the rollup.
+	ID []byte
+	// Type of aggregations performed within each unique dimension combination.
+	AggregationID aggregation.ID
+}
+
+// Equal determines whether two rollup operations are equal.
+func (op Rollup) Equal(other Rollup) bool {
+	return op.AggregationID == other.AggregationID && bytes.Equal(op.ID, other.ID)
+}
+
+// Clone clones the rollup operation.
+func (op Rollup) Clone() Rollup {
+	idClone := make([]byte, len(op.ID))
+	copy(idClone, op.ID)
+	return Rollup{ID: idClone, AggregationID: op.AggregationID}
+}
+
+func (op Rollup) String() string {
+	return fmt.Sprintf("{id: %s, aggregation: %v}", op.ID, op.AggregationID)
+}
+
+// Union is a union of different types of operation.
+type Union struct {
+	Type           op.Type
+	Transformation op.Transformation
+	Rollup         Rollup
+}
+
+// Equal determines whether two operation unions are equal.
+func (u Union) Equal(other Union) bool {
+	if u.Type != other.Type {
+		return false
+	}
+	switch u.Type {
+	case op.TransformationType:
+		return u.Transformation.Equal(other.Transformation)
+	case op.RollupType:
+		return u.Rollup.Equal(other.Rollup)
+	}
+	return true
+}
+
+// Clone clones an operation union.
+func (u Union) Clone() Union {
+	clone := Union{Type: u.Type}
+	switch u.Type {
+	case op.TransformationType:
+		clone.Transformation = u.Transformation.Clone()
+	case op.RollupType:
+		clone.Rollup = u.Rollup.Clone()
+	}
+	return clone
+}
+
+func (u Union) String() string {
+	var b bytes.Buffer
+	b.WriteString("{")
+	switch u.Type {
+	case op.TransformationType:
+		fmt.Fprintf(&b, "transformation: %s", u.Transformation.String())
+	case op.RollupType:
+		fmt.Fprintf(&b, "rollup: %s", u.Rollup.String())
+	default:
+		fmt.Fprintf(&b, "unknown op type: %v", u.Type)
+	}
+	b.WriteString("}")
+	return b.String()
+}
+
+// Pipeline is a pipeline of operations.
+type Pipeline struct {
+	// a list of pipeline operations.
+	Operations []Union
+}
+
+// IsEmpty determines whether a pipeline is empty.
+func (p Pipeline) IsEmpty() bool {
+	return len(p.Operations) == 0
+}
+
+// Equal determines whether two pipelines are equal.
+func (p Pipeline) Equal(other Pipeline) bool {
+	if len(p.Operations) != len(other.Operations) {
+		return false
+	}
+	for i := 0; i < len(p.Operations); i++ {
+		if !p.Operations[i].Equal(other.Operations[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Clone clones the pipeline.
+func (p Pipeline) Clone() Pipeline {
+	clone := make([]Union, len(p.Operations))
+	for i, op := range p.Operations {
+		clone[i] = op.Clone()
+	}
+	return Pipeline{Operations: clone}
+}
+
+func (p Pipeline) String() string {
+	var b bytes.Buffer
+	b.WriteString("{operations: [")
+	for i, op := range p.Operations {
+		b.WriteString(op.String())
+		if i < len(p.Operations)-1 {
+			b.WriteString(", ")
+		}
+	}
+	b.WriteString("]}")
+	return b.String()
+}
