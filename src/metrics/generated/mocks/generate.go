a556cd0852ee5965e37ef4813e955f97bd02f57a
xichen2020 <xichen@uber.com>
2018-06-09 21:27:47 -0400

    Generate mock for matcher and ID (#180)

--- src/metrics/generated/mocks/generate.go
+++ src/metrics/generated/mocks/generate.go
@@ -19,7 +19,9 @@
 // THE SOFTWARE.
 
 // mockgen rules for generating mocks for exported interfaces (reflection mode).
-//go:generate sh -c "mockgen -package=rules $PACKAGE/rules Store| mockclean -pkg $PACKAGE/rules -out $GOPATH/src/$PACKAGE/rules/store_mock.go"
+//go:generate sh -c "mockgen -package=id $PACKAGE/metric/id ID | mockclean -pkg $PACKAGE/metric/id -out $GOPATH/src/$PACKAGE/metric/id/id_mock.go"
+//go:generate sh -c "mockgen -package=matcher $PACKAGE/matcher Matcher | mockclean -pkg $PACKAGE/matcher -out $GOPATH/src/$PACKAGE/matcher/matcher_mock.go"
 //go:generate sh -c "mockgen -package=protobuf $PACKAGE/encoding/protobuf UnaggregatedEncoder | mockclean -pkg $PACKAGE/encoding/protobuf -out $GOPATH/src/$PACKAGE/encoding/protobuf/protobuf_mock.go"
+//go:generate sh -c "mockgen -package=rules $PACKAGE/rules Store| mockclean -pkg $PACKAGE/rules -out $GOPATH/src/$PACKAGE/rules/store_mock.go"
 
 package mocks
