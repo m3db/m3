c11920065ccc4af612a0a2ff296a0690107f29ed
Richard Artoul <rartoul@uber.com>
2018-07-16 21:23:17 -0400

    working prop test

--- src/dbnode/integration/fs_commitlog_snapshot_mixed_mode_read_write_prop_test.go
+++ src/dbnode/integration/fs_commitlog_snapshot_mixed_mode_read_write_prop_test.go
@@ -23,6 +23,7 @@
 package integration
 
 import (
+	"os"
 	"testing"
 	"time"
 
@@ -31,8 +32,13 @@ import (
 	"github.com/m3db/m3x/context"
 	xtime "github.com/m3db/m3x/time"
 
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
 	"github.com/stretchr/testify/require"
 )
+
+const maxBlockSize = 12 * time.Hour
 
 func TestFsCommitLogMixedModeReadWriteProp(t *testing.T) {
 	testMixedModeReadWriteProp(t, false)
@@ -42,80 +48,130 @@ func testMixedModeReadWriteProp(t *testing.T, snapshotEnabled bool) {
 	if testing.Short() {
 		t.SkipNow() // Just skip if we're doing a short run
 	}
-	// Test setup
 	var (
-		ns1BlockSize = 1 * time.Hour
-		// setting time to 2017/02/13 15:30:10
-		fakeStart          = time.Date(2017, time.February, 13, 15, 30, 10, 0, time.Local)
-		blkStart15         = fakeStart.Truncate(ns1BlockSize)
-		blkStart16         = blkStart15.Add(ns1BlockSize)
-		blkStart17         = blkStart16.Add(ns1BlockSize)
-		blkStart18         = blkStart17.Add(ns1BlockSize)
-		timesToCheck       = []time.Time{blkStart15, blkStart16, blkStart17, blkStart18}
-		commitLogBlockSize = 15 * time.Minute
-		commitLogRetetion  = 6 * time.Hour
-		bufferPast         = 5 * time.Minute
-		bufferFuture       = 10 * time.Minute
-		ns1ROpts           = retention.NewOptions().
-					SetRetentionPeriod(10 * time.Hour).
-					SetBlockSize(ns1BlockSize).
-					SetBufferPast(bufferPast).
-					SetBufferFuture(bufferFuture)
-		nsID       = testNamespaces[0]
-		numMinutes = 200
+		parameters = gopter.DefaultTestParameters()
+		seed       = time.Now().UnixNano()
+		props      = gopter.NewProperties(parameters)
+		reporter   = gopter.NewFormatedReporter(true, 160, os.Stdout)
+		fakeStart  = time.Date(2017, time.February, 13, 15, 30, 10, 0, time.Local)
 	)
 
-	ns1Opts := namespace.NewOptions().
-		SetRetentionOptions(ns1ROpts).
-		SetSnapshotEnabled(snapshotEnabled)
-	ns1, err := namespace.NewMetadata(nsID, ns1Opts)
-	require.NoError(t, err)
-	opts := newTestOptions(t).
-		SetCommitLogRetentionPeriod(commitLogRetetion).
-		SetCommitLogBlockSize(commitLogBlockSize).
-		SetNamespaces([]namespace.Metadata{ns1})
+	parameters.MinSuccessfulTests = 10
+	parameters.Rng.Seed(seed)
 
-	// Test setup
-	setup := newTestSetupWithCommitLogAndFilesystemBootstrapper(t, opts)
-	defer setup.close()
-
-	log := setup.storageOpts.InstrumentOptions().Logger()
-	log.Info("commit log & fileset files, write, read, and merge bootstrap test")
-
-	setup.setNowFn(fakeStart)
-
-	var (
-		ids        = &idGen{longTestID}
-		datapoints = generateDatapoints(fakeStart, numMinutes, ids)
-		lastIdx    = 0
-	)
-	for _, timeToCheck := range timesToCheck {
-		startServerWithNewInspection(t, opts, setup)
-		var (
-			ctx = context.NewContext()
-		)
-		defer ctx.Close()
-
-		log.Infof("writing datapoints")
-		for i := lastIdx; ; i++ {
+	props.Property("Blah", prop.ForAll(
+		func(input propTestInput) (bool, error) {
+			// Test setup
 			var (
-				dp = datapoints[i]
-				ts = dp.time
+				ns1BlockSize = input.blockSize
+				// setting time to 2017/02/13 15:30:10
+				blkStart15         = fakeStart.Truncate(ns1BlockSize)
+				blkStart16         = blkStart15.Add(ns1BlockSize)
+				blkStart17         = blkStart16.Add(ns1BlockSize)
+				blkStart18         = blkStart17.Add(ns1BlockSize)
+				timesToCheck       = []time.Time{blkStart15, blkStart16, blkStart17, blkStart18}
+				commitLogBlockSize = 15 * time.Minute
+				// TODO: Vary this?
+				retentionPeriod = maxBlockSize * 5
+				bufferPast      = input.bufferPast
+				bufferFuture    = input.bufferFuture
+				ns1ROpts        = retention.NewOptions().
+						SetRetentionPeriod(retentionPeriod).
+						SetBlockSize(ns1BlockSize).
+						SetBufferPast(bufferPast).
+						SetBufferFuture(bufferFuture)
+				nsID       = testNamespaces[0]
+				numMinutes = 200
 			)
-			if !ts.Before(timeToCheck) {
-				lastIdx = i
-				break
+
+			if bufferPast > ns1BlockSize {
+				ns1ROpts = ns1ROpts.SetBufferPast(ns1BlockSize - 1)
+			}
+			if bufferFuture > ns1BlockSize {
+				ns1ROpts = ns1ROpts.SetBufferFuture(ns1BlockSize - 1)
 			}
 
-			setup.setNowFn(ts)
-			require.NoError(t, setup.db.Write(ctx, nsID, dp.series, ts, dp.value, xtime.Second, nil))
-		}
-		log.Infof("wrote datapoints")
+			ns1Opts := namespace.NewOptions().
+				SetRetentionOptions(ns1ROpts).
+				SetSnapshotEnabled(snapshotEnabled)
+			ns1, err := namespace.NewMetadata(nsID, ns1Opts)
+			require.NoError(t, err)
+			opts := newTestOptions(t).
+				SetCommitLogRetentionPeriod(retentionPeriod).
+				SetCommitLogBlockSize(commitLogBlockSize).
+				SetNamespaces([]namespace.Metadata{ns1})
 
-		expectedSeriesMap := datapoints[:lastIdx].toSeriesMap(ns1BlockSize)
-		log.Infof("verifying data in database equals expected data")
-		verifySeriesMaps(t, setup, nsID, expectedSeriesMap)
-		log.Infof("verified data in database equals expected data")
-		require.NoError(t, setup.stopServer())
+			// Test setup
+			setup := newTestSetupWithCommitLogAndFilesystemBootstrapper(t, opts)
+			defer setup.close()
+
+			log := setup.storageOpts.InstrumentOptions().Logger()
+			log.Info("commit log & fileset files, write, read, and merge bootstrap test")
+
+			setup.setNowFn(fakeStart)
+
+			var (
+				ids        = &idGen{longTestID}
+				datapoints = generateDatapoints(fakeStart, numMinutes, ids)
+				lastIdx    = 0
+			)
+			for _, timeToCheck := range timesToCheck {
+				startServerWithNewInspection(t, opts, setup)
+				var (
+					ctx = context.NewContext()
+				)
+				defer ctx.Close()
+
+				log.Infof("writing datapoints")
+				for i := lastIdx; i < len(datapoints); i++ {
+					var (
+						dp = datapoints[i]
+						ts = dp.time
+					)
+					if !ts.Before(timeToCheck) {
+						lastIdx = i
+						break
+					}
+
+					setup.setNowFn(ts)
+					require.NoError(t, setup.db.Write(ctx, nsID, dp.series, ts, dp.value, xtime.Second, nil))
+				}
+				log.Infof("wrote datapoints")
+
+				expectedSeriesMap := datapoints[:lastIdx].toSeriesMap(ns1BlockSize)
+				log.Infof("verifying data in database equals expected data")
+				verifySeriesMaps(t, setup, nsID, expectedSeriesMap)
+				log.Infof("verified data in database equals expected data")
+				require.NoError(t, setup.stopServer())
+			}
+			return true, nil
+		}, genPropTestInputs(fakeStart),
+	))
+
+	if !props.Run(reporter) {
+		t.Errorf(
+			"failed with initial seed: %d and startTime: %d",
+			seed, fakeStart.UnixNano())
 	}
+}
+
+func genPropTestInputs(blockStart time.Time) gopter.Gen {
+	return gopter.CombineGens(
+		gen.Int64Range(1, int64(maxBlockSize/2)*2),
+		gen.Int64Range(1, int64(maxBlockSize/2)*2),
+		gen.Int64Range(1, int64(maxBlockSize/2)*2),
+	).Map(func(val interface{}) propTestInput {
+		inputs := val.([]interface{})
+		return propTestInput{
+			blockSize:    time.Duration(inputs[0].(int64)),
+			bufferPast:   time.Duration(inputs[1].(int64)),
+			bufferFuture: time.Duration(inputs[2].(int64)),
+		}
+	})
+}
+
+type propTestInput struct {
+	blockSize    time.Duration
+	bufferPast   time.Duration
+	bufferFuture time.Duration
 }
