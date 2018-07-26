5eece2bac4247b67f0ff9bffb4796186af2d478c
Rob Skillington <rob.skillington@gmail.com>
2018-07-26 19:00:53 -0400

    Add UnmarshalYAML for DropPolicy type (#200)

--- src/metrics/metadata/metadata_test.go
+++ src/metrics/metadata/metadata_test.go
@@ -1087,8 +1087,8 @@ func TestVersionedStagedMetadatasMarshalJSON(t *testing.T) {
 		`{"version":12,` +
 			`"stagedMetadatas":` +
 			`[{"metadata":{"pipelines":[` +
-			`{"aggregation":["Sum"],"storagePolicies":["1s:1h","1m:12h"],"dropPolicy":0},` +
-			`{"aggregation":null,"storagePolicies":["10s:1h"],"dropPolicy":0}]},` +
+			`{"aggregation":["Sum"],"storagePolicies":["1s:1h","1m:12h"]},` +
+			`{"aggregation":null,"storagePolicies":["10s:1h"]}]},` +
 			`"cutoverNanos":4567,` +
 			`"tombstoned":true}]}`
 	require.Equal(t, expected, string(res))
