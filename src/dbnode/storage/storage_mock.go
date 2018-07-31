846a7a61aa1d8f5bb2ab56e7be9f8d91d0f009a5
Rob Skillington <r@uber.com>
2018-07-31 11:41:49 -0400

    Fix tests

--- src/dbnode/storage/storage_mock.go
+++ src/dbnode/storage/storage_mock.go
@@ -2546,18 +2546,6 @@ func (mr *MockOptionsMockRecorder) PersistManager() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PersistManager", reflect.TypeOf((*MockOptions)(nil).PersistManager))
 }
 
-// SetMaxFlushRetries mocks base method
-func (m *MockOptions) SetMaxFlushRetries(value int) Options {
-	ret := m.ctrl.Call(m, "SetMaxFlushRetries", value)
-	ret0, _ := ret[0].(Options)
-	return ret0
-}
-
-// SetMaxFlushRetries indicates an expected call of SetMaxFlushRetries
-func (mr *MockOptionsMockRecorder) SetMaxFlushRetries(value interface{}) *gomock.Call {
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetMaxFlushRetries", reflect.TypeOf((*MockOptions)(nil).SetMaxFlushRetries), value)
-}
-
 // SetMinimumSnapshotInterval mocks base method
 func (m *MockOptions) SetMinimumSnapshotInterval(value time.Duration) Options {
 	ret := m.ctrl.Call(m, "SetMinimumSnapshotInterval", value)
