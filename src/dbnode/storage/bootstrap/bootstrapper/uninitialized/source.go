1d0e220c9359dda58549b90de26a1b07f7ceb333
Richard Artoul <rartoul@uber.com>
2018-09-20 11:41:08 -0400

    Refactor uninitialized bootstrapper AvailableData and AvailableIndex to return errors

--- src/dbnode/storage/bootstrap/bootstrapper/uninitialized/source.go
+++ src/dbnode/storage/bootstrap/bootstrapper/uninitialized/source.go
@@ -21,6 +21,9 @@
 package uninitialized
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/m3db/m3/src/dbnode/storage/bootstrap"
 	"github.com/m3db/m3/src/dbnode/storage/bootstrap/result"
 	"github.com/m3db/m3/src/dbnode/storage/namespace"
@@ -61,7 +64,7 @@ func (s *uninitializedTopologySource) AvailableData(
 	ns namespace.Metadata,
 	shardsTimeRanges result.ShardTimeRanges,
 	runOpts bootstrap.RunOptions,
-) result.ShardTimeRanges {
+) (result.ShardTimeRanges, error) {
 	return s.availability(ns, shardsTimeRanges, runOpts)
 }
 
@@ -69,7 +72,7 @@ func (s *uninitializedTopologySource) AvailableIndex(
 	ns namespace.Metadata,
 	shardsTimeRanges result.ShardTimeRanges,
 	runOpts bootstrap.RunOptions,
-) result.ShardTimeRanges {
+) (result.ShardTimeRanges, error) {
 	return s.availability(ns, shardsTimeRanges, runOpts)
 }
 
@@ -77,7 +80,7 @@ func (s *uninitializedTopologySource) availability(
 	ns namespace.Metadata,
 	shardsTimeRanges result.ShardTimeRanges,
 	runOpts bootstrap.RunOptions,
-) result.ShardTimeRanges {
+) (result.ShardTimeRanges, error) {
 	var (
 		topoState                = runOpts.InitialTopologyState()
 		availableShardTimeRanges = result.ShardTimeRanges{}
@@ -121,8 +124,9 @@ func (s *uninitializedTopologySource) availability(
 			default:
 				// TODO(rartoul): Make this a hard error once we refactor the interface to support
 				// returning errors.
-				s.opts.InstrumentOptions().Logger().Errorf("unknown shard state: %v", shardState)
-				return result.ShardTimeRanges{}
+				errMsg := fmt.Sprintf("unknown shard state: %v", shardState)
+				s.opts.InstrumentOptions().Logger().Error(errMsg)
+				return nil, errors.New(errMsg)
 			}
 		}
 
@@ -138,7 +142,7 @@ func (s *uninitializedTopologySource) availability(
 		}
 	}
 
-	return availableShardTimeRanges
+	return availableShardTimeRanges, nil
 }
 
 func (s *uninitializedTopologySource) ReadData(
@@ -146,10 +150,12 @@ func (s *uninitializedTopologySource) ReadData(
 	shardsTimeRanges result.ShardTimeRanges,
 	runOpts bootstrap.RunOptions,
 ) (result.DataBootstrapResult, error) {
-	var (
-		availability = s.availability(ns, shardsTimeRanges, runOpts)
-		missing      = shardsTimeRanges.Copy()
-	)
+	availability, err := s.availability(ns, shardsTimeRanges, runOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	missing := shardsTimeRanges.Copy()
 	missing.Subtract(availability)
 
 	if missing.IsEmpty() {
@@ -164,10 +170,12 @@ func (s *uninitializedTopologySource) ReadIndex(
 	shardsTimeRanges result.ShardTimeRanges,
 	runOpts bootstrap.RunOptions,
 ) (result.IndexBootstrapResult, error) {
-	var (
-		availability = s.availability(ns, shardsTimeRanges, runOpts)
-		missing      = shardsTimeRanges.Copy()
-	)
+	availability, err := s.availability(ns, shardsTimeRanges, runOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	missing := shardsTimeRanges.Copy()
 	missing.Subtract(availability)
 
 	if missing.IsEmpty() {
