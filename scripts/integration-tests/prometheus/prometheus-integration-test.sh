dbf1810d13f06ae941f40cdcfbd926dab13988c6
Rob Skillington <r@uber.com>
2018-09-21 10:52:40 -0400

    Paramaterize Prometheus integration test to use for local testing

--- scripts/integration-tests/prometheus/prometheus-integration-test.sh
+++ scripts/integration-tests/prometheus/prometheus-integration-test.sh
@@ -5,9 +5,16 @@ set -xe
 rm -rf /tmp/m3dbdata/
 mkdir -p /tmp/m3dbdata/
 
-echo "Build docker images"
+PARAM_TEST_BUILD="${TEST_BUILD:-true}"
+PARAM_TEST_VERIFY="${TEST_VERIFY:-true}"
+PARAM_TEST_TEARDOWN="${TEST_TEARDOWN:-true}"
 
-docker-compose -f docker-compose.yml build
+if [ $PARAM_TEST_BUILD != "true" ]; do
+  echo "SKIP build docker images"
+else
+  echo "Build docker images"
+  docker-compose -f docker-compose.yml build
+fi
 
 echo "Run m3dbnode and m3coordinator containers"
 
@@ -78,56 +85,61 @@ echo "Start Prometheus container"
 
 docker-compose -f docker-compose.yml up -d prometheus01
 
-sleep 10
-
-echo "Write data"
-
-curl -vvvsSf -X POST localhost:9003/writetagged -d '{
-  "namespace": "prometheus_metrics",
-  "id": "foo",
-  "tags": [
-    {
-      "name": "city",
-      "value": "new_york"
-    },
-    {
-      "name": "endpoint",
-      "value": "/request"
-    }
-  ],
-  "datapoint": {
-    "timestamp":'"$(date +"%s")"',
-    "value": 42.123456789
-  }
-}'
-
-echo "Read data"
-
-queryResult=$(curl -sSf -X POST localhost:9003/query -d '{
-  "namespace": "prometheus_metrics",
-  "query": {
-    "regexp": {
-      "field": "city",
-      "regexp": ".*"
-    }
-  },
-  "rangeStart": 0,
-  "rangeEnd":'"$(date +"%s")"'
-}' | jq '.results | length')
-
-if [ "$queryResult" -lt 1 ]; then
-  echo "Result not found"
-  exit 1
+if [ $PARAM_TEST_VERIFY != "true" ]; do
+  echo "SKIP verify"
 else
-  echo "Result found"
+  echo "Write direct test data"
+
+  curl -vvvsSf -X POST localhost:9003/writetagged -d '{
+    "namespace": "prometheus_metrics",
+    "id": "foo",
+    "tags": [
+      {
+        "name": "city",
+        "value": "new_york"
+      },
+      {
+        "name": "endpoint",
+        "value": "/request"
+      }
+    ],
+    "datapoint": {
+      "timestamp":'"$(date +"%s")"',
+      "value": 42.123456789
+    }
+  }'
+
+  echo "Read direct test data"
+
+  queryResult=$(curl -sSf -X POST localhost:9003/query -d '{
+    "namespace": "prometheus_metrics",
+    "query": {
+      "regexp": {
+        "field": "city",
+        "regexp": ".*"
+      }
+    },
+    "rangeStart": 0,
+    "rangeEnd":'"$(date +"%s")"'
+  }' | jq '.results | length')
+
+  if [ "$queryResult" -lt 1 ]; then
+    echo "Result not found"
+    exit 1
+  else
+    echo "Result found"
+  fi
+
+  echo "Sleep for 30 seconds to let the remote write endpoint generate some data"
+
+  sleep 30
+
+  # Ensure Prometheus can proxy a Prometheus query
+  [ "$(curl -sSf localhost:9090/api/v1/query?query=prometheus_remote_storage_succeeded_samples_total | jq .data.result[].value[1])" != '"0"' ]
 fi
 
-echo "Sleep for 30 seconds to let the remote write endpoint generate some data"
-
-sleep 30
-
-# Ensure Prometheus can proxy a Prometheus query
-[ "$(curl -sSf localhost:9090/api/v1/query?query=prometheus_remote_storage_succeeded_samples_total | jq .data.result[].value[1])" != '"0"' ]
-
-
-docker-compose -f docker-compose.yml down || echo "unable to shutdown containers" # CI fails to stop all containers sometimes
+if [ $PARAM_TEST_TEARDOWN != "true" ]; do
+  echo "SKIP teardown"
+else
+  docker-compose -f docker-compose.yml down || echo "unable to shutdown containers" # CI fails to stop all containers sometimes
+fi
