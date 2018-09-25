1508061d1f5be7a741ec830a7781db88d7b1b69d
Richard Artoul <rartoul@uber.com>
2018-09-25 13:59:13 -0400

    Add m3_stack

---
+++ scripts/development/m3_stack/README.md
@@ -0,0 +1,8 @@
+# Local Development
+
+This docker-compose file will setup the following environment:
+
+1. 3 M3DB nodes with a single node acting as an EtcD seed
+2. 1 M3Coordinator node
+3. 1 Grafana node (with a pre-configured Prometheus source)
+4. 1 Prometheus node that scrapes the M3DB/M3Coordinator nodes and writes the metrics to M3Coordinator
---
+++ scripts/development/m3_stack/start.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+set -xe
+
+echo "Bringing up nodes in the backgorund with docker compose, remember to run ./stop.sh when done"
+docker-compose -f docker-compose.yml up -d --renew-anon-volumes
+echo "Nodes online"
+
+echo "Initializing namespace"
+curl -vvvsSf -X POST localhost:7201/api/v1/namespace -d '{
+  "name": "prometheus_metrics",
+  "options": {
+    "bootstrapEnabled": true,
+    "flushEnabled": true,
+    "writesToCommitLog": true,
+    "cleanupEnabled": true,
+    "snapshotEnabled": true,
+    "repairEnabled": false,
+    "retentionOptions": {
+      "retentionPeriodNanos": 172800000000000,
+      "blockSizeNanos": 7200000000000,
+      "bufferFutureNanos": 600000000000,
+      "bufferPastNanos": 600000000000,
+      "blockDataExpiry": true,
+      "blockDataExpiryAfterNotAccessPeriodNanos": 300000000000
+    },
+    "indexOptions": {
+      "enabled": true,
+      "blockSizeNanos": 7200000000000
+    }
+  }
+}'
+echo "Done initializing namespace"
+
+echo "Initializing topology"
+curl -vvvsSf -X POST localhost:7201/api/v1/placement/init -d '{
+    "num_shards": 64,
+    "replication_factor": 3,
+    "instances": [
+        {
+            "id": "m3db_seed",
+            "isolation_group": "rack-a",
+            "zone": "embedded",
+            "weight": 1024,
+            "endpoint": "m3db_seed:9000",
+            "hostname": "m3db_seed",
+            "port": 9000
+        },
+        {
+            "id": "m3db_data01",
+            "isolation_group": "rack-b",
+            "zone": "embedded",
+            "weight": 1024,
+            "endpoint": "m3db_data01:9000",
+            "hostname": "m3db_data01",
+            "port": 9000
+        },
+        {
+            "id": "m3db_data02",
+            "isolation_group": "rack-c",
+            "zone": "embedded",
+            "weight": 1024,
+            "endpoint": "m3db_data02:9000",
+            "hostname": "m3db_data02",
+            "port": 9000
+        }
+    ]
+}'
+echo "Done initializing topology"
+
+echo "Prometheus available at localhost:9090"
+echo "Grafana available at localhost:3000"
+echo "Run ./stop.sh to shutdown nodes when done"
