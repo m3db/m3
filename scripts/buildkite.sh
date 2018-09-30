d1dae7dbfb508d6839afe726eba34e179fbe071d
Prateek Rungta <prungta2@gmail.com>
2018-09-30 03:50:58 -0400

    wip

--- scripts/buildkite.sh
+++ scripts/buildkite.sh
@@ -4,14 +4,7 @@ set -x
 
 git submodule update --init
 
-export CGO_ENABLED=0
-echo $PATH
-eval "$(.ci/gimme.sh 1.10.x)"
-echo $PATH
-
 which go
-
-go get -u github.com/twitchtv/retool
 
 make install-ci
 
