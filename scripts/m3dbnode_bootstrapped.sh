4c1970a931593cc8a23ec5c47bab410f8e177b80
Matt Schallert <mattschallert@gmail.com>
2018-10-01 11:27:51 -0400

    use dbnode health endpoint after coord check

--- scripts/m3dbnode_bootstrapped.sh
+++ scripts/m3dbnode_bootstrapped.sh
@@ -2,49 +2,43 @@
 
 # Exits with code 0 if either:
 # - There is no placement
-# - The current host is not in the placement
-# - The current is in the placement AND all it's shards are AVAILABLE
+# - The current host reports that it is bootstrapped.
 #
 # Exits 1 otherwise.
 
 set -exo pipefail
 
 COORDINATOR_PORT=${COORDINATOR_PORT:-7201}
-ENDPOINT="http://localhost:${COORDINATOR_PORT}/api/v1/placement"
+DBNODE_PORT=${DBNODE_PORT:-9004}
+COORD_ENDPOINT="http://localhost:${COORDINATOR_PORT}/api/v1/placement"
+DBNODE_ENDPOINT="http://localhost:${DBNODE_PORT}:9004/"
+
 HOSTNAME=${HOSTNAME:-$(hostname)}
 
-TMPFILE=$(mktemp)
+COORD_TMPFILE=$(mktemp)
+DBNODE_TMPFILE=$(mktemp)
 
 function cleanup() {
-  rm -f "$TMPFILE"
+  rm -f "$COORD_TMPFILE" "$DBNODE_TMPFILE"
 }
 
 trap cleanup EXIT
 
-curl -sSf -o "$TMPFILE" "$ENDPOINT"
+curl -sSf -o "$COORD_TMPFILE" "$COORD_ENDPOINT"
 RES=$?
 
 # Curl exits 22 for 400+ status code. Note this leaves us vulnerable to saying
 # bootstrapped if our script makes a bad request and must use caution when
 # modifying the script or the coordinator placement endpoint.
 if [[ "$RES" -eq 22 ]]; then
-  echo "Received 400"
+  echo "Received 4xx from coordinator"
   exit 0
 fi
 
-# TODO(schallert): should a leaving node be considered bootstrapped?
-TOTAL=$(jq ".placement.instances | .[\"${HOSTNAME}\"] | .shards | length" < "$TMPFILE")
-
-if [[ "$TOTAL" -eq 0 ]]; then
-  # Host is not in placement.
-  echo "$HOSTNAME not found in placement"
-  exit 0
-fi
-
-AVAILABLE=$(jq ".placement.instances | .[\"${HOSTNAME}\"] | .shards | map(select(.state == \"AVAILABLE\")) | length" < "$TMPFILE")
-
-if [[ "$AVAILABLE" -ne "$TOTAL" ]]; then
-  echo "Available shards not equal total ($AVAILABLE != $TOTAL)"
+curl -sSf -o "$DBNODE_TMPFILE" "$DBNODE_ENDPOINT"
+BOOTSTRAPPED=$(jq .bootstrapped < "$DBNODE_TMPFILE")
+if [[ "$BOOTSTRAPPED" != "true" ]]; then
+  echo "Not bootstrapped ($BOOTSTRAPPED)"
   exit 1
 fi
 
