d2be044c6fabe4179fbb4db72cb6ca2ef446456d
Benjamin Raskin <braskin@uber.com>
2018-10-04 16:08:05 -0400

    Address comments

--- docs/how_to/single_node.md
+++ docs/how_to/single_node.md
@@ -16,7 +16,7 @@ directory on your host for durability:
 
 ```
 docker pull quay.io/m3/m3dbnode:latest
-docker run -p 7201:7201 -p 9003:9003 --name m3db -v $(pwd)/m3db_data:/var/lib/m3db quay.io/m3/m3dbnode:latest
+docker run -p 7201:7201 -p 9003:9003 --name m3db quay.io/m3/m3dbnode:latest
 ```
 
 <!-- TODO: link to docs containing explanations of what namespaces, the coordinator,
@@ -59,7 +59,7 @@ going to `localhost:7201/api/v1/openapi` in your browser.
 
 Now you can experiment with writing tagged metrics:
 ```json
-curl -sSf -X POST http://localhost:9003/writetagged -d '{
+curl -vvvsS -X POST http://localhost:9003/writetagged -d '{
   "namespace": "default",
   "id": "foo",
   "tags": [
--- docs/how_to/single_node.sh
+++ docs/how_to/single_node.sh
@@ -4,13 +4,33 @@ set -xe
 
 source $GOPATH/src/github.com/m3db/m3/scripts/docker-integration-tests/common.sh
 
-docker pull quay.io/m3/m3dbnode:latest
-docker run -p 7201:7201 -p 9003:9003 --name m3db -v $(pwd)/m3db_data:/var/lib/m3db quay.io/m3/m3dbnode:latest
+REVISION=$(git rev-parse HEAD)
 
-sleep 15
+CONTAINER_NAME="m3dbnode-version-${REVISION}"
+
+# think of this as a defer func() in golang
+function defer {
+  echo "Remove docker container"
+  docker rm --force "${CONTAINER_NAME}"
+}
+trap defer EXIT
+
+docker pull quay.io/m3/m3dbnode:latest
+docker run -p 7201:7201 -p 9003:9003 --name m3db quay.io/m3/m3dbnode:latest
+
+docker pull quay.io/m3/m3dbnode:latest
+docker create --name "${CONTAINER_NAME}" -p 7201:7201 -p 9003:9003 -p 9002:9002 quay.io/m3/m3dbnode:latest
+docker start "${CONTAINER_NAME}"
+if [ $? -ne 0 ]; then
+  echo "m3dbnode docker failed to start"
+  docker logs "${CONTAINER_NAME}"
+  exit 1
+fi
+
+sleep 5
 echo "checking uptime of coordinator service"
-ATTEMPTS=4 TIMEOUT=1 retry_with_backoff  \
-  '[ "$(curl -sSf 0.0.0.0:7201/health | jq 'has("uptime")')" == true ]'
+ATTEMPTS=6 TIMEOUT=1 retry_with_backoff  \
+  '[ "$(curl -sSf 0.0.0.0:7201/health | jq .uptime)" ]'
 
 curl -X POST http://localhost:7201/api/v1/database/create -d '{
   "type": "local",
@@ -18,12 +38,15 @@ curl -X POST http://localhost:7201/api/v1/database/create -d '{
   "retentionTime": "48h"
 }'
 
-sleep 15
 echo "check placement info"
-ATTEMPTS=4 TIMEOUT=1 retry_with_backoff  \
-  '[ "$(curl http://localhost:7201/api/v1/placement | jq .placement.instances.m3db_local.id)" == "m3db_local" ]'
+ATTEMPTS=6 TIMEOUT=1 retry_with_backoff  \
+  '[ "$(curl -sSf 0.0.0.0:7201/api/v1/placement | jq .placement.instances.m3db_local.id)" == \"m3db_local\" ]'
 
-curl -sSf -X POST http://localhost:9003/writetagged -d '{
+echo "sleep until bootstrapped"
+ATTEMPTS=6 TIMEOUT=2 retry_with_backoff  \
+  '[ "$(curl -sSf 0.0.0.0:9002/health | jq .bootstrapped)" == true ]'
+
+curl -vvvsS -X POST http://localhost:9003/writetagged -d '{
   "namespace": "default",
   "id": "foo",
   "tags": [
@@ -56,7 +79,7 @@ curl -sSf -X POST http://localhost:9003/query -d '{
 }' | jq .
 
 echo "Read data"
-queryResult=$(curl -sSf -X POST 0.0.0.0:9003/query -d '{
+queryResult=$(curl -sSf -X POST localhost:9003/query -d '{
   "namespace": "default",
   "query": {
     "regexp": {
