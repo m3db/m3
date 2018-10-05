9c42b2ffc8b20bc7fdcd6191dc70e8f62fd3a608
Richard Artoul <rartoul@uber.com>
2018-10-05 19:00:09 -0400

    rewrite snapshot integration tests

--- src/dbnode/integration/disk_snapshot_test.go
+++ src/dbnode/integration/disk_snapshot_test.go
@@ -39,11 +39,25 @@ func TestDiskSnapshotSimple(t *testing.T) {
 		t.SkipNow() // Just skip if we're doing a short run
 	}
 	// Test setup
-	nOpts := namespace.NewOptions().SetSnapshotEnabled(true)
+	var (
+		nOpts = namespace.NewOptions().
+			SetSnapshotEnabled(true)
+		bufferPast   = 50 * time.Minute
+		bufferFuture = 50 * time.Minute
+		blockSize    = time.Hour
+	)
+
+	nOpts = nOpts.
+		SetRetentionOptions(nOpts.RetentionOptions().
+			SetBufferFuture(bufferFuture).
+			SetBufferPast(bufferPast).
+			SetBlockSize(blockSize))
 	md1, err := namespace.NewMetadata(testNamespaces[0], nOpts)
 	require.NoError(t, err)
 	md2, err := namespace.NewMetadata(testNamespaces[1], nOpts)
 	require.NoError(t, err)
+
+	// md1.SetOptions()
 
 	testOpts := newTestOptions(t).
 		SetTickMinimumInterval(time.Second).
@@ -52,8 +66,7 @@ func TestDiskSnapshotSimple(t *testing.T) {
 	require.NoError(t, err)
 	defer testSetup.close()
 
-	blockSize := md1.Options().RetentionOptions().BlockSize()
-	filePathPrefix := testSetup.storageOpts.CommitLogOptions().FilesystemOptions().FilePathPrefix()
+	shardSet := testSetup.shardSet
 
 	// Start the server
 	log := testSetup.storageOpts.InstrumentOptions().Logger()
@@ -68,41 +81,91 @@ func TestDiskSnapshotSimple(t *testing.T) {
 	}()
 
 	// Write test data
-	now := testSetup.getNowFn()
-	seriesMaps := make(map[xtime.UnixNano]generate.SeriesBlock)
-	inputData := []generate.BlockConfig{
-		{IDs: []string{"foo", "bar", "baz"}, NumPoints: 100, Start: now},
-	}
+	var (
+		currBlock = testSetup.getNowFn().Truncate(blockSize)
+		now       = currBlock.Add(11 * time.Minute)
+	)
+	// Make sure now is within bufferPast of the previous block
+	require.True(t, now.Before(now.Truncate(blockSize).Add(bufferPast)))
+	// Make sure now is within bufferFuture of the next block
+	require.True(t, now.After(now.Truncate(blockSize).Add(blockSize).Add(-bufferFuture)))
+	testSetup.setNowFn(now)
+
+	var (
+		seriesMaps = make(map[xtime.UnixNano]generate.SeriesBlock)
+		inputData  = []generate.BlockConfig{
+			// Writes in the previous block which should be mutable due to bufferPast
+			{IDs: []string{"foo", "bar", "baz"}, NumPoints: 5, Start: currBlock.Add(-10 * time.Minute)},
+			// Writes in the current block
+			{IDs: []string{"a", "b", "c"}, NumPoints: 30, Start: currBlock},
+			// Writes in the next block which should be mutable due to bufferFuture
+			{IDs: []string{"1", "2", "3"}, NumPoints: 30, Start: currBlock.Add(blockSize)},
+		}
+	)
 	for _, input := range inputData {
-		testSetup.setNowFn(input.Start)
+		// testSetup.setNowFn(input.Start)
 		testData := generate.Block(input)
-		seriesMaps[xtime.ToUnixNano(input.Start)] = testData
+		seriesMaps[xtime.ToUnixNano(input.Start.Truncate(blockSize))] = testData
 		for _, ns := range testSetup.namespaces {
 			require.NoError(t, testSetup.writeBatch(ns.ID(), testData))
 		}
 	}
 
-	now = testSetup.getNowFn().Add(blockSize).Add(-10 * time.Minute)
+	now = testSetup.getNowFn().Add(2 * time.Minute)
+	// TODO: Make this a function that can be repeated
+	// Make sure now is within bufferPast of the previous block
+	require.True(t, now.Before(now.Truncate(blockSize).Add(bufferPast)))
+	// Make sure now is within bufferFuture of the next block
+	require.True(t, now.After(now.Truncate(blockSize).Add(blockSize).Add(-bufferFuture)))
 	testSetup.setNowFn(now)
-	maxWaitTime := time.Minute
+
+	var (
+		maxWaitTime    = time.Minute
+		filePathPrefix = testSetup.storageOpts.
+				CommitLogOptions().
+				FilesystemOptions().
+				FilePathPrefix()
+	)
+
 	for _, ns := range testSetup.namespaces {
-		log.Debug("waiting for snapshot files to flush")
-		require.NoError(t, waitUntilSnapshotFilesFlushed(filePathPrefix, testSetup.shardSet, ns.ID(), []time.Time{now.Truncate(blockSize)}, maxWaitTime))
-		log.Debug("verifying snapshot files")
-		verifySnapshottedDataFiles(t, testSetup.shardSet, testSetup.storageOpts, ns.ID(), now.Truncate(blockSize), seriesMaps)
+		snapshotsToWaitFor := []snapshotID{
+			{
+				blockStart: currBlock.Add(-blockSize),
+				minVolume: getLatestSnapshotVolumeIndex(
+					filePathPrefix, shardSet, ns.ID(), currBlock.Add(-blockSize)),
+			},
+			{
+				blockStart: currBlock,
+				minVolume: getLatestSnapshotVolumeIndex(
+					filePathPrefix, shardSet, ns.ID(), currBlock),
+			},
+			{
+				blockStart: currBlock.Add(blockSize),
+				minVolume: getLatestSnapshotVolumeIndex(
+					filePathPrefix, shardSet, ns.ID(), currBlock.Add(blockSize)),
+			},
+		}
+
+		log.Info("waiting for snapshot files to flush")
+		require.NoError(t, waitUntilSnapshotFilesFlushed(
+			filePathPrefix, shardSet, ns.ID(), snapshotsToWaitFor, maxWaitTime))
+		log.Info("verifying snapshot files")
+		verifySnapshottedDataFiles(t, shardSet, testSetup.storageOpts, ns.ID(), seriesMaps)
 	}
 
-	oldTime := testSetup.getNowFn()
-	newTime := oldTime.Add(blockSize * 2)
+	var (
+		oldTime = testSetup.getNowFn()
+		newTime = oldTime.Add(blockSize * 2)
+	)
 	testSetup.setNowFn(newTime)
 
-	log.Debug("sleeping for 10x minimum tick interval")
-	testSetup.sleepFor10xTickMinimumInterval()
 	for _, ns := range testSetup.namespaces {
-		log.Debug("waiting for new snapshot files to be written out")
-		require.NoError(t, waitUntilSnapshotFilesFlushed(filePathPrefix, testSetup.shardSet, ns.ID(), []time.Time{newTime.Truncate(blockSize)}, maxWaitTime))
-		for _, shard := range testSetup.shardSet.All() {
-			log.Debug("waiting for old snapshot files to be deleted")
+		log.Info("waiting for new snapshot files to be written out")
+		snapshotsToWaitFor := []snapshotID{{blockStart: newTime.Truncate(blockSize)}}
+		require.NoError(t, waitUntilSnapshotFilesFlushed(
+			filePathPrefix, shardSet, ns.ID(), snapshotsToWaitFor, maxWaitTime))
+		log.Info("waiting for old snapshot files to be deleted")
+		for _, shard := range shardSet.All() {
 			waitUntil(func() bool {
 				exists, err := fs.SnapshotFileSetExistsAt(filePathPrefix, ns.ID(), shard.ID(), oldTime.Truncate(blockSize))
 				require.NoError(t, err)
