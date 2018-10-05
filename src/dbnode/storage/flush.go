ee4f425581be99d2e9b48a670e6dee06df22bb19
Richard Artoul <rartoul@uber.com>
2018-10-05 19:14:20 -0400

    Fix typo

--- src/dbnode/storage/flush.go
+++ src/dbnode/storage/flush.go
@@ -232,7 +232,7 @@ func (m *flushManager) namespaceSnapshotTimes(ns databaseNamespace, curr time.Ti
 		rOpts     = ns.Options().RetentionOptions()
 		blockSize = rOpts.BlockSize()
 		// Earliest possible snapshottable block is the earliest possible flushable
-		// block start which is the first block in the retention period.
+		// blockStart which is the first block in the retention period.
 		earliest = retention.FlushTimeStart(rOpts, curr)
 		// Latest possible snapshotting block is either the current block OR the
 		// next block if the current time and bufferFuture configuration would
