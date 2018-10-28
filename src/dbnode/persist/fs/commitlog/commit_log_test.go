becb40b1f76a8a0fda0622a62ee6e7fde0ce05d0
Richard Artoul <rartoul@uber.com>
2018-10-27 22:15:05 -0700

    Add unit test for RotateLogs api

--- src/dbnode/persist/fs/commitlog/commit_log_test.go
+++ src/dbnode/persist/fs/commitlog/commit_log_test.go
@@ -855,6 +855,58 @@ func TestCommitLogActiveLogs(t *testing.T) {
 	require.Error(t, err)
 }
 
+func TestCommitLogRotateLogs(t *testing.T) {
+	var (
+		clock       = mclock.NewMock()
+		opts, scope = newTestOptions(t, overrides{
+			clock:    clock,
+			strategy: StrategyWriteWait,
+		})
+	)
+	defer cleanup(t, opts)
+
+	var (
+		commitLog    = newTestCommitLog(t, opts)
+		blockSize    = opts.BlockSize()
+		alignedStart = clock.Now().Truncate(blockSize)
+	)
+	require.True(t, time.Second < blockSize)
+
+	// Writes spaced apart by block size
+	writes := []testWrite{
+		{testSeries(0, "foo.bar", testTags1, 127), alignedStart, 123.456, xtime.Millisecond, nil, nil},
+		{testSeries(1, "foo.baz", testTags2, 150), alignedStart.Add(1 * time.Second), 456.789, xtime.Millisecond, nil, nil},
+		{testSeries(2, "foo.qux", testTags3, 291), alignedStart.Add(2 * time.Second), 789.123, xtime.Millisecond, nil, nil},
+	}
+
+	for _, write := range writes {
+		// Set clock to align with the write
+		clock.Add(write.t.Sub(clock.Now()))
+
+		// Write entry
+		wg := writeCommitLogs(t, scope, commitLog, []testWrite{write})
+
+		// TODO: Assert on file
+		_, err := commitLog.RotateLogs()
+		require.NoError(t, err)
+
+		// Flush until finished, this is required as timed flusher not active when clock is mocked
+		flushUntilDone(commitLog, wg)
+	}
+
+	// Ensure files present for each block size time window
+	fsopts := opts.FilesystemOptions()
+	files, err := fs.SortedCommitLogFiles(fs.CommitLogsDirPath(fsopts.FilePathPrefix()))
+	require.NoError(t, err)
+	require.Equal(t, len(files), len(writes)+1) // +1 to account for the initial file
+
+	// Close and consequently flush
+	require.NoError(t, commitLog.Close())
+
+	// Assert write flushed by reading the commit log
+	assertCommitLogWritesByIterating(t, commitLog, writes)
+}
+
 var (
 	testTag1 = ident.StringTag("name1", "val1")
 	testTag2 = ident.StringTag("name2", "val2")
