9e4298e7c127c2d95f0d63b5d8f412b17ce6d232
Richard Artoul <rartoul@uber.com>
2018-11-07 23:35:35 +0200

    Add comments

--- src/dbnode/persist/fs/msgpack/encoder.go
+++ src/dbnode/persist/fs/msgpack/encoder.go
@@ -319,58 +319,58 @@ func (enc *Encoder) encodeArrayLen(value int) {
 	enc.err = enc.enc.EncodeArrayLen(value)
 }
 
-// EncodeLogEntryFast encodes a commit log entry without buffering
-// for faster encoding.
+// EncodeLogEntryFast encodes a commit log entry with no buffering and using optimized helper
+// functions that bypass the msgpack encoding library. This results in a lot of code duplication
+// for this one path, but we pay the price because this is the most frequently called function
+// in M3DB and it directly applies back-pressure on every part of the system. As a result, almost
+// any performance gains that can be had in this function are worth it. Please run all the benchmarks
+// in encoder_bench.go before making any changes.
 func EncodeLogEntryFast(b []byte, entry schema.LogEntry) ([]byte, error) {
 	if logEntryHeaderErr != nil {
 		return nil, logEntryHeaderErr
 	}
 
 	b = append(b, logEntryHeader...)
-
 	b = encodeVarUint64(b, entry.Index)
-	// write8(b, codes.Uint64, entry.Index)
-
 	b = encodeVarInt64(b, entry.Create)
-	// write8(b, codes.Int64, uint64(entry.Create))
-
 	b = encodeBytes(b, entry.Metadata)
-
 	b = encodeVarInt64(b, entry.Timestamp)
-	// write8(b, codes.Int64, uint64(entry.Timestamp))
-
 	b = encodeFloat64(b, entry.Value)
-
 	b = encodeVarUint64(b, uint64(entry.Unit))
-	// write8(b, codes.Uint64, uint64(entry.Unit))
-
 	b = encodeBytes(b, entry.Annotation)
+
 	return b, nil
 }
 
+// encodeVarUint64 is used in EncodeLogEntryFast which is a very hot path.
+// As a result, many of the function calls in this function have been
+// manually inlined to reduce function call overhead.
 func encodeVarUint64(b []byte, v uint64) []byte {
 	if v <= math.MaxInt8 {
 		b, buf := growAndReturn(b, 1)
 		buf[0] = byte(v)
 		return b
 	}
+
 	if v <= math.MaxUint8 {
-		// return write1(b, codes.Uint8, v)
+		// Equivalent to: return write1(b, codes.Uint8, v)
 		b, buf := growAndReturn(b, 2)
 		buf[0] = codes.Uint8
 		buf[1] = byte(v)
 		return b
 	}
+
 	if v <= math.MaxUint16 {
-		// return write2(b, codes.Uint16, v)
+		// Equivalent to: return write2(b, codes.Uint16, v)
 		b, buf := growAndReturn(b, 3)
 		buf[0] = codes.Uint16
 		buf[1] = byte(v >> 8)
 		buf[2] = byte(v)
 		return b
 	}
+
 	if v <= math.MaxUint32 {
-		// return write4(b, codes.Uint32, v)
+		// Equivalent to: return write4(b, codes.Uint32, v)
 		b, buf := growAndReturn(b, 5)
 		buf[0] = codes.Uint32
 		buf[1] = byte(v >> 24)
@@ -380,7 +380,7 @@ func encodeVarUint64(b []byte, v uint64) []byte {
 		return b
 	}
 
-	// return write8(b, codes.Uint64, v)
+	// Equivalent to: return write8(b, codes.Uint64, v)
 	b, buf := growAndReturn(b, 9)
 	buf[0] = codes.Uint64
 	buf[1] = byte(v >> 56)
@@ -391,34 +391,41 @@ func encodeVarUint64(b []byte, v uint64) []byte {
 	buf[6] = byte(v >> 16)
 	buf[7] = byte(v >> 8)
 	buf[8] = byte(v)
+
 	return b
 }
 
+// encodeVarInt64 is used in EncodeLogEntryFast which is a very hot path.
+// As a result, many of the function calls in this function have been
+// manually inlined to reduce function call overhead.
 func encodeVarInt64(b []byte, v int64) []byte {
 	if v >= 0 {
-		// return encodeVarUint64(b, uint64(v))
+		// Equivalent to: return encodeVarUint64(b, uint64(v))
 		if v <= math.MaxInt8 {
 			b, buf := growAndReturn(b, 1)
 			buf[0] = byte(v)
 			return b
 		}
+
 		if v <= math.MaxUint8 {
-			// return write1(b, codes.Uint8, v)
+			// Equivalent to: return write1(b, codes.Uint8, v)
 			b, buf := growAndReturn(b, 2)
 			buf[0] = codes.Uint8
 			buf[1] = byte(v)
 			return b
 		}
+
 		if v <= math.MaxUint16 {
-			// return write2(b, codes.Uint16, v)
+			// Equivalent to: return write2(b, codes.Uint16, v)
 			b, buf := growAndReturn(b, 3)
 			buf[0] = codes.Uint16
 			buf[1] = byte(v >> 8)
 			buf[2] = byte(v)
 			return b
 		}
+
 		if v <= math.MaxUint32 {
-			// return write4(b, codes.Uint32, v)
+			// Equivalent to: return write4(b, codes.Uint32, v)
 			b, buf := growAndReturn(b, 5)
 			buf[0] = codes.Uint32
 			buf[1] = byte(v >> 24)
@@ -428,7 +435,7 @@ func encodeVarInt64(b []byte, v int64) []byte {
 			return b
 		}
 
-		// return write8(b, codes.Uint64, v)
+		// Equivalent to: return write8(b, codes.Uint64, v)
 		b, buf := growAndReturn(b, 9)
 		buf[0] = codes.Uint64
 		buf[1] = byte(v >> 56)
@@ -447,20 +454,57 @@ func encodeVarInt64(b []byte, v int64) []byte {
 		buff[0] = byte(v)
 		return b
 	}
+
 	if v >= math.MinInt8 {
-		return write1(b, codes.Int8, uint64(v))
+		// Equivalent to: return write1(b, codes.Int8, uint64(v))
+		b, buf := growAndReturn(b, 2)
+		buf[0] = codes.Int8
+		buf[1] = byte(uint64(v))
+		return b
 	}
+
 	if v >= math.MinInt16 {
-		return write2(b, codes.Int16, uint64(v))
+		// Equivalent to: return write2(b, codes.Int16, uint64(v))
+		b, buf := growAndReturn(b, 3)
+		n := uint64(v)
+		buf[0] = codes.Int16
+		buf[1] = byte(uint64(n) >> 8)
+		buf[2] = byte(uint64(n))
+		return b
 	}
+
 	if v >= math.MinInt32 {
-		return write4(b, codes.Int32, uint64(v))
+		// Equivalent to: return write4(b, codes.Int32, uint64(v))
+		b, buf := growAndReturn(b, 5)
+		n := uint64(v)
+		buf[0] = codes.Int32
+		buf[1] = byte(n >> 24)
+		buf[2] = byte(n >> 16)
+		buf[3] = byte(n >> 8)
+		buf[4] = byte(n)
+		return b
 	}
-	return write8(b, codes.Int64, uint64(v))
+
+	// Equivalent to: return write8(b, codes.Int64, uint64(v))
+	b, buf := growAndReturn(b, 9)
+	n := uint64(v)
+	buf[0] = codes.Int64
+	buf[1] = byte(n >> 56)
+	buf[2] = byte(n >> 48)
+	buf[3] = byte(n >> 40)
+	buf[4] = byte(n >> 32)
+	buf[5] = byte(n >> 24)
+	buf[6] = byte(n >> 16)
+	buf[7] = byte(n >> 8)
+	buf[8] = byte(n)
+	return b
 }
 
+// encodeFloat64 is used in EncodeLogEntryFast which is a very hot path.
+// As a result, many of the function calls in this function have been
+// manually inlined to reduce function call overhead.
 func encodeFloat64(b []byte, v float64) []byte {
-	// return write8(b, codes.Double, math.Float64bits(n))
+	// Equivalent to: return write8(b, codes.Double, math.Float64bits(n))
 	b, buf := growAndReturn(b, 9)
 	buf[0] = codes.Double
 	n := math.Float64bits(v)
@@ -475,13 +519,17 @@ func encodeFloat64(b []byte, v float64) []byte {
 	return b
 }
 
+// encodeBytes is used in EncodeLogEntryFast which is a very hot path.
+// As a result, many of the function calls in this function have been
+// manually inlined to reduce function call overhead.
 func encodeBytes(b []byte, data []byte) []byte {
 	if data == nil {
 		b, buf := growAndReturn(b, 1)
 		buf[0] = codes.Nil
 		return b
 	}
-	// b = encodeBytesLen(b, len(data))
+
+	// Equivalent to: encodeBytesLen(b, len(data))
 	var (
 		l   = len(data)
 		v   = uint64(l)
@@ -510,6 +558,8 @@ func encodeBytes(b []byte, data []byte) []byte {
 	return b
 }
 
+// encodeBytesLen is not used, it is left here to demonstrate what a manually inlined
+// version of this function should look like.
 func encodeBytesLen(b []byte, l int) []byte {
 	if l < 256 {
 		return write1(b, codes.Bin8, uint64(l))
@@ -520,6 +570,8 @@ func encodeBytesLen(b []byte, l int) []byte {
 	return write4(b, codes.Bin32, uint64(l))
 }
 
+// write1 is not used, it is left here to demonstrate what a manually inlined
+// version of this function should look like.
 func write1(b []byte, code byte, n uint64) []byte {
 	b, buf := growAndReturn(b, 2)
 	buf[0] = code
@@ -527,6 +579,8 @@ func write1(b []byte, code byte, n uint64) []byte {
 	return b
 }
 
+// write2 is not used, it is left here to demonstrate what a manually inlined
+// version of this function should look like.
 func write2(b []byte, code byte, n uint64) []byte {
 	b, buf := growAndReturn(b, 3)
 	buf[0] = code
@@ -535,6 +589,8 @@ func write2(b []byte, code byte, n uint64) []byte {
 	return b
 }
 
+// write4 is not used, it is left here to demonstrate what a manually inlined
+// version of this function should look like.
 func write4(b []byte, code byte, n uint64) []byte {
 	b, buf := growAndReturn(b, 5)
 	buf[0] = code
@@ -545,6 +601,8 @@ func write4(b []byte, code byte, n uint64) []byte {
 	return b
 }
 
+// write8 is not used, it is left here to demonstrate what a manually inlined
+// version of this function should look like.
 func write8(b []byte, code byte, n uint64) []byte {
 	b, buf := growAndReturn(b, 9)
 	buf[0] = code
