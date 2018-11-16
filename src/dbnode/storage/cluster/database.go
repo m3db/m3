e0110b916e25a08fae5e992cd087f24806eb99b3
Richard Artoul <rartoul@uber.com>
2018-11-16 14:49:30 -0500

    remove print

--- src/dbnode/storage/cluster/database.go
+++ src/dbnode/storage/cluster/database.go
@@ -190,7 +190,6 @@ func (d *clusterDB) activeTopologyWatch() {
 		for {
 			select {
 			case <-ticker.C:
-				fmt.Println("ticking")
 				d.analyzeAndReportShardStates()
 			case <-reportClosingCh:
 				ticker.Stop()
