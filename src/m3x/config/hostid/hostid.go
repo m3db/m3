f61a9a05d6ba965567200ac044a3c854d040d94c
Matt Schallert <mattschallert@gmail.com>
2018-11-24 15:17:40 -0500

    [hostid] support reading ID from a file (#204)
    Under some orchestration systems, such as Kubernetes, we may need to
    read host identity from a file on disk that is dynamically populated
    with instance metadata. Additionally, this metadata may sometimes be
    populated by an external actor and may not be available at startup, so
    we provide an optional timeout to wait for it.
    
    As a concrete example, on Kubernetes this supports reading host ID from
    pod metadata that is injected using [downward API
    volumes](https://git.io/fp4hL). The host ID may not be immediately
    populated, but the user can provide a timeout that the instance will
    wait for it before failing to start. An example of a pod metadata file
    in Kubernetes is:
    
    ```
    root@matt-test-656988ff69-gk49b:/# cat /etc/podinfo/id
    test-id
    ```

--- src/m3x/config/hostid/hostid.go
+++ src/m3x/config/hostid/hostid.go
@@ -23,8 +23,20 @@
 package hostid
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
+	"time"
+)
+
+const (
+	defaultFileCheckInterval = time.Second
+)
+
+var (
+	errHostIDFileEmpty = errors.New("host ID file is empty")
 )
 
 // Resolver is a type of host ID resolver
@@ -38,7 +50,14 @@ const (
 	// EnvironmentResolver resolves host using an environment variable
 	// of which the name is provided in config
 	EnvironmentResolver Resolver = "environment"
+	// FileResolver reads its identity from a non-empty file.
+	FileResolver Resolver = "file"
 )
+
+// IDResolver represents a method of resolving host identity.
+type IDResolver interface {
+	ID() (string, error)
+}
 
 // Configuration is the configuration for resolving a host ID.
 type Configuration struct {
@@ -50,34 +69,135 @@ type Configuration struct {
 
 	// EnvVarName is the environment specified host ID if using environment host ID resolver.
 	EnvVarName *string `yaml:"envVarName"`
+
+	// File is the file config.
+	File *FileConfig `yaml:"file"`
+}
+
+// FileConfig contains the info needed to construct a FileResolver.
+type FileConfig struct {
+	// Path of the file containing the host ID.
+	Path string `yaml:"path"`
+
+	// Timeout to wait for the file to be non-empty.
+	Timeout *time.Duration `yaml:"timeout"`
+}
+
+func (c Configuration) resolver() (IDResolver, error) {
+	switch c.Resolver {
+	case HostnameResolver:
+		return hostnameResolver{}, nil
+	case ConfigResolver:
+		return &configResolver{value: c.Value}, nil
+	case EnvironmentResolver:
+		return &environmentResolver{envVarName: c.EnvVarName}, nil
+	case FileResolver:
+		if c.File == nil {
+			return nil, errors.New("file config cannot be nil")
+		}
+		return &file{
+			path:    c.File.Path,
+			timeout: c.File.Timeout,
+		}, nil
+	}
+	return nil, fmt.Errorf("unknown host ID resolver: resolver=%s",
+		string(c.Resolver))
 }
 
 // Resolve returns the resolved host ID given the configuration.
 func (c Configuration) Resolve() (string, error) {
-	switch c.Resolver {
-	case HostnameResolver:
-		return os.Hostname()
-	case ConfigResolver:
-		if c.Value == nil {
-			err := fmt.Errorf("missing host ID using: resolver=%s",
-				string(c.Resolver))
-			return "", err
-		}
-		return *c.Value, nil
-	case EnvironmentResolver:
-		if c.EnvVarName == nil {
-			err := fmt.Errorf("missing host ID env var name using: resolver=%s",
-				string(c.Resolver))
-			return "", err
-		}
-		v := os.Getenv(*c.EnvVarName)
-		if v == "" {
-			err := fmt.Errorf("missing host ID env var value using: resolver=%s, name=%s",
-				string(c.Resolver), *c.EnvVarName)
-			return "", err
-		}
-		return v, nil
+	r, err := c.resolver()
+	if err != nil {
+		return "", err
 	}
-	return "", fmt.Errorf("unknown host ID resolver: resolver=%s",
-		string(c.Resolver))
+	return r.ID()
+}
+
+type hostnameResolver struct{}
+
+func (hostnameResolver) ID() (string, error) {
+	return os.Hostname()
+}
+
+type configResolver struct {
+	value *string
+}
+
+func (c *configResolver) ID() (string, error) {
+	if c.value == nil {
+		err := fmt.Errorf("missing host ID using: resolver=%s", string(ConfigResolver))
+		return "", err
+	}
+	return *c.value, nil
+}
+
+type environmentResolver struct {
+	envVarName *string
+}
+
+func (c *environmentResolver) ID() (string, error) {
+	if c.envVarName == nil {
+		err := fmt.Errorf("missing host ID env var name using: resolver=%s",
+			string(EnvironmentResolver))
+		return "", err
+	}
+	v := os.Getenv(*c.envVarName)
+	if v == "" {
+		err := fmt.Errorf("missing host ID env var value using: resolver=%s, name=%s",
+			string(EnvironmentResolver), *c.envVarName)
+		return "", err
+	}
+	return v, nil
+}
+
+type file struct {
+	path     string
+	interval time.Duration
+	timeout  *time.Duration
+}
+
+// ID attempts to parse an ID from a  file. It will optionally wait a timeout to
+// find the value, as in some environments the file may be dynamically generated
+// from external metadata and not immediately available when the instance starts
+// up.
+func (c *file) ID() (string, error) {
+	checkF := func() (string, error) {
+		f, err := os.Open(c.path)
+		if err != nil {
+			return "", err
+		}
+
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			return "", err
+		}
+
+		val := strings.TrimSpace(string(data))
+		if len(val) == 0 {
+			return "", errHostIDFileEmpty
+		}
+
+		return val, nil
+	}
+
+	if c.timeout == nil {
+		return checkF()
+	}
+
+	interval := c.interval
+	if interval == 0 {
+		interval = defaultFileCheckInterval
+	}
+
+	startT := time.Now()
+	for time.Since(startT) < *c.timeout {
+		v, err := checkF()
+		if err == nil {
+			return v, nil
+		}
+
+		time.Sleep(interval)
+	}
+
+	return "", fmt.Errorf("did not find value in %s within %s", c.path, c.timeout)
 }
