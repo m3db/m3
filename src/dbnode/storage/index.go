694e4e0b323cc54f1e441af9bd59f0af146980f3
Rob Skillington <r@uber.com>
2018-11-25 17:42:18 -0500

    Fix some logging

--- src/dbnode/storage/index.go
+++ src/dbnode/storage/index.go
@@ -331,6 +331,10 @@ func (i *nsIndex) reportStats() error {
 					break
 				}
 			}))
+		if err == index.ErrUnableReportStatsBlockClosed {
+			// Closed blocks are temporarily in the list still
+			continue
+		}
 		if err != nil {
 			return err
 		}
