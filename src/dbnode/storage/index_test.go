76dd8fec406167b7eb2322e4f307aa2651d2dde1
Rob Skillington <r@uber.com>
2018-11-28 20:25:58 -0500

    Add concurrent test

--- src/dbnode/storage/index_test.go
+++ src/dbnode/storage/index_test.go
@@ -22,6 +22,7 @@ package storage
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -29,13 +30,19 @@ import (
 	"github.com/m3db/m3/src/dbnode/retention"
 	"github.com/m3db/m3/src/dbnode/storage/block"
 	"github.com/m3db/m3/src/dbnode/storage/index"
+	"github.com/m3db/m3/src/dbnode/storage/index/convert"
 	"github.com/m3db/m3/src/dbnode/storage/namespace"
+	"github.com/m3db/m3/src/m3ninx/doc"
+	"github.com/m3db/m3/src/m3ninx/idx"
 	"github.com/m3db/m3/src/m3ninx/index/segment"
+	"github.com/m3db/m3x/context"
 	"github.com/m3db/m3x/ident"
 	xtest "github.com/m3db/m3x/test"
 	xtime "github.com/m3db/m3x/time"
 
+	"github.com/fortytw2/leaktest"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -87,26 +94,15 @@ func TestNamespaceIndexFlushSuccess(t *testing.T) {
 	ctrl := gomock.NewController(xtest.Reporter{t})
 	defer ctrl.Finish()
 
-	blockSize := time.Hour
-	indexBlockSize := 2 * time.Hour
-	period := 8 * time.Hour
-	nopts := namespace.NewOptions().
-		SetRetentionOptions(retention.NewOptions().
-			SetBlockSize(blockSize).
-			SetRetentionPeriod(period)).
-		SetIndexOptions(namespace.NewIndexOptions().SetBlockSize(indexBlockSize))
-	md, err := namespace.NewMetadata(ident.StringID("testns"), nopts)
-	require.NoError(t, err)
-	nsIdx, err := newNamespaceIndex(md, testDatabaseOptions())
-	require.NoError(t, err)
+	test := newTestIndex(t, ctrl)
 
-	now := time.Now().Truncate(indexBlockSize)
-	idx := nsIdx.(*nsIndex)
+	now := time.Now().Truncate(test.indexBlockSize)
+	idx := test.index.(*nsIndex)
 
 	mockBlock := index.NewMockBlock(ctrl)
-	blockTime := now.Add(-2 * indexBlockSize)
+	blockTime := now.Add(-2 * test.indexBlockSize)
 	mockBlock.EXPECT().StartTime().Return(blockTime).AnyTimes()
-	mockBlock.EXPECT().EndTime().Return(blockTime.Add(indexBlockSize)).AnyTimes()
+	mockBlock.EXPECT().EndTime().Return(blockTime.Add(test.indexBlockSize)).AnyTimes()
 	idx.state.blocksByTime[xtime.ToUnixNano(blockTime)] = mockBlock
 
 	mockBlock.EXPECT().IsSealed().Return(true)
@@ -115,7 +111,7 @@ func TestNamespaceIndexFlushSuccess(t *testing.T) {
 	mockShard := NewMockdatabaseShard(ctrl)
 	mockShard.EXPECT().ID().Return(uint32(0)).AnyTimes()
 	mockShard.EXPECT().FlushState(blockTime).Return(fileOpState{Status: fileOpSuccess})
-	mockShard.EXPECT().FlushState(blockTime.Add(blockSize)).Return(fileOpState{Status: fileOpSuccess})
+	mockShard.EXPECT().FlushState(blockTime.Add(test.blockSize)).Return(fileOpState{Status: fileOpSuccess})
 	shards := []databaseShard{mockShard}
 
 	mockFlush := persist.NewMockIndexFlush(ctrl)
@@ -135,7 +131,7 @@ func TestNamespaceIndexFlushSuccess(t *testing.T) {
 		Persist: persistFn,
 	}
 	mockFlush.EXPECT().PrepareIndex(xtest.CmpMatcher(persist.IndexPrepareOptions{
-		NamespaceMetadata: md,
+		NamespaceMetadata: test.metadata,
 		BlockStart:        blockTime,
 		FileSetType:       persist.FileSetFlushType,
 		Shards:            map[uint32]struct{}{0: struct{}{}},
@@ -144,13 +140,13 @@ func TestNamespaceIndexFlushSuccess(t *testing.T) {
 	results := block.NewMockFetchBlocksMetadataResults(ctrl)
 	results.EXPECT().Results().Return(nil)
 	results.EXPECT().Close()
-	mockShard.EXPECT().FetchBlocksMetadataV2(gomock.Any(), blockTime, blockTime.Add(indexBlockSize),
+	mockShard.EXPECT().FetchBlocksMetadataV2(gomock.Any(), blockTime, blockTime.Add(test.indexBlockSize),
 		gomock.Any(), gomock.Any(), block.FetchBlocksMetadataOptions{}).Return(results, nil, nil)
 
 	mockBlock.EXPECT().AddResults(gomock.Any()).Return(nil)
 	mockBlock.EXPECT().EvictMutableSegments().Return(index.EvictMutableSegmentResults{}, nil)
 
-	require.NoError(t, nsIdx.Flush(mockFlush, shards))
+	require.NoError(t, idx.Flush(mockFlush, shards))
 	require.True(t, persistCalled)
 	require.True(t, persistClosed)
 }
@@ -159,26 +155,15 @@ func TestNamespaceIndexFlushShardStateNotSuccess(t *testing.T) {
 	ctrl := gomock.NewController(xtest.Reporter{t})
 	defer ctrl.Finish()
 
-	blockSize := time.Hour
-	indexBlockSize := 2 * time.Hour
-	period := 8 * time.Hour
-	nopts := namespace.NewOptions().
-		SetRetentionOptions(retention.NewOptions().
-			SetBlockSize(blockSize).
-			SetRetentionPeriod(period)).
-		SetIndexOptions(namespace.NewIndexOptions().SetBlockSize(indexBlockSize))
-	md, err := namespace.NewMetadata(ident.StringID("testns"), nopts)
-	require.NoError(t, err)
-	nsIdx, err := newNamespaceIndex(md, testDatabaseOptions())
-	require.NoError(t, err)
+	test := newTestIndex(t, ctrl)
 
-	now := time.Now().Truncate(indexBlockSize)
-	idx := nsIdx.(*nsIndex)
+	now := time.Now().Truncate(test.indexBlockSize)
+	idx := test.index.(*nsIndex)
 
 	mockBlock := index.NewMockBlock(ctrl)
-	blockTime := now.Add(-2 * indexBlockSize)
+	blockTime := now.Add(-2 * test.indexBlockSize)
 	mockBlock.EXPECT().StartTime().Return(blockTime).AnyTimes()
-	mockBlock.EXPECT().EndTime().Return(blockTime.Add(indexBlockSize)).AnyTimes()
+	mockBlock.EXPECT().EndTime().Return(blockTime.Add(test.indexBlockSize)).AnyTimes()
 	idx.state.blocksByTime[xtime.ToUnixNano(blockTime)] = mockBlock
 
 	mockBlock.EXPECT().IsSealed().Return(true)
@@ -187,38 +172,27 @@ func TestNamespaceIndexFlushShardStateNotSuccess(t *testing.T) {
 	mockShard := NewMockdatabaseShard(ctrl)
 	mockShard.EXPECT().ID().Return(uint32(0)).AnyTimes()
 	mockShard.EXPECT().FlushState(blockTime).Return(fileOpState{Status: fileOpSuccess})
-	mockShard.EXPECT().FlushState(blockTime.Add(blockSize)).Return(fileOpState{Status: fileOpFailed})
+	mockShard.EXPECT().FlushState(blockTime.Add(test.blockSize)).Return(fileOpState{Status: fileOpFailed})
 	shards := []databaseShard{mockShard}
 
 	mockFlush := persist.NewMockIndexFlush(ctrl)
 
-	require.NoError(t, nsIdx.Flush(mockFlush, shards))
+	require.NoError(t, idx.Flush(mockFlush, shards))
 }
 
 func TestNamespaceIndexFlushSuccessMultipleShards(t *testing.T) {
 	ctrl := gomock.NewController(xtest.Reporter{t})
 	defer ctrl.Finish()
 
-	blockSize := time.Hour
-	indexBlockSize := 2 * time.Hour
-	period := 8 * time.Hour
-	nopts := namespace.NewOptions().
-		SetRetentionOptions(retention.NewOptions().
-			SetBlockSize(blockSize).
-			SetRetentionPeriod(period)).
-		SetIndexOptions(namespace.NewIndexOptions().SetBlockSize(indexBlockSize))
-	md, err := namespace.NewMetadata(ident.StringID("testns"), nopts)
-	require.NoError(t, err)
-	nsIdx, err := newNamespaceIndex(md, testDatabaseOptions())
-	require.NoError(t, err)
+	test := newTestIndex(t, ctrl)
 
-	now := time.Now().Truncate(indexBlockSize)
-	idx := nsIdx.(*nsIndex)
+	now := time.Now().Truncate(test.indexBlockSize)
+	idx := test.index.(*nsIndex)
 
 	mockBlock := index.NewMockBlock(ctrl)
-	blockTime := now.Add(-2 * indexBlockSize)
+	blockTime := now.Add(-2 * test.indexBlockSize)
 	mockBlock.EXPECT().StartTime().Return(blockTime).AnyTimes()
-	mockBlock.EXPECT().EndTime().Return(blockTime.Add(indexBlockSize)).AnyTimes()
+	mockBlock.EXPECT().EndTime().Return(blockTime.Add(test.indexBlockSize)).AnyTimes()
 	idx.state.blocksByTime[xtime.ToUnixNano(blockTime)] = mockBlock
 
 	mockBlock.EXPECT().IsSealed().Return(true)
@@ -227,12 +201,12 @@ func TestNamespaceIndexFlushSuccessMultipleShards(t *testing.T) {
 	mockShard1 := NewMockdatabaseShard(ctrl)
 	mockShard1.EXPECT().ID().Return(uint32(0)).AnyTimes()
 	mockShard1.EXPECT().FlushState(blockTime).Return(fileOpState{Status: fileOpSuccess})
-	mockShard1.EXPECT().FlushState(blockTime.Add(blockSize)).Return(fileOpState{Status: fileOpSuccess})
+	mockShard1.EXPECT().FlushState(blockTime.Add(test.blockSize)).Return(fileOpState{Status: fileOpSuccess})
 
 	mockShard2 := NewMockdatabaseShard(ctrl)
 	mockShard2.EXPECT().ID().Return(uint32(1)).AnyTimes()
 	mockShard2.EXPECT().FlushState(blockTime).Return(fileOpState{Status: fileOpSuccess})
-	mockShard2.EXPECT().FlushState(blockTime.Add(blockSize)).Return(fileOpState{Status: fileOpSuccess})
+	mockShard2.EXPECT().FlushState(blockTime.Add(test.blockSize)).Return(fileOpState{Status: fileOpSuccess})
 
 	shards := []databaseShard{mockShard1, mockShard2}
 
@@ -253,7 +227,7 @@ func TestNamespaceIndexFlushSuccessMultipleShards(t *testing.T) {
 		Persist: persistFn,
 	}
 	mockFlush.EXPECT().PrepareIndex(xtest.CmpMatcher(persist.IndexPrepareOptions{
-		NamespaceMetadata: md,
+		NamespaceMetadata: test.metadata,
 		BlockStart:        blockTime,
 		FileSetType:       persist.FileSetFlushType,
 		Shards:            map[uint32]struct{}{0: struct{}{}, 1: struct{}{}},
@@ -262,19 +236,262 @@ func TestNamespaceIndexFlushSuccessMultipleShards(t *testing.T) {
 	results1 := block.NewMockFetchBlocksMetadataResults(ctrl)
 	results1.EXPECT().Results().Return(nil)
 	results1.EXPECT().Close()
-	mockShard1.EXPECT().FetchBlocksMetadataV2(gomock.Any(), blockTime, blockTime.Add(indexBlockSize),
+	mockShard1.EXPECT().FetchBlocksMetadataV2(gomock.Any(), blockTime, blockTime.Add(test.indexBlockSize),
 		gomock.Any(), gomock.Any(), block.FetchBlocksMetadataOptions{}).Return(results1, nil, nil)
 
 	results2 := block.NewMockFetchBlocksMetadataResults(ctrl)
 	results2.EXPECT().Results().Return(nil)
 	results2.EXPECT().Close()
-	mockShard2.EXPECT().FetchBlocksMetadataV2(gomock.Any(), blockTime, blockTime.Add(indexBlockSize),
+	mockShard2.EXPECT().FetchBlocksMetadataV2(gomock.Any(), blockTime, blockTime.Add(test.indexBlockSize),
 		gomock.Any(), gomock.Any(), block.FetchBlocksMetadataOptions{}).Return(results2, nil, nil)
 
 	mockBlock.EXPECT().AddResults(gomock.Any()).Return(nil)
 	mockBlock.EXPECT().EvictMutableSegments().Return(index.EvictMutableSegmentResults{}, nil)
 
-	require.NoError(t, nsIdx.Flush(mockFlush, shards))
+	require.NoError(t, idx.Flush(mockFlush, shards))
 	require.Equal(t, 2, numPersistCalls)
 	require.True(t, persistClosed)
+}
+
+func TestNamespaceIndexQueryNoMatchingBlocks(t *testing.T) {
+	ctrl := gomock.NewController(xtest.Reporter{t})
+	defer ctrl.Finish()
+
+	test := newTestIndex(t, ctrl)
+
+	now := time.Now().Truncate(test.indexBlockSize)
+	query := index.Query{idx.NewTermQuery([]byte("foo"), []byte("bar"))}
+	idx := test.index.(*nsIndex)
+
+	mockBlock := index.NewMockBlock(ctrl)
+	blockTime := now.Add(-1 * test.indexBlockSize)
+	mockBlock.EXPECT().StartTime().Return(blockTime).AnyTimes()
+	mockBlock.EXPECT().EndTime().Return(blockTime.Add(test.indexBlockSize)).AnyTimes()
+	idx.state.blocksByTime[xtime.ToUnixNano(blockTime)] = mockBlock
+
+	ctx := context.NewContext()
+	defer ctx.Close()
+
+	// Query non-overlapping range
+	result, err := idx.Query(ctx, query, index.QueryOptions{
+		StartInclusive: now.Add(-3 * test.indexBlockSize),
+		EndExclusive:   now.Add(-2 * test.indexBlockSize),
+	})
+	require.NoError(t, err)
+	assert.True(t, result.Exhaustive)
+	assert.Equal(t, 0, result.Results.Size())
+}
+
+func TestNamespaceIndexHighConcurrentQueriesWithTimeouts(t *testing.T) {
+	testNamespaceIndexHighConcurrentQueries(t, true)
+}
+
+func TestNamespaceIndexHighConcurrentQueriesWithoutTimeouts(t *testing.T) {
+	testNamespaceIndexHighConcurrentQueries(t, false)
+}
+
+func testNamespaceIndexHighConcurrentQueries(
+	t *testing.T,
+	withTimeout bool,
+) {
+	ctrl := gomock.NewController(xtest.Reporter{t})
+	defer ctrl.Finish()
+
+	defer leaktest.CheckTimeout(t, 2*time.Minute)()
+
+	test := newTestIndex(t, ctrl)
+	defer func() {
+		err := test.index.Close()
+		require.NoError(t, err)
+	}()
+
+	now := time.Now().Truncate(test.indexBlockSize)
+
+	min, max := now.Add(-6*test.indexBlockSize), now.Add(-test.indexBlockSize)
+
+	nsIdx := test.index.(*nsIndex)
+	nsIdx.state.Lock()
+	if withTimeout {
+		nsIdx.state.runtimeOpts.defaultQueryTimeout = time.Minute
+	} else {
+		nsIdx.state.runtimeOpts.defaultQueryTimeout = 0
+	}
+
+	currNow := min
+	nowLock := &sync.Mutex{}
+	nsIdx.nowFn = func() time.Time {
+		nowLock.Lock()
+		defer nowLock.Unlock()
+		return currNow
+	}
+	setNow := func(t time.Time) {
+		nowLock.Lock()
+		defer nowLock.Unlock()
+		currNow = t
+	}
+	nsIdx.state.Unlock()
+
+	var (
+		idsPerBlock     = 16
+		expectedResults = make(map[string]doc.Document)
+		blockStarts     []time.Time
+		blockIdx        = -1
+	)
+	for st := min; !st.After(max); st = st.Add(test.indexBlockSize) {
+		blockIdx++
+		blockStarts = append(blockStarts, st)
+
+		mutableBlockTime := st.Add(test.indexBlockSize).Add(-1 * (test.blockSize / 2))
+		setNow(mutableBlockTime)
+
+		var onIndexWg sync.WaitGroup
+		onIndexWg.Add(idsPerBlock)
+		onIndexSeries := index.NewMockOnIndexSeries(ctrl)
+		onIndexSeries.EXPECT().
+			OnIndexSuccess(gomock.Any()).
+			Times(idsPerBlock).
+			Do(func(arg interface{}) {
+				onIndexWg.Done()
+			})
+		onIndexSeries.EXPECT().
+			OnIndexFinalize(gomock.Any()).
+			Times(idsPerBlock)
+
+		batch := index.NewWriteBatch(index.WriteBatchOptions{
+			InitialCapacity: idsPerBlock,
+			IndexBlockSize:  test.indexBlockSize,
+		})
+		for i := 0; i < idsPerBlock; i++ {
+			id := fmt.Sprintf("foo.block_%d.id_%d", blockIdx, i)
+			doc := doc.Document{
+				ID: []byte(id),
+				Fields: []doc.Field{
+					{
+						Name:  []byte("bar"),
+						Value: []byte(fmt.Sprintf("baz.%d", i)),
+					},
+					{
+						Name:  []byte("qux"),
+						Value: []byte(fmt.Sprintf("qaz.%d", i)),
+					},
+				},
+			}
+			expectedResults[id] = doc
+			batch.Append(index.WriteBatchEntry{
+				Timestamp:     mutableBlockTime,
+				OnIndexSeries: onIndexSeries,
+			}, doc)
+		}
+
+		err := test.index.WriteBatch(batch)
+		require.NoError(t, err)
+		onIndexWg.Wait()
+	}
+
+	var (
+		startWg, readyWg sync.WaitGroup
+		queryConcurrency = 16
+		workerLoops      = 16
+	)
+
+	var enqueueWg sync.WaitGroup
+	query, err := idx.NewRegexpQuery([]byte("bar"), []byte("baz.*"))
+	require.NoError(t, err)
+	startWg.Add(1)
+	for i := 0; i < queryConcurrency; i++ {
+		readyWg.Add(1)
+		enqueueWg.Add(1)
+		go func() {
+			defer enqueueWg.Done()
+			readyWg.Done()
+			startWg.Wait()
+
+			for j := 0; j < workerLoops; j++ {
+				rangeStart := min
+				for k := 0; k < len(blockStarts); k++ {
+					rangeEnd := blockStarts[k].Add(test.indexBlockSize)
+
+					ctx := context.NewContext()
+					results, err := test.index.Query(ctx, index.Query{
+						Query: query,
+					}, index.QueryOptions{
+						StartInclusive: rangeStart,
+						EndExclusive:   rangeEnd,
+					})
+
+					assert.NoError(t, err)
+
+					// Read the results concurrently too
+					hits := make(map[string]struct{}, results.Results.Size())
+					for _, entry := range results.Results.Map().Iter() {
+						id := entry.Key().String()
+
+						doc, err := convert.FromMetricNoClone(entry.Key(), entry.Value())
+						assert.NoError(t, err)
+						if err != nil {
+							continue // this will fail the test anyway, but don't want to panic
+						}
+
+						expectedDoc, ok := expectedResults[id]
+						assert.True(t, ok)
+						if !ok {
+							continue // this will fail the test anyway, but don't want to panic
+						}
+
+						assert.Equal(t, expectedDoc, doc)
+						hits[id] = struct{}{}
+					}
+					expectedHits := idsPerBlock * (k + 1)
+					assert.Equal(t, len(hits), expectedHits)
+
+					// Now safe to close the context after reading results
+					ctx.Close()
+				}
+			}
+		}()
+	}
+
+	// Wait for all routines to be ready then start
+	readyWg.Wait()
+	startWg.Done()
+
+	// Wait until done
+	enqueueWg.Wait()
+}
+
+type testIndex struct {
+	index          namespaceIndex
+	metadata       namespace.Metadata
+	opts           Options
+	ropts          retention.Options
+	blockSize      time.Duration
+	indexBlockSize time.Duration
+	retention      time.Duration
+}
+
+func newTestIndex(t *testing.T, ctrl *gomock.Controller) testIndex {
+	blockSize := time.Hour
+	indexBlockSize := 2 * time.Hour
+	retentionPeriod := 24 * time.Hour
+	ropts := retention.NewOptions().
+		SetBlockSize(blockSize).
+		SetRetentionPeriod(retentionPeriod).
+		SetBufferPast(blockSize / 2)
+	nopts := namespace.NewOptions().
+		SetRetentionOptions(ropts).
+		SetIndexOptions(namespace.NewIndexOptions().SetBlockSize(indexBlockSize))
+	md, err := namespace.NewMetadata(ident.StringID("testns"), nopts)
+	require.NoError(t, err)
+	opts := testDatabaseOptions()
+	index, err := newNamespaceIndex(md, opts)
+	require.NoError(t, err)
+
+	return testIndex{
+		index:          index,
+		metadata:       md,
+		opts:           opts,
+		blockSize:      blockSize,
+		indexBlockSize: indexBlockSize,
+		retention:      retentionPeriod,
+	}
 }
