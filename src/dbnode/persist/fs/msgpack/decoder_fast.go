d57d79e59112b47c71e3a0c36fd99c000eeaf2fc
Richard Artoul <rartoul@uber.com>
2018-12-06 16:31:49 -0500

    Implement DecodeLogEntryFast

--- src/dbnode/persist/fs/msgpack/decoder_fast.go
+++ src/dbnode/persist/fs/msgpack/decoder_fast.go
@@ -23,6 +23,7 @@ package msgpack
 import (
 	"errors"
 	"fmt"
+	"math"
 
 	"github.com/m3db/m3/src/dbnode/persist/schema"
 
@@ -47,60 +48,77 @@ import (
 // degradation in this functions performance can have a substantial impact on M3DB.
 func DecodeLogEntryFast(b []byte) (schema.LogEntry, error) {
 	schema := schema.LogEntry{}
-	// decodeRootObject
-	//     version
-	_, b, err := decodeInt(b)
-	if err != nil {
-		return schema, err
-	}
-	// decodeRootObject
-	//     numFieldsToSkip
-	//          numObjectFields
-	_, b, err = decodeArrayLen(b)
-	if err != nil {
-		return schema, err
-	}
-	// decodeRootObject
-	//     numFieldsToSkip
-	//        objectType
-	_, b, err = decodeInt(b)
+	b = b[len(logEntryHeader):]
 
-	// decodeLogEntry
-	//     index int
+	var err error
 	schema.Index, b, err = decodeUint(b)
-	// decodeLogEntry
-	//     create int
-	schema.Create, b, err = decodeInt(b)
-	// decodeLogEntry
-	//     metadata (bytes)
-	// decodeLogEntry
-	//     value (float64)
-	// decodeLogEntry
-	//     unit (int)
-	// decodeLogEntry
-	//     annotation (bytes)
-	panic(objectType(ot))
+	if err != nil {
+		return schema, err
+	}
 
-	return schema.LogEntry{}, nil
+	schema.Create, b, err = decodeInt(b)
+	if err != nil {
+		return schema, err
+	}
+	schema.Metadata, b, err = decodeBytes(b)
+	if err != nil {
+		return schema, err
+	}
+
+	schema.Timestamp, b, err = decodeInt(b)
+	if err != nil {
+		return schema, err
+	}
+
+	schema.Value, b, err = decodeFloat64(b)
+	if err != nil {
+		return schema, err
+	}
+
+	unit, b, err := decodeUint(b)
+	if err != nil {
+		return schema, err
+	}
+	schema.Unit = uint32(unit)
+
+	schema.Annotation, b, err = decodeBytes(b)
+	if err != nil {
+		return schema, err
+	}
+
+	return schema, nil
 }
 
 // DecodeLogMetadataFast is the same as EncodeLogEntryFast except for the metadata
 // entries instead of the data entries.
-func DecodeLogMetadataFast(b []byte, entry schema.LogMetadata) ([]byte, error) {
-	if logMetadataHeaderErr != nil {
-		return nil, logMetadataHeaderErr
+func DecodeLogMetadataFast(b []byte) (schema.LogMetadata, error) {
+	metadata := schema.LogMetadata{}
+
+	b = b[len(logMetadataHeader):]
+
+	id, b, err := decodeBytes(b)
+	if err != nil {
+		return metadata, err
+	}
+	metadata.ID = id
+
+	metadata.Namespace, b, err = decodeBytes(b)
+	if err != nil {
+		return metadata, err
 	}
 
-	// TODO(rartoul): Can optimize this further by storing the version as part of the
-	// info for the commit log file itself, instead of in every entry.
-	// https://github.com/m3db/m3/issues/1161
-	b = append(b, logMetadataHeader...)
-	b = encodeBytes(b, entry.ID)
-	b = encodeBytes(b, entry.Namespace)
-	b = encodeVarUint64(b, uint64(entry.Shard))
-	b = encodeBytes(b, entry.EncodedTags)
+	shard, b, err := decodeUint(b)
+	if err != nil {
+		return metadata, err
+	}
+	metadata.Shard = uint32(shard)
 
-	return b, nil
+	metadata.EncodedTags, b, err = decodeBytes(b)
+	if err != nil {
+		return metadata, err
+	}
+
+	return metadata, nil
 }
 
 func decodeArrayLen(b []byte) (int, []byte, error) {
@@ -131,9 +149,10 @@ func decodeInt(b []byte) (int64, []byte, error) {
 
 	switch c {
 	case codes.Uint8:
-		return int64(b[1]), b[1:], nil
+		// Zero?
+		return int64(b[0]), b[1:], nil
 	case codes.Int8:
-		return int64(int8(b[1])), b[1:], nil
+		return int64(int8(b[0])), b[1:], nil
 	case codes.Uint16:
 		return int64((uint16(b[0]) << 8) | uint16(b[1])), b[2:], nil
 	case codes.Int16:
@@ -176,9 +195,9 @@ func decodeUint(b []byte) (uint64, []byte, error) {
 
 	switch c {
 	case codes.Uint8:
-		return uint64(b[1]), b[1:], nil
+		return uint64(b[0]), b[1:], nil
 	case codes.Int8:
-		return uint64(int8(b[1])), b[1:], nil
+		return uint64(int8(b[0])), b[1:], nil
 	case codes.Uint16:
 		return uint64((uint16(b[0]) << 8) | uint16(b[1])), b[2:], nil
 	case codes.Int16:
@@ -207,34 +226,65 @@ func decodeUint(b []byte) (uint64, []byte, error) {
 	}
 }
 
-func decodeBytes(b []byte) ([]byte, int, int) {
-	// If we need to allocate new space for decoded byte slice, we delegate it to msgpack
-	// API which allocates a new slice under the hood, otherwise we simply locate the byte
-	// slice as part of the encoded byte stream and return it
-	var (
-		bytesLen     = dec.decodeBytesLen()
-		backingBytes = dec.reader.Bytes()
-		numBytes     = len(backingBytes)
-		currPos      = int(int64(numBytes) - dec.reader.Remaining())
-	)
+func decodeFloat64(b []byte) (float64, []byte, error) {
+	c := b[0]
+	b = b[1:]
 
-	if dec.err != nil {
-		return nil, -1, -1
+	if c == codes.Float {
+		i := (uint32(b[0]) << 24) |
+			(uint32(b[1]) << 16) |
+			(uint32(b[2]) << 8) |
+			uint32(b[3])
+		return float64(math.Float32frombits(i)), b[4:], nil
 	}
-	// NB(xichen): DecodeBytesLen() returns -1 if the byte slice is nil
+
+	if c == codes.Double {
+		i := (uint64(b[0]) << 56) |
+			(uint64(b[1]) << 48) |
+			(uint64(b[2]) << 40) |
+			(uint64(b[3]) << 32) |
+			(uint64(b[4]) << 24) |
+			(uint64(b[5]) << 16) |
+			(uint64(b[6]) << 8) |
+			uint64(b[7])
+		return float64(math.Float64frombits(i)), b[8:], nil
+	}
+
+	return 0, b, fmt.Errorf("invalid code: %d decoding float64", c)
+}
+
+func decodeBytesLen(b []byte) (int, []byte, error) {
+	c := b[0]
+	b = b[1:]
+
+	if c == codes.Nil {
+		return -1, b, nil
+	} else if codes.IsFixedString(c) {
+		return int(c & codes.FixedStrMask), b, nil
+	}
+	switch c {
+	case codes.Str8, codes.Bin8:
+		return int(b[0]), b[1:], nil
+	case codes.Str16, codes.Bin16:
+		return int((uint16(b[0]) << 8) | uint16(b[1])), b[2:], nil
+	case codes.Str32, codes.Bin32:
+		return int(int32((uint32(b[0]) << 24) |
+			(uint32(b[1]) << 16) |
+			(uint32(b[2]) << 8) |
+			uint32(b[3]))), b[4:], nil
+	}
+	return -1, b, fmt.Errorf("invalid code: %d decoding bytes length", c)
+}
+
+func decodeBytes(b []byte) ([]byte, []byte, error) {
+	bytesLen, b, err := decodeBytesLen(b)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	if bytesLen == -1 {
-		return nil, -1, -1
+		return nil, nil, nil
 	}
 
-	targetPos := currPos + bytesLen
-	if bytesLen < 0 || currPos < 0 || targetPos > numBytes {
-		dec.err = fmt.Errorf("invalid currPos %d, bytesLen %d, numBytes %d", currPos, bytesLen, numBytes)
-		return nil, -1, -1
-	}
-	if err := dec.reader.Skip(int64(bytesLen)); err != nil {
-		dec.err = err
-		return nil, -1, -1
-	}
-	value = backingBytes[currPos:targetPos]
-	return value, currPos, bytesLen
+	return b[:bytesLen], b[bytesLen:], nil
 }
