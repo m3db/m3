46438d13883ae081036a3ebb6ca7da000921852a
cw9 <chaow929@gmail.com>
2018-12-17 16:56:15 -0500

    Revert "More retries"
    This reverts commit 8069fd5b4364793fe22389d40bb4a9fce77511d8.

--- scripts/docker-integration-tests/prometheus/test.sh
+++ scripts/docker-integration-tests/prometheus/test.sh
@@ -112,12 +112,12 @@ docker-compose -f ${COMPOSE_FILE} up -d prometheus01
 
 # Ensure Prometheus can proxy a Prometheus query
 echo "Wait until the remote write endpoint generates and allows for data to be queried"
-ATTEMPTS=10 TIMEOUT=2 retry_with_backoff  \
+ATTEMPTS=6 TIMEOUT=2 retry_with_backoff  \
   '[[ $(curl -sSf 0.0.0.0:9090/api/v1/query?query=prometheus_remote_storage_succeeded_samples_total | jq -r .data.result[].value[1]) -gt 100 ]]'
 
 # Make sure we're proxying writes to the unaggregated namespace
 echo "Wait until data begins being written to remote storage for the unaggregated namespace"
-ATTEMPTS=10 TIMEOUT=2 retry_with_backoff  \
+ATTEMPTS=6 TIMEOUT=2 retry_with_backoff  \
   '[[ $(curl -sSf 0.0.0.0:9090/api/v1/query?query=database_write_tagged_success\\{namespace=\"unagg\"\\} | jq -r .data.result[0].value[1]) -gt 0 ]]'
 
 # Make sure we're proxying writes to the aggregated namespace
