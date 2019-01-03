faeb3f8344ef9f417b4caf313b3acdf07485e868
Justin Chan <juchan@uber.com>
2019-01-03 15:45:33 -0500

    Make sure buffer pools get set

--- src/dbnode/storage/options.go
+++ src/dbnode/storage/options.go
@@ -102,7 +102,9 @@ func NewSeriesOptionsFromOptions(opts Options, ropts retention.Options) series.O
 		SetContextPool(opts.ContextPool()).
 		SetEncoderPool(opts.EncoderPool()).
 		SetMultiReaderIteratorPool(opts.MultiReaderIteratorPool()).
-		SetIdentifierPool(opts.IdentifierPool())
+		SetIdentifierPool(opts.IdentifierPool()).
+		SetBufferBucketPool(opts.BufferBucketPool()).
+		SetBufferBucketVersionsPool(opts.BufferBucketVersionsPool())
 }
 
 type options struct {
@@ -138,6 +140,8 @@ type options struct {
 	fetchBlocksMetadataResultsPool block.FetchBlocksMetadataResultsPool
 	queryIDsWorkerPool             xsync.WorkerPool
 	writeBatchPool                 *ts.WriteBatchPool
+	bufferBucketPool               series.BufferBucketPool
+	bufferBucketVersionsPool       series.BufferBucketVersionsPool
 }
 
 // NewOptions creates a new set of storage options with defaults
@@ -609,4 +613,24 @@ func (o *options) SetWriteBatchPool(value *ts.WriteBatchPool) Options {
 
 func (o *options) WriteBatchPool() *ts.WriteBatchPool {
 	return o.writeBatchPool
+}
+
+func (o *options) SetBufferBucketPool(value series.BufferBucketPool) Options {
+	opts := *o
+	opts.bufferBucketPool = value
+	return &opts
+}
+
+func (o *options) BufferBucketPool() series.BufferBucketPool {
+	return o.bufferBucketPool
+}
+
+func (o *options) SetBufferBucketVersionsPool(value series.BufferBucketVersionsPool) Options {
+	opts := *o
+	opts.bufferBucketVersionsPool = value
+	return &opts
+}
+
+func (o *options) BufferBucketVersionsPool() series.BufferBucketVersionsPool {
+	return o.bufferBucketVersionsPool
 }
