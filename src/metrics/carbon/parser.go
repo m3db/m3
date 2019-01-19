fdb8385d0f623e8ef0c8922ad469eb776708fad1
Richard Artoul <rartoul@uber.com>
2019-01-19 16:19:55 -0500

    copy over carbon package without migration code

---
+++ src/metrics/carbon/parser.go
@@ -0,0 +1,244 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package carbon
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+	"unicode/utf8"
+
+	"code.uber.internal/infra/statsdex/protocols/tools"
+)
+
+const (
+	negativeNanStr = "-nan"
+	nanStr         = "nan"
+
+	floatFormatByte = 'f'
+	floatPrecision  = -1
+	floatBitSize    = 64
+	intBase         = 10
+)
+
+var (
+	errInvalidLine      = errors.New("invalid line")
+	errNotUTF8          = errors.New("not valid UTF8 string")
+	recvValidTimeBuffer = time.Hour
+	mathNan             = math.NaN()
+)
+
+// Metric represents a carbon metric.
+type Metric struct {
+	Name string
+	Time time.Time
+	Val  float64
+	Recv time.Time
+}
+
+// ToLine converts the carbon Metric struct to a line.
+func (m *Metric) ToLine() string {
+	return m.Name + " " + strconv.FormatFloat(m.Val, floatFormatByte, floatPrecision, floatBitSize) +
+		" " + strconv.FormatInt(m.Time.Unix(), intBase) + "\n"
+}
+
+// ParsePacket parses a carbon packet and returns the metrics and number of malformed lines.
+func ParsePacket(packet string) ([]Metric, int) {
+	var malformed, prevIdx, i int
+	mets := []Metric{}
+	for i = 0; i < len(packet); i++ {
+		if packet[i] == '\n' {
+			if (i - prevIdx) > 1 {
+				name, timestamp, value, err := Parse(packet[prevIdx:i])
+				if err == nil {
+					mets = append(mets, Metric{
+						Name: name,
+						Time: timestamp,
+						Val:  value,
+					})
+				} else {
+					malformed++
+				}
+			}
+			prevIdx = i + 1
+		}
+	}
+
+	if (i - prevIdx) > 1 {
+		name, timestamp, value, err := Parse(packet[prevIdx:i])
+		if err == nil {
+			mets = append(mets, Metric{
+				Name: name,
+				Time: timestamp,
+				Val:  value,
+			})
+		} else {
+			malformed++
+		}
+	}
+
+	return mets, malformed
+}
+
+// ParseName parses out the name portion of a string and returns the
+// name and the remaining portion of the line.
+func ParseName(line string) (name string, rest string, err error) {
+	firstSepIdx := -1
+	for i := 0; i < len(line); i++ {
+		if line[i] == ' ' && !(i != 0 && line[i-1] == ' ') {
+			firstSepIdx = i
+			break
+		}
+	}
+
+	if firstSepIdx == -1 {
+		err = errInvalidLine
+		return
+	}
+
+	name = line[:firstSepIdx]
+	if len(name) == 0 {
+		err = errInvalidLine
+		return
+	}
+
+	nonSpaceIdx := firstSepIdx + 1
+	for nonSpaceIdx < len(line) && line[nonSpaceIdx] == ' ' {
+		nonSpaceIdx++
+	}
+
+	rest = line[nonSpaceIdx:]
+	return
+}
+
+// ParseRemainder parses a line's components (name and remainder) and returns
+// all but the name and returns the timestamp of the metric, its value, the
+// time it was received and any error encountered.
+func ParseRemainder(name, rest string) (timestamp time.Time, value float64, err error) {
+	if !utf8.ValidString(name) || !utf8.ValidString(rest) {
+		err = errNotUTF8
+		return
+	}
+
+	if strings.Contains(rest, "  ") {
+		rest = tools.CondenseDuplicateChars(rest, ' ')
+	}
+
+	firstSecIdx := -1
+
+	lineLen := len(rest)
+	for i := 0; i < lineLen; i++ {
+		if rest[i] == ' ' && !(i != 0 && rest[i-1] == ' ') {
+			firstSecIdx = i
+			break
+		}
+	}
+
+	if firstSecIdx == -1 {
+		// Incorrect number of ' ' chars
+		err = errInvalidLine
+		return
+	}
+
+	tsEndIdx := lineLen
+
+	var tsInSecs int64
+	tsInSecs, err = strconv.ParseInt(rest[firstSecIdx+1:tsEndIdx], 10, 64)
+	if err != nil {
+		err = fmt.Errorf("invalid timestamp %s: %v", rest[firstSecIdx+1:tsEndIdx], err)
+		return
+	}
+	timestamp = time.Unix(tsInSecs, 0)
+
+	if val := strings.ToLower(rest[:firstSecIdx]); val == negativeNanStr || val == nanStr {
+		value = mathNan
+	} else {
+		value, err = strconv.ParseFloat(rest[:firstSecIdx], 64)
+	}
+
+	return
+}
+
+// Parse parses a carbon line into the corresponding parts.
+func Parse(line string) (name string, timestamp time.Time, value float64, err error) {
+	var rest string
+	name, rest, err = ParseName(line)
+	if err != nil {
+		return
+	}
+
+	timestamp, value, err = ParseRemainder(name, rest)
+	return
+}
+
+// A Scanner is used to scan carbon lines from an underlying io.Reader.
+type Scanner struct {
+	scanner   *bufio.Scanner
+	timestamp time.Time
+	path      string
+	value     float64
+	recv      time.Time
+
+	// The number of malformed metrics encountered.
+	MalformedCount int
+}
+
+// NewScanner creates a new carbon scanner.
+func NewScanner(r io.Reader) *Scanner {
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanLines)
+	return &Scanner{scanner: s}
+}
+
+// Scan scans for the next carbon metric. Malformed metrics are skipped but counted.
+func (s *Scanner) Scan() bool {
+	for {
+		if !s.scanner.Scan() {
+			return false
+		}
+
+		var err error
+		if s.path, s.timestamp, s.value, err = Parse(s.scanner.Text()); err != nil {
+			s.MalformedCount++
+			continue
+		}
+
+		return true
+	}
+}
+
+// Metric returns the path, timestamp, and value of the last parsed metric.
+func (s *Scanner) Metric() (string, time.Time, float64) {
+	return s.path, s.timestamp, s.value
+}
+
+// MetricAndRecvTime returns the path, timestamp, value and receive time of the last parsed metric.
+func (s *Scanner) MetricAndRecvTime() (string, time.Time, float64) {
+	return s.path, s.timestamp, s.value
+}
+
+// Err returns any errors in the scan.
+func (s *Scanner) Err() error { return s.scanner.Err() }
---
+++ src/metrics/carbon/parser_test.go
@@ -0,0 +1,192 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package carbon
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	nullTime time.Time
+)
+
+type carbonLine struct {
+	path  string
+	value float64
+	time  time.Time
+	line  string
+}
+
+var testLines = []carbonLine{
+	{"foo.bar.zed", 45565.02, time.Unix(1428951394, 0),
+		"foo.bar.zed 45565.02 1428951394"},
+	{"foo.bar.quad", 10.0, time.Unix(1428951394, 0),
+		"foo.bar.quad  10 1428951394"},
+	{"foo.bar.nan", math.NaN(), time.Unix(1428951394, 0),
+		"foo.bar.nan nan  1428951394"},
+	{"foo.bar.nan", math.NaN(), time.Unix(1428951394, 0),
+		"foo.bar.nan -NaN 1428951394"},
+	{"foo.bar.negative", -18000.00000, time.Unix(1429480924, 0),
+		"foo.bar.negative -18000.000000 1429480924"},
+	{"foo.bar.withrecv", 45565.02, time.Unix(1428951394, 0),
+		"foo.bar.withrecv 45565.02 1428951394"},
+	{"foo.bar.withrecv-seconds", 45565.02, time.Unix(1428951394, 0),
+		"foo.bar.withrecv-seconds 45565.02 1428951394"},
+	{"foo.bar.withrecv-invalid", 45565.02, time.Unix(1428951394, 0),
+		"foo.bar.withrecv-invalid 45565.02 1428951394"},
+}
+
+func TestScannerMetric(t *testing.T) {
+	var buf bytes.Buffer
+	for _, line := range testLines {
+		fmt.Fprintf(&buf, "%s\n", line.line)
+	}
+
+	s := NewScanner(&buf)
+	for _, line := range testLines {
+
+		require.True(t, s.Scan(), "could not parse to line %s", line.line)
+
+		name, ts, value := s.Metric()
+		expectedName, expectedTime, expectedValue :=
+			line.path, line.time, line.value
+		assert.Equal(t, expectedName, name)
+		assert.Equal(t, expectedTime, ts)
+		if math.IsNaN(expectedValue) {
+			require.True(t, math.IsNaN(value))
+		} else {
+			assert.Equal(t, expectedValue, value)
+		}
+	}
+
+	assert.False(t, s.Scan(), "parsed past end of buffer")
+	assert.Nil(t, s.Err())
+	assert.Equal(t, 0, s.MalformedCount)
+}
+
+func TestParse(t *testing.T) {
+	for i := range testLines {
+		name, ts, value, err := Parse(testLines[i].line)
+		require.Nil(t, err, "could not parse %s", testLines[i].line)
+		assert.Equal(t, testLines[i].path, name)
+		assert.Equal(t, testLines[i].time, ts)
+
+		if math.IsNaN(testLines[i].value) {
+			require.True(t, math.IsNaN(value))
+		} else {
+			assert.Equal(t, testLines[i].value, value)
+		}
+
+	}
+}
+
+func TestParseName(t *testing.T) {
+	for _, line := range testLines {
+		name, rest, err := ParseName(line.line)
+		_ = rest
+		assert.Equal(t, line.path, name)
+		// we expect the returned string to have de-duped spaces after ParseName's
+		// initial pass
+		exp := strings.TrimPrefix(line.line[len(line.path)+1:], " ")
+		assert.Equal(t, exp, rest)
+		assert.Nil(t, err)
+	}
+
+	_, _, err := ParseName("thereisnospacehere")
+	assert.NotNil(t, err)
+}
+
+func TestParseErrors(t *testing.T) {
+	assertParseError(t, " ")
+	assertParseError(t, "  ")
+	assertParseError(t, "   ")
+	assertParseError(t, "a ")
+	assertParseError(t, "a 1")
+	assertParseError(t, "a  ")
+	assertParseError(t, "a 1 ")
+	assertParseError(t, "a  1")
+	assertParseError(t, " 1 1")
+	assertParseError(t, "  1")
+	assertParseError(t, "foo")
+	assertParseError(t, "foo bar zed")
+	assertParseError(t, "foo 4394 zed")
+	assertParseError(t, "foo zed 1428951394")
+	assertParseError(t, "foo 4394")
+	assertParseError(t, "foo 4384 1428951394 bar")
+	assertParseError(t, "foo 4384 1428951394 1428951394 bar")
+}
+
+func TestParsePacket(t *testing.T) {
+	mets, malformed := ParsePacket(`
+foo.bar.zed 45565.02 1428951394
+foo.bar.zed NaN 1428951394
+foo.bar.invalid invalid 1428951394
+
+foo.bar.invalid 1428951394`)
+	require.Equal(t, 2, len(mets))
+	require.Equal(t, 2, malformed)
+}
+
+func TestCarbonToLine(t *testing.T) {
+	validateLine(t, "foo.bar.zed 45565.02 1428951394")
+	validateLine(t, "foo.bar.nan NaN 1428951395")
+}
+
+func TestParseValidatesUTF8Encoding(t *testing.T) {
+	start := "statsdex.metrics.indexer.statsdex29-sjc1.metrics.service."
+	end := "gairos-a.unique 4394 1428951394"
+
+	var b []byte
+	b = append(b, []byte(start)...)
+	b = append(b, []byte{byte(255), byte(253)}...)
+	b = append(b, []byte(end)...)
+
+	_, _, _, err := Parse(string(b))
+	if err == nil {
+		t.Fatalf("expected UTF8 error")
+	}
+	assert.Equal(t, err.Error(), "not valid UTF8 string")
+	validateLineError(t, string(b))
+}
+
+func validateLine(t *testing.T, line string) {
+	mets, malformed := ParsePacket(line)
+	require.Equal(t, 0, malformed)
+	require.Equal(t, line+"\n", mets[0].ToLine())
+}
+
+func validateLineError(t *testing.T, line string) {
+	_, malformed := ParsePacket(line)
+	require.Equal(t, 1, malformed)
+}
+
+func assertParseError(t *testing.T, metric string) {
+	_, _, _, err := Parse(metric)
+	assert.NotNil(t, err, "allowed parsing of %s", metric)
+}
