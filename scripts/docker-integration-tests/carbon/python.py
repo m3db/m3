2ff51c8e1e0c40f06eed277f13a11c563670c346
Richard Artoul <rartoul@uber.com>
2019-01-24 17:20:43 -0500

    remove unused file

--- scripts/docker-integration-tests/carbon/python.py
+++
@@ -1,4 +0,0 @@
-import pipes
-rawstring = r's=$(date +%s); curl -sSfg "http://localhost:7201/api/v1/query_range?start=$(( s - 300))&end=$s&step=10&query={__graphite0__='foo',__graphite1__='bar',__graphite2__='baz'}" | jq ".data.result[0].values[][1]==\"1\"" | grep -m 1 true'
-
-print pipes.quote(rawstring)
