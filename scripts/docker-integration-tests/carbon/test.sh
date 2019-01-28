4ba08b0497b00387b37101b8507a7be291ec58d2
Richard Artoul <rartoul@uber.com>
2019-01-28 15:40:24 -0500

    improve carbon integration test

--- scripts/docker-integration-tests/carbon/test.sh
+++ scripts/docker-integration-tests/carbon/test.sh
@@ -19,38 +19,44 @@ trap defer EXIT
 
 setup_single_m3db_node
 
+function read_carbon {
+  target=$1
+  expected_val=$2
+  end=$(date +%s)
+  start=$(($end-1000))
+  RESPONSE=$(curl -sSfg "http://localhost:7201/api/v1/graphite/render?target=$target&from=$start&until=$end")
+  test "$(echo "$RESPONSE" | jq ".[0].datapoints | .[][0] | select(. != null)" | tail -n 1)" = "$expected_val"
+  return $?
+}
+
 echo "Writing out a carbon metric that should use a min aggregation"
 t=$(date +%s)
 # 41 should win out here because min(42,41) == 41.
+# TODO(rartoul): Note that we want to set the bufferPast/bufferFuture values for the
+# downsampler to a reasonably large value to ensure this test is acting correctly.
 echo "foo.min.aggregate.baz 41 $t" | nc 0.0.0.0 7204
 echo "foo.min.aggregate.baz 42 $t" | nc 0.0.0.0 7204
 echo "Attempting to read min aggregated carbon metric"
-ATTEMPTS=10 TIMEOUT=1 retry_with_backoff read_carbon 41
+ATTEMPTS=10 TIMEOUT=1 retry_with_backoff read_carbon foo.min.aggregate.baz 41
 
 echo "Writing out a carbon metric that should not be aggregated"
 t=$(date +%s)
-# 43 should win out here because of M3DB's upsert semantics.
+# 43 should win out here because of M3DB's upsert semantics. While M3DB's upsert
+# semantics are not always guaranteed, it is guaranteed for a minimum time window
+# that is as large as bufferPast/bufferFuture which should be much more than enough
+# time for these two commands to complete.
 echo "foo.min.already-aggregated.baz 42 $t" | nc 0.0.0.0 7204
 echo "foo.min.already-aggregated.baz 43 $t" | nc 0.0.0.0 7204
 echo "Attempting to read unaggregated carbon metric"
-ATTEMPTS=10 TIMEOUT=1 retry_with_backoff read_carbon 43
+ATTEMPTS=10 TIMEOUT=1 retry_with_backoff read_carbon foo.min.already-aggregated.baz 43
 
 echo "Writing out a carbon metric that should should use the default mean aggregation"
 t=$(date +%s)
 # Mean of 10 and 20 is 15.
-echo "foo.min.already-aggregated.baz 10 $t" | nc 0.0.0.0 7204
-echo "foo.min.already-aggregated.baz 20 $t" | nc 0.0.0.0 7204
+# TODO(rartoul): Note that we want to set the bufferPast/bufferFuture values for the
+# downsampler to a reasonably large value to ensure this test is acting correctly.
+echo "foo.min.catch-all.baz 10 $t" | nc 0.0.0.0 7204
+echo "foo.min.catch-all.baz 20 $t" | nc 0.0.0.0 7204
 echo "Attempting to read mean aggregated carbon metric"
-ATTEMPTS=10 TIMEOUT=1 retry_with_backoff read_carbon 15
+ATTEMPTS=10 TIMEOUT=1 retry_with_backoff read_carbon foo.min.catch-all.baz 15
 
-
-echo "Attempting to read carbon metric back"
-function read_carbon {
-  expected_val=$1
-  end=$(date +%s)
-  start=$(($end-1000))
-  RESPONSE=$(curl -sSfg "http://localhost:7201/api/v1/graphite/render?target=foo.bar.*&from=$start&until=$end")
-  test "$(echo "$RESPONSE" | jq ".[0].datapoints | .[][0] | select(. != null)" | tail -n 1)" = "$expected_val"
-  return $?
-}
-ATTEMPTS=10 TIMEOUT=1 retry_with_backoff read_carbon
