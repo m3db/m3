17f0b455d8abf18a2f188c1d5c088d8638bacd32
Artem Nikolayevsky <arnikola@uber.com>
2019-02-13 15:52:22 -0500

    WIP moving shouldWrite up the stack

--- src/dbnode/storage/series/series.go
+++ src/dbnode/storage/series/series.go
@@ -282,11 +282,11 @@ func (s *dbSeries) Write(
 	value float64,
 	unit xtime.Unit,
 	annotation []byte,
-) error {
+) (bool, error) {
 	s.Lock()
-	_, err := s.buffer.Write(ctx, timestamp, value, unit, annotation)
+	shouldWrite, err := s.buffer.Write(ctx, timestamp, value, unit, annotation)
 	s.Unlock()
-	return err
+	return shouldWrite, err
 }
 
 func (s *dbSeries) ReadEncoded(
