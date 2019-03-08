d7c8bc26a47c476111ca158687addbb181ff9e11
Richard Artoul <rartoul@uber.com>
2019-03-08 14:24:02 -0500

    Fix Ostream panic + regression test

--- src/dbnode/storage/series/series_test.go
+++ src/dbnode/storage/series/series_test.go
@@ -24,6 +24,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"sync"
 	"testing"
 	"time"
 
@@ -847,6 +848,55 @@ func TestSeriesWriteReadFromTheSameBucket(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, 3, len(values))
+}
+
+// TestSeriesWriteReadParallel is a regression test that was added to capture panics that might
+// arise when many parallel writes and reads are happening at the same time.
+func TestSeriesWriteReadParallel(t *testing.T) {
+	var (
+		numWorkers        = 100
+		numStepsPerWorker = 100
+		opts              = newSeriesTestOptions()
+		curr              = time.Now()
+		series            = NewDatabaseSeries(ident.StringID("foo"), ident.Tags{}, opts).(*dbSeries)
+	)
+
+	_, err := series.Bootstrap(nil)
+	assert.NoError(t, err)
+
+	ctx := context.NewContext()
+	defer ctx.Close()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for i := 0; i < numStepsPerWorker; i++ {
+			wasWritten, err := series.Write(
+				ctx, curr.Add(time.Duration(i)*time.Nanosecond), float64(i), xtime.Second, nil)
+			if err != nil {
+				panic(err)
+			}
+			if !wasWritten {
+				panic("write failed")
+			}
+		}
+		wg.Done()
+	}()
+
+	// Outer loop so that reads are competing with other reads, not just writes.
+	for j := 0; j < numWorkers; j++ {
+		wg.Add(1)
+		go func() {
+			for i := 0; i < numStepsPerWorker; i++ {
+				_, err := series.ReadEncoded(ctx, curr.Add(-5*time.Minute), curr.Add(time.Minute))
+				if err != nil {
+					panic(err)
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
 
 func TestSeriesCloseNonCacheLRUPolicy(t *testing.T) {
