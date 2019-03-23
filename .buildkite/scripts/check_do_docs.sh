06e0a5726fcbab80df730189f915fba8e17395e9
Matt Schallert <mattschallert@gmail.com>
2019-03-23 12:39:02 -0400

    [ci] push / build docs from BK
    We currently have auto builds set up for our GH pages site, but not
    https://docs.m3db.io which we also link to frequently.
    
    This PR adds a Buildkite step that allows us to build and push docs from
    builds on `master`.

---
+++ .buildkite/scripts/check_do_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ "$BUILDKITE_BRANCH" != "master" ]]; then
+  echo "nothing to do"
+  exit 0
+fi
+
+buildkite-agent pipeline upload .buildkite/docs-build-pipeline.yml
---
+++ .buildkite/scripts/docs_push.sh
@@ -0,0 +1,26 @@
+#!/bin/ash
+# shellcheck shell=dash
+
+set -exo pipefail
+
+if [ "$BUILDKITE_BRANCH" != "master" ]; then
+  echo "nothing to do"
+  exit 0
+fi
+
+mkdir -p "$HOME/.ssh"
+ssh-keyscan github.com >> "$HOME/.ssh/known_hosts"
+git config --local user.email "buildkite@m3db.io"
+git config --local user.name "M3 Buildkite Bot"
+
+rm -rf site
+mkdocs build -e docs/theme -t material
+
+git checkout -t origin/docs
+rm -rf m3db.io/*
+cp -r site/* m3db.io/
+
+git remote -v
+git add m3db.io
+git commit -m "Docs update $(date)"
+git push
