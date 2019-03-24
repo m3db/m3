242c38b5ecacb4848eb1ab9fe932b54fde36889c
Richard Artoul <rartoul@uber.com>
2019-03-24 13:54:41 -0400

    remove TODO

--- src/dbnode/client/config.go
+++ src/dbnode/client/config.go
@@ -281,7 +281,6 @@ func (c Configuration) NewAdminClient(
 	}
 
 	v = v.SetReaderIteratorAllocate(func(r io.Reader) encoding.ReaderIterator {
-		// TODO: need to set this too?
 		intOptimized := m3tsz.DefaultIntOptimizationEnabled
 		return m3tsz.NewReaderIterator(r, nil, intOptimized, encodingOpts)
 	})
