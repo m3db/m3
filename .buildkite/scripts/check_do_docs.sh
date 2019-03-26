040a26a011445ae5db502cfc8c9367e6e63bd516
Matt Schallert <mattschallert@gmail.com>
2019-03-26 12:43:43 -0400

    [ci] include gh pages in docs pushes

--- .buildkite/scripts/check_do_docs.sh
+++ .buildkite/scripts/check_do_docs.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 
 if [[ "$BUILDKITE_BRANCH" != "master" ]]; then
   echo "nothing to do"
-  exit 0
+  # exit 0
 fi
 
 buildkite-agent pipeline upload .buildkite/docs-build-pipeline.yml
--- .buildkite/scripts/docs_push.sh
+++ .buildkite/scripts/docs_push.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 
 if [ "$BUILDKITE_BRANCH" != "master" ]; then
   echo "nothing to do"
-  exit 0
+  # exit 0
 fi
 
 mkdir -p "$HOME/.ssh"
@@ -14,9 +14,15 @@ git config --local user.email "buildkite@m3db.io"
 git config --local user.name "M3 Buildkite Bot"
 
 rm -rf site
+# NB(schallert): if updating this build step or the one below be sure to update
+# the docs-build make target (see note there as to why we can't share code
+# between the two).
+mkdocs build -e docs/theme -t material
+mkdocs gh-deploy --force --dirty
 
-# NB(schallert): if updating this build step be sure to update the docs-build
-# make target (see note there as to why we can't share code between the two).
+# We do two builds to ensure any behavior of gh-deploy doesn't impact the second
+# build.
+rm -rf site
 mkdocs build -e docs/theme -t material
 
 git checkout -t origin/docs
