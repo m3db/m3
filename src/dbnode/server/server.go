3818be61f8616e01208628d91cf66473f1ddbec4
Justin Chan <juchan@uber.com>
2019-03-28 04:26:28 -0400

    Add bucket pooling to test

--- src/dbnode/server/server.go
+++ src/dbnode/server/server.go
@@ -1168,7 +1168,6 @@ func withEncodingAndPoolingOptions(
 
 	writeBatchPool.Init()
 
-	// TODO: Should we default the bucket pool sizes here if policy.Size == 0?
 	bucketPool := series.NewBufferBucketPool(
 		poolOptions(policy.BufferBucketPool, scope.SubScope("buffer-bucket-pool")))
 	bucketVersionsPool := series.NewBufferBucketVersionsPool(
