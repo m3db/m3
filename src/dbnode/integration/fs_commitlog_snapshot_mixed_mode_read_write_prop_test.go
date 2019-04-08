1462e1ed5df3a8d9f78e2714b873f2cdef11c2c3
Justin Chan <juchan@uber.com>
2019-04-08 16:36:00 -0400

    Fix missed WriteOption references

--- src/dbnode/integration/fs_commitlog_snapshot_mixed_mode_read_write_prop_test.go
+++ src/dbnode/integration/fs_commitlog_snapshot_mixed_mode_read_write_prop_test.go
@@ -31,7 +31,6 @@ import (
 
 	"github.com/m3db/m3/src/dbnode/retention"
 	"github.com/m3db/m3/src/dbnode/storage/namespace"
-	"github.com/m3db/m3/src/dbnode/storage/series"
 	"github.com/m3db/m3x/context"
 	xtime "github.com/m3db/m3x/time"
 
@@ -181,7 +180,7 @@ func TestFsCommitLogMixedModeReadWriteProp(t *testing.T) {
 
 						setup.setNowFn(ts)
 
-						err := setup.db.Write(ctx, nsID, dp.series, ts, dp.value, xtime.Second, nil, series.WriteOptions{})
+						err := setup.db.Write(ctx, nsID, dp.series, ts, dp.value, xtime.Second, nil, nil)
 						if err != nil {
 							log.Warnf("error writing series datapoint: %v", err)
 							return false, err
