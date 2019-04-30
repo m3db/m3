91c8063fb78e14ad035c0f3ad227f2bb7f8f9c3f
Richard Artoul <rartoul@uber.com>
2019-04-30 15:28:26 -0400

    factor out encodeZeroValue method

--- src/dbnode/encoding/proto/encoder.go
+++ src/dbnode/encoding/proto/encoder.go
@@ -668,7 +668,6 @@ func (enc *Encoder) encodeProtoValues(m *dynamic.Message) error {
 		return nil
 	}
 
-	fmt.Println("encoding: ", m.String())
 	// Reset for re-use.
 	enc.changedValues = enc.changedValues[:0]
 	changedFields := enc.changedValues
