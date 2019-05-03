0b7b5c909f0ce98832aaaed414c4d5d18ee7c495
Richard Artoul <rartoul@uber.com>
2019-05-03 14:04:30 -0400

    create new file and rename structs

--- src/dbnode/encoding/proto/encoder.go
+++ src/dbnode/encoding/proto/encoder.go
@@ -76,7 +76,7 @@ type Encoder struct {
 	fieldsChangedToDefault []int32
 	marshalBuf             []byte
 
-	unmarshalIter *unmarshalIter
+	unmarshalIter *sortedUnmarshalIter
 
 	hardErr          error
 	hasEncodedSchema bool
