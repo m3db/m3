732916fe874cae114b7c516d8ecbcbd9e43c94bd
Richard Artoul <rartoul@uber.com>
2019-05-05 11:57:53 -0400

    fix broken test

--- src/dbnode/encoding/proto/round_trip_test.go
+++ src/dbnode/encoding/proto/round_trip_test.go
@@ -199,7 +199,7 @@ func TestRoundTripMidStreamSchemaChanges(t *testing.T) {
 	vl2WriteTime := vl1WriteTime.Add(time.Second)
 	err = enc.Encode(ts.Datapoint{Timestamp: vl2WriteTime}, xtime.Second, marshaledVL)
 	require.Equal(t,
-		"proto encoder: error unmarshaling message: encountered unknown field with tag: 6",
+		"proto encoder: error unmarshaling message: encountered unknown field with field number: 6",
 		err.Error())
 
 	enc.SetSchema(namespace.GetTestSchemaDescr(testVL2Schema))
