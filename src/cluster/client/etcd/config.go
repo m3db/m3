992f19be520f4d77ff9b0342e49ea52f1ff6a703
Simon Robb <srobb@uber.com>
2019-08-13 15:06:54 -0400

    Removed obselete param from etcd config

--- src/cluster/client/etcd/config.go
+++ src/cluster/client/etcd/config.go
@@ -97,7 +97,6 @@ type Configuration struct {
 	SDConfig          services.Configuration `yaml:"m3sd"`
 	WatchWithRevision int64                  `yaml:"watchWithRevision"`
 	NewDirectoryMode  *os.FileMode           `yaml:"newDirectoryMode"`
-	Async             bool                   `yaml:"async"`
 }
 
 // NewClient creates a new config service client.
