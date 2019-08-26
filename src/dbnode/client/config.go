7ceb8cdf66adb4480f5e511bb4a8c6dce114c593
Haijun Cao <haijun@uber.com>
2019-08-26 12:40:32 -0700

    remove schema validation from m3db client

--- src/dbnode/client/config.go
+++ src/dbnode/client/config.go
@@ -93,7 +93,6 @@ type Configuration struct {
 type ProtoConfiguration struct {
 	// Enabled specifies whether proto is enabled.
 	Enabled bool `yaml:"enabled"`
-	TestOnly bool `yaml:"testOnly"`
 	// load user schema from client configuration into schema registry
 	// at startup/initialization time.
 	SchemaRegistry map[string]NamespaceProtoSchema `yaml:"schema_registry"`
@@ -324,29 +323,13 @@ func (c Configuration) NewAdminClient(
 
 	if c.Proto != nil && c.Proto.Enabled {
 		v = v.SetEncodingProto(encodingOpts)
-
 		schemaRegistry := namespace.NewSchemaRegistry(true, nil)
-		if c.Proto.TestOnly {
-			// Load schema registry from file.
-			deployID := "fromfile"
-			for nsID, protoConfig := range c.Proto.SchemaRegistry {
-				err = namespace.LoadSchemaRegistryFromFile(schemaRegistry, ident.StringID(nsID), deployID, protoConfig.SchemaFilePath, protoConfig.MessageName)
-				if err != nil {
-					return nil, xerrors.Wrapf(err, "could not load schema registry from file %s for namespace %s", protoConfig.SchemaFilePath, nsID)
-				}
-			}
-		} else {
-			// Load schema registry from m3db metadata store.
-			err := loadSchemaRegistryFromKVStore(schemaRegistry, envCfg.KVStore)
+		// Load schema registry from file.
+		deployID := "fromfile"
+		for nsID, protoConfig := range c.Proto.SchemaRegistry {
+			err = namespace.LoadSchemaRegistryFromFile(schemaRegistry, ident.StringID(nsID), deployID, protoConfig.SchemaFilePath, protoConfig.MessageName)
 			if err != nil {
-				return nil, xerrors.Wrap(err, "could not load schema registry from m3db metadata store")
-			}
-			// Validate the schema deploy ID.
-			for nsID, protoConfig := range c.Proto.SchemaRegistry {
-				_, err := schemaRegistry.GetSchema(ident.StringID(nsID), protoConfig.SchemaDeployID)
-				if err != nil {
-					return nil, xerrors.Wrapf(err, "could not find schema for namespace: %s with schema deploy ID: %s", nsID, protoConfig.SchemaDeployID)
-				}
+				return nil, xerrors.Wrapf(err, "could not load schema registry from file %s for namespace %s", protoConfig.SchemaFilePath, nsID)
 			}
 		}
 		v = v.SetSchemaRegistry(schemaRegistry)
