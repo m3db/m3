35fb9d72856121e32b2ad855590936cbc80d6491
Artem Nikolayevsky <arnikola@uber.com>
2019-09-22 14:01:14 -0400

    test update

--- src/aggregator/aggregator/election_test.go
+++ src/aggregator/aggregator/election_test.go
@@ -21,24 +21,28 @@
 package aggregator
 
 import (
-	"context"
 	"fmt"
 	"math"
 	"testing"
 	"time"
 
-	"github.com/coreos/etcd/integration"
+	"github.com/m3db/m3/src/cluster/integration/etcd"
+
+	"go.uber.org/zap"
+
 	"github.com/m3db/m3/src/cluster/kv"
 	"github.com/m3db/m3/src/cluster/kv/mem"
 	"github.com/m3db/m3/src/cluster/placement"
 	xplacement "github.com/m3db/m3/src/cluster/placement"
 	"github.com/m3db/m3/src/cluster/services"
-	"github.com/m3db/m3/src/cluster/services/leader"
+
+	// "github.com/m3db/m3/src/cluster/services/leader"
 	"github.com/m3db/m3/src/cluster/shard"
 
 	"github.com/stretchr/testify/require"
 )
 
+// newPlacement creates a new placement.
 func newPlacement(
 	id string,
 	numShards int,
@@ -85,17 +89,16 @@ func setPlacement(
 	return err
 }
 
-func newLeaderService(t *testing.T) services.LeaderService {
-	var (
-		testEnvironment     = "testEnv"
-		testServiceName     = "testSvc"
-		testZone            = "testZone"
-		testElectionTTLSecs = 5
-		testClusterSize     = 1
-		etcdClient          = integration.NewClusterV3(t, &integration.ClusterConfig{
-			Size: testClusterSize,
-		}).RandClient()
-	)
+var (
+	testEnvironment     = "testEnv"
+	testServiceName     = "testSvc"
+	testZone            = "testZone"
+	testElectionTTLSecs = 5
+)
+
+func newLeaderService(t *testing.T, kv etcd.EmbeddedKV) services.LeaderService {
+	etcdClient, err := kv.ConfigServiceClient()
+	require.NoError(t, err)
 
 	sid := services.NewServiceID().
 		SetEnvironment(testEnvironment).
@@ -103,13 +106,12 @@ func newLeaderService(t *testing.T) services.LeaderService {
 		SetZone(testZone)
 	eopts := services.NewElectionOptions().
 		SetTTLSecs(testElectionTTLSecs)
-	opts := leader.NewOptions().
-		SetServiceID(sid).
-		SetElectionOpts(eopts)
-
-	svc, err := leader.NewService(etcdClient, opts)
+	svcs, err := etcdClient.Services(nil)
 	require.NoError(t, err)
-	return svc
+
+	leader, err := svcs.LeaderService(sid, eopts)
+	require.NoError(t, err)
+	return leader
 }
 
 func buildElectionManagerOptions(
@@ -159,65 +161,86 @@ func TestElections(t *testing.T) {
 	// Placement setup.
 	placement := newPlacement(instanceID, numShards, shardID)
 	require.NoError(t, setPlacement(placementKey, store, placement))
-	defaultElectionStateChangeTimeout := 10 * time.Second
+	defaultElectionStateChangeTimeout := 30 * time.Second
 
-	leaderService := newLeaderService(t)
+	eOpts := etcd.
+		NewOptions().
+		SetZone(testZone).
+		SetEnvironment(testEnvironment).
+		SetServiceID(testServiceName)
+	kv, err := etcd.New(eOpts)
+	require.NoError(t, err)
+	// etcdClient, err := kv.ConfigServiceClient()
+	require.NoError(t, err)
+
+	require.NoError(t, kv.Start())
+	leaderService := newLeaderService(t, kv)
 	electionManagerOpts := buildElectionManagerOptions(t, placementKey, store,
 		instanceID, electionKeyFormat, shardID, leaderService)
 
 	for i := 0; i < count; i++ {
+		lg := electionManagerOpts.InstrumentOptions().Logger()
+		lg = lg.With(zap.Int("instance", i))
+		instrumentOptions := electionManagerOpts.InstrumentOptions().SetLogger(lg)
+
+		opts := electionManagerOpts.SetInstrumentOptions(instrumentOptions)
 		electionManagers = append(electionManagers,
-			NewElectionManager(electionManagerOpts))
+			NewElectionManager(opts))
 	}
 
 	for _, manager := range electionManagers {
+		//?
 		manager.Open(shardID)
 	}
 
 	electionKey := fmt.Sprintf(electionKeyFormat, shardID)
 
 	wait := func() error {
+		time.Sleep(time.Second * 10)
+		return nil
 		deadline := time.Now().Add(defaultElectionStateChangeTimeout)
 		var err error
+		var str string
+		fmt.Println("starting to wait", time.Now().Format("3:04:05PM"), deadline.Format("3:04:05PM"))
 		for time.Now().Before(deadline) {
-			_, err = leaderService.Leader(electionKey)
+			str, err = leaderService.Leader(electionKey)
+			fmt.Println("waiting", err, str, time.Now().Format("3:04:05PM"), deadline.Format("3:04:05PM"))
 			if err == nil {
-				break
+				fmt.Println(str)
+				return nil
 			}
 
 			time.Sleep(time.Second)
+			fmt.Println("waiting2", err, str, time.Now().Format("3:04:05PM"), deadline.Format("3:04:05PM"))
 		}
 
 		return err
 	}
 
-	runs := 100
+	runs := 5
 	for j := 0; j < runs; j++ {
 		require.NoError(t, wait())
 
 		counts := 0
-		index := -1
 		for i, manager := range electionManagers {
 			state := manager.ElectionState()
 			if state == LeaderState {
 				fmt.Println("Found a leader state", i)
 				counts++
-				index = i
 			}
 
 			fmt.Println("election manager", i, manager.ElectionState())
 		}
 
 		if counts != 1 {
-			fmt.Println("Multileader!", counts)
+			fmt.Println("Multileader!", j, counts)
 			t.FailNow()
 		}
 
-		fmt.Println("Resigning at", index)
-		require.NoError(t, electionManagers[index].Resign(context.TODO()))
-		fmt.Println("Resigned")
-
-		require.NoError(t, wait())
-
+		fmt.Println("Terminating")
+		require.NoError(t, kv.Close())
+		fmt.Println("Terminated")
+		require.NoError(t, kv.Start())
+		fmt.Println("I'll be back")
 	}
 }
