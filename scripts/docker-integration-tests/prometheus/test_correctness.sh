9f59039e9392a09d5061071e5e63c71033cc4029
Artem <artem.nikol.usa@hotmail.com>
2019-10-15 16:52:41 -0400

    Fix integration test

--- scripts/docker-integration-tests/prometheus/test_correctness.sh
+++ scripts/docker-integration-tests/prometheus/test_correctness.sh
@@ -6,10 +6,10 @@ source $GOPATH/src/github.com/m3db/m3/scripts/docker-integration-tests/common.sh
 function write_metrics {
   NUM=$1
   echo "Writing $NUM metrics to [0.0.0.0:9003]"
-  set +x
+  # set +x
   for (( i=0; i<$NUM; i++ ))
   do
-    curl -X POST 0.0.0.0:$PORT/writetagged -d '{
+    curl -X POST 0.0.0.0:9003/writetagged -d '{
       "namespace": "unagg",
       "id": "{__name__=\"'$METRIC_NAME'\",val=\"'$i'\"}",
       "tags": [
@@ -17,7 +17,7 @@ function write_metrics {
           "name": "__name__",
           "value": "'$METRIC_NAME'"
         },
-        {
+        { 
           "name": "val",
           "value": "'$i'"
         }
@@ -28,7 +28,7 @@ function write_metrics {
       }
     }'
   done
-  set -x
+  # set -x
 }
  
 function test_instantaneous {
@@ -36,17 +36,22 @@ function test_instantaneous {
   EXPECTED_COUNT=$2
   EXPECTED=$3
   RESPONSE=$(curl -sSL "http://localhost:7201/api/v1/query?query=$QUERY")
+  echo $REPONSE | jq .data.result
   ACTUAL_COUNT=$(echo $RESPONSE | jq '.data.result | length')
-  ACTUAL=$(echo $RESPONSE | jq .data.result[].metric.foo | tr "\n" ":")
-  CONCAT=$(echo $EXPECTED | tr " " ":")
+  ACTUAL=$(echo $RESPONSE | jq .data.result[].metric.foo | tr -d "\n")
+  CONCAT=$(echo $EXPECTED | tr -d " ")
   test $ACTUAL_COUNT = $EXPECTED_COUNT && test $ACTUAL = $CONCAT
 }
 
 function test_replace {
+  export t=$(date +%s)
   METRIC_NAME="quail_$t"
-  write_metrics coordinator-cluster-a 5
-  test_instantaneous "label_replace($METRIC_NAME,\"foo\",\"bar_\$1\",\"val\",\"(.*)\")" label_replace 5 "\"bar_0\" \"bar_1\" \"bar_2\" \"bar_3\" \"bar_4\""
-  test_instantaneous "label_replace($METRIC_NAME,\"foo\",\"bar_\$1\",\"val\",\"(.*)\")+0" label_replace 5 "\"bar_0\" \"bar_1\" \"bar_2\" \"bar_3\" \"bar_4\"" 
+  write_metrics 5
+  sleep 1
+  query='label_replace('$METRIC_NAME',"foo","bar_$1","val","(.*)")'
+  test_instantaneous $query 5 "\"bar_0\" \"bar_1\" \"bar_2\" \"bar_3\" \"bar_4\""
+  query='label_replace('$METRIC_NAME',"foo","bar_$1","val","(.*)")-0'
+  test_instantaneous $query 5 "\"bar_0\" \"bar_1\" \"bar_2\" \"bar_3\" \"bar_4\""
 }
 
 function test_correctness {
