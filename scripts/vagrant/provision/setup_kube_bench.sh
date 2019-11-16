e0258c8ffd72e91037f3c6245bad50a970dfd526
Bo Du <bo@chronosphere.io>
2019-11-16 12:04:45 -0500

    Use string delimited array of target write/query URLs.

--- scripts/vagrant/provision/setup_kube_bench.sh
+++ scripts/vagrant/provision/setup_kube_bench.sh
@@ -25,8 +25,13 @@ kind create cluster
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 
 # Populate m3ccord primary/secondary IPs
-perl -pi -e "s/M3COORDINATOR_PRIMARY_IP/$M3COORDINATOR_PRIMARY_IP/" ./manifests/promremotebench-multi.yaml
-perl -pi -e "s/M3COORDINATOR_SECONDARY_IP/$M3COORDINATOR_SECONDARY_IP/" ./manifests/promremotebench-multi.yaml
+M3COORDINATOR_PRIMARY_WRITE_ADDR="http:\/\/$M3COORDINATOR_PRIMARY_IP:7201\/api\/v1\/prom\/remote\/write"
+M3COORDINATOR_SECONDARY_WRITE_ADDR="http:\/\/$M3COORDINATOR_SECONDARY_IP:7201\/api\/v1\/prom\/remote\/write"
+perl -pi -e "s/M3COORDINATOR_WRITE_TARGETS/$M3COORDINATOR_PRIMARY_WRITE_ADDR,$M3COORDINATOR_SECONDARY_WRITE_ADDR/" ./manifests/promremotebench-multi.yaml
+
+M3COORDINATOR_PRIMARY_QUERY_ADDR="http:\/\/$M3COORDINATOR_PRIMARY_IP:7201\/api\/v1\/query_range"
+M3COORDINATOR_SECONDARY_QUERY_ADDR="http:\/\/$M3COORDINATOR_SECONDARY_IP:7201\/api\/v1\/query_range"
+perl -pi -e "s/M3COORDINATOR_QUERY_TARGETS/$M3COORDINATOR_PRIMARY_QUERY_ADDR,$M3COORDINATOR_SECONDARY_QUERY_ADDR/" ./manifests/promremotebench-multi.yaml
 
 # Ready load generator (but don't scale up yet)
 kubectl apply -f ./manifests/promremotebench-multi.yaml
