440818bfab6604465a799567f436999f9d8095a2
Rob Skillington <rob.skillington@gmail.com>
2019-11-27 10:54:22 -0500

    Address feedback

--- src/dbnode/storage/bootstrap/process.go
+++ src/dbnode/storage/bootstrap/process.go
@@ -217,28 +217,26 @@ func (b bootstrapProcess) Run(
 		begin := b.nowFn()
 		res, err := b.bootstrapper.Bootstrap(namespaces)
 		took := b.nowFn().Sub(begin)
+		if err != nil {
+			b.log.Error("bootstrap process error",
+				zap.Duration("took", took),
+				zap.Error(err))
+			return NamespaceResults{}, err
+		}
+
 		for _, entry := range namespaces.Namespaces.Iter() {
 			namespace := entry.Value()
 			nsID := namespace.Metadata.ID()
 
-			var result NamespaceResult
-			if err == nil {
-				// Only can access the result if we didn't receive an error.
-				var ok bool
-				result, ok = res.Results.Get(nsID)
-				if !ok {
-					return NamespaceResults{},
-						fmt.Errorf("result missing for namespace: %v", nsID.String())
-				}
+			result, ok := res.Results.Get(nsID)
+			if !ok {
+				return NamespaceResults{},
+					fmt.Errorf("result missing for namespace: %v", nsID.String())
 			}
 
 			logFields := b.logFields(namespace.Metadata, namespace.Shards,
 				namespace.DataTargetRange.Range, namespace.IndexTargetRange.Range)
-			b.logBootstrapResult(result, logFields, err, took)
-		}
-
-		if err != nil {
-			return NamespaceResults{}, err
+			b.logBootstrapResult(result, logFields, took)
 		}
 
 		bootstrapResult = MergeNamespaceResults(bootstrapResult, res)
@@ -292,7 +290,6 @@ func (b bootstrapProcess) logBootstrapRun(
 func (b bootstrapProcess) logBootstrapResult(
 	result NamespaceResult,
 	logFields []zapcore.Field,
-	err error,
 	took time.Duration,
 ) {
 	logFields = append(logFields,
@@ -301,13 +298,8 @@ func (b bootstrapProcess) logBootstrapResult(
 		logFields = append(logFields,
 			zap.Int("numIndexBlocks", len(result.IndexResult.IndexResults())))
 	}
-	if err != nil {
-		logFields = append(logFields, zap.Error(err))
-		b.log.Info("bootstrap range completed with error", logFields...)
-		return
-	}
 
-	b.log.Info("bootstrap range completed successfully", logFields...)
+	b.log.Info("bootstrap range completed", logFields...)
 }
 
 func (b bootstrapProcess) targetRangesForData(
