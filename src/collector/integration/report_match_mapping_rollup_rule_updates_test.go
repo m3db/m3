fc770876716080aeb6113d0e7e724c713191b699
Paul Schooss <pschooss@uber.com>
2020-01-08 13:12:06 -0500

    Backout change not required

--- src/collector/integration/report_match_mapping_rollup_rule_updates_test.go
+++ src/collector/integration/report_match_mapping_rollup_rule_updates_test.go
@@ -40,7 +40,6 @@ import (
 // rule matching and report metrics correctly when the metrics match against both
 // mapping rules and rollup rules, while the rules are updated at a high frequency.
 func TestReportMatchMappingRollupWithRuleUpdates(t *testing.T) {
-	t.Skip("test needs to be updated to work with ETCD 3.2.28")
 	namespaces := defaultNamespaces()
 	ruleSet := defaultRuleSet()
 	placement, err := defaultStagedPlacementProto()
