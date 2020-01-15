8c8aea196ea4ccbf97608eb404437c4abfb33945
Bo Du <bokwandu@gmail.com>
2020-01-14 21:02:29 -0500

    Shareable Vagrant Scripts && Azure NVMe Disk Support (#2107)
    * Add shared scripts and nvme disk provisioning code.

--- scripts/vagrant/provision/setup_privileged.sh
+++ scripts/vagrant/provision/setup_privileged.sh
@@ -3,14 +3,32 @@
 # Use with Ubuntu 16.x+
 set -xe
 
+apt-get update
+
+DOCKER_CONFIG_FILE=daemon.json
+
+# Mount nvme devices (if present)
+apt-get install -y mhddfs
+DISKS=$(lsblk | grep nvme | awk '{ print $1 }')
+for DISK in $DISKS; do
+    mkfs -t ext4 /dev/$DISK
+    mkdir /mnt/$DISK
+    mount -t ext4 /dev/$DISK /mnt/$DISK
+done
+MOUNTED_DISKS=$(df -h | grep nvme | awk '{ print $6 }' | paste -sd ',' -)
+if [[ "$MOUNTED_DISKS" != "" ]]; then
+    mkdir /mnt/nvme
+    mhddfs $MOUNTED_DISKS /mnt/nvme
+    DOCKER_CONFIG_FILE=daemon_nvme.json
+fi
+
 DOCKER_USER=${DOCKER_USER:-$USER}
 
 # Copy over docker daemon config for azure deployments.
 if [[ "$AZURE_TENANT_ID" != "" ]]; then
-    cp -r /home/$DOCKER_USER/docker /etc/docker
+    mkdir /etc/docker
+    cp /home/$DOCKER_USER/docker/$DOCKER_CONFIG_FILE /etc/docker/daemon.json
 fi
-
-apt-get update
 
 # Install git
 apt-get install -y git
---
+++ scripts/vagrant/shared/tunnel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -xe
+
+if [[ "$USER" == "" ]]; then
+    echo "USER env var not set"
+    exit 1
+fi
+if [[ "$SSH_KEY" == "" ]]; then
+    echo "SSH_KEY env var not set"
+    exit 1
+fi
+if [[ "$IP_ADDRESS" == "" ]]; then
+    echo "IP_ADDRESS env var not set"
+    exit 1
+fi
+
+# Run tunnels
+echo "Tunnelling"
+echo "Grafana available at http://localhost:3333"
+ssh -i $SSH_KEY $USER@$IP_ADDRESS "cd provision && ./run_tunnels.sh" --\
+    -L 3333:localhost:3000 \
+    -L 7201:localhost:7201 \
+    -L 9003:localhost:9003 \
+    -L 9004:localhost:9004 \
