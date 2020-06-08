c808b30ef04a7915e1b4747cda81323d05d0fa62
Rob Skillington <rob@chronosphere.io>
2020-06-08 00:27:08 -0400

    Cleanup

--- src/dbnode/persist/fs/commitlog/read_write_prop_test.go
+++ src/dbnode/persist/fs/commitlog/read_write_prop_test.go
@@ -33,13 +33,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/m3db/m3/src/x/pool"
-	"github.com/m3db/m3/src/x/serialize"
-
 	"github.com/m3db/m3/src/dbnode/ts"
 	"github.com/m3db/m3/src/x/context"
 	"github.com/m3db/m3/src/x/ident"
 	xos "github.com/m3db/m3/src/x/os"
+	"github.com/m3db/m3/src/x/pool"
+	"github.com/m3db/m3/src/x/serialize"
 	xtest "github.com/m3db/m3/src/x/test"
 	xtime "github.com/m3db/m3/src/x/time"
 
