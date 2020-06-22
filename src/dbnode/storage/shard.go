6d73d82f40f69074f89a4742ce9a389970772f7f
Rob Skillington <rob@chronosphere.io>
2020-06-21 23:27:15 -0400

    Add reminder about bootstrap path

--- src/dbnode/storage/shard.go
+++ src/dbnode/storage/shard.go
@@ -1028,6 +1028,12 @@ func (s *dbShard) SeriesReadWriteRef(
 		}, nil
 	}
 
+	// BEFORE MERGE: CONSIDER BELOW FOR NOW OR FOLLOWUP
+	// TODO(r): Probably can't insert series sync otherwise we stall a ton
+	// of writes... need a better solution for bootstrapping.
+	// This is what causes writes to degrade during bootstrap.
+	// This is a note to consider before merging this PR.
+
 	// NB(r): Insert synchronously so caller has access to the series
 	// immediately, otherwise calls to LoadBlock(..) etc on the series itself
 	// may have no effect if a collision with the same series
