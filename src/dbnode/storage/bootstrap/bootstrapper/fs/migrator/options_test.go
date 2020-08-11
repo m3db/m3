d3178bc83dbf7d7aab5c986c490f929811ab9ac9
Nate Broyles <nate@chronosphere.io>
2020-08-11 15:17:01 -0400

    Add migrator -- machinery for executing data migrations

---
+++ src/dbnode/storage/bootstrap/bootstrapper/fs/migrator/options_test.go
@@ -0,0 +1,114 @@
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package migrator
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/m3db/m3/src/dbnode/namespace"
+	"github.com/m3db/m3/src/dbnode/persist/fs"
+	"github.com/m3db/m3/src/dbnode/persist/fs/migration"
+	"github.com/m3db/m3/src/dbnode/storage"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOptionsValidateStorageOptions(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	opts := newTestOptions(ctrl)
+	require.NoError(t, opts.Validate())
+
+	opts = opts.SetStorageOptions(nil)
+	require.Error(t, opts.Validate())
+
+	opts = opts.SetStorageOptions(storage.NewOptions())
+	require.Error(t, opts.Validate())
+}
+
+func TestOptionsValidateMigrationTaskFn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	opts := newTestOptions(ctrl)
+	require.NoError(t, opts.Validate())
+
+	opts = opts.SetMigrationTaskFn(nil)
+	require.Error(t, opts.Validate())
+}
+
+func TestOptionsValidateInfoFilesByNamespace(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	opts := newTestOptions(ctrl)
+	require.NoError(t, opts.Validate())
+
+	opts = opts.SetInfoFilesByNamespace(nil)
+	require.Error(t, opts.Validate())
+}
+
+func TestOptionsValidateMigrationOpts(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	opts := newTestOptions(ctrl)
+	require.NoError(t, opts.Validate())
+
+	opts = opts.SetMigrationOptions(nil)
+	require.Error(t, opts.Validate())
+}
+
+func TestOptionsValidateInstrumentOpts(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	opts := newTestOptions(ctrl)
+	require.NoError(t, opts.Validate())
+
+	opts = opts.SetInstrumentOptions(nil)
+	require.Error(t, opts.Validate())
+}
+
+func TestOptionsValidateFilesystemOpts(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	opts := newTestOptions(ctrl)
+	require.NoError(t, opts.Validate())
+
+	opts = opts.SetFilesystemOptions(nil)
+	require.Error(t, opts.Validate())
+}
+
+func newTestOptions(ctrl *gomock.Controller) Options {
+	mockOpts := storage.NewMockOptions(ctrl)
+	mockOpts.EXPECT().Validate().AnyTimes()
+
+	return NewOptions().
+		SetMigrationTaskFn(func(result fs.ReadInfoFileResult) (migration.NewTaskFn, bool) {
+			return nil, false
+		}).
+		SetInfoFilesByNamespace(make(map[namespace.Metadata]fs.InfoFileResultsPerShard)).
+		SetStorageOptions(mockOpts)
+
+}
---
+++ src/dbnode/storage/bootstrap/bootstrapper/fs/migrator/types.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package migrator
+
+import (
+	"github.com/m3db/m3/src/dbnode/namespace"
+	"github.com/m3db/m3/src/dbnode/persist/fs"
+	"github.com/m3db/m3/src/dbnode/persist/fs/migration"
+	"github.com/m3db/m3/src/dbnode/storage"
+	"github.com/m3db/m3/src/x/instrument"
+)
+
+// MigrationTaskFn decides if a fileset needs to be migrated and if so, returns a function to instantiate that task.
+type MigrationTaskFn func(result fs.ReadInfoFileResult) (migration.NewTaskFn, bool)
+
+// Options represents the options for the migrator.
+type Options interface {
+	// Validate checks that options are valid.
+	Validate() error
+
+	// SetMigrationTaskFn sets the function for determining if the migrator should migrate a fileset.
+	SetMigrationTaskFn(value MigrationTaskFn) Options
+
+	// MigrationTaskFn gets the function for determining if the migrator should migrate a fileset.
+	MigrationTaskFn() MigrationTaskFn
+
+	// SetInfoFilesByNamespaces sets the info file results to operate on keyed by namespace.
+	SetInfoFilesByNamespace(value map[namespace.Metadata]fs.InfoFileResultsPerShard) Options
+
+	// InfoFilesByNamespaces returns the info file results to operate on keyed by namespace.
+	InfoFilesByNamespace() map[namespace.Metadata]fs.InfoFileResultsPerShard
+
+	// SetMigrationOptions sets the migration options.
+	SetMigrationOptions(value migration.Options) Options
+
+	// MigrationOptions returns the migration options.
+	MigrationOptions() migration.Options
+
+	// SetFilesystemOptions sets the filesystem options.
+	SetFilesystemOptions(value fs.Options) Options
+
+	// FileSystemOptions returns the filesystem options.
+	FilesystemOptions() fs.Options
+
+	// SetInstrumentOptions sets the instrument options.
+	SetInstrumentOptions(value instrument.Options) Options
+
+	// InstrumentOptions returns the instrument options.
+	InstrumentOptions() instrument.Options
+
+	// SetStorageOptions sets the storage options.
+	SetStorageOptions(value storage.Options) Options
+
+	// StorageOptions returns the storage options.
+	StorageOptions() storage.Options
+}
