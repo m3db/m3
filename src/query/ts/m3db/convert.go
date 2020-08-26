ba490de7b10566937c3debbfc020b06334c125b5
Ryan Allen <rallen090@gmail.com>
2020-08-26 02:32:08 -0400

    iter options propagate 5

--- src/query/ts/m3db/convert.go
+++ src/query/ts/m3db/convert.go
@@ -94,13 +94,16 @@ func ConvertM3DBSeriesIterators(
 	opts Options,
 ) ([]block.Block, error) {
 	if err := opts.Validate(); err != nil {
+		fmt.Println("INVALID")
 		return nil, err
 	}
 
 	if opts.SplittingSeriesByBlock() {
+		fmt.Println("SPLIT")
 		return convertM3DBSegmentedBlockIterators(result, bounds, opts)
 	}
 
+	fmt.Println("NONSPLIT")
 	return seriesIteratorsToEncodedBlockIterators(result, bounds, opts)
 }
 
