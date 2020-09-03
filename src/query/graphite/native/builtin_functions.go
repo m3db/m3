007e83e39678186f9c09e3af543b1efe3317daf5
teddywahle <69990143+teddywahle@users.noreply.github.com>
2020-09-02 20:14:42 -0400

    Update src/query/graphite/native/builtin_functions.go

--- src/query/graphite/native/builtin_functions.go
+++ src/query/graphite/native/builtin_functions.go
@@ -1919,7 +1919,6 @@ func init() {
 	MustRegisterFunction(mostDeviant)
 	MustRegisterFunction(movingAverage)
 	MustRegisterFunction(movingMedian)
-	MustRegisterFunction(movingSum)
 	MustRegisterFunction(multiplySeries)
 	MustRegisterFunction(nonNegativeDerivative).WithDefaultParams(map[uint8]interface{}{
 		2: math.NaN(), // maxValue
