14d1886c862e42b493038900248eef9a31b9896f
Artem <artem@chronosphere.io>
2020-09-26 08:31:27 -0400

    Trying other test write methods

--- src/dbnode/integration/wide_fetch_test.go
+++ src/dbnode/integration/wide_fetch_test.go
@@ -21,21 +21,95 @@
 package integration
 
 import (
+	"fmt"
+	"io/ioutil"
+	"sort"
 	"testing"
 	"time"
 
 	"github.com/m3db/m3/src/dbnode/integration/generate"
 	"github.com/m3db/m3/src/dbnode/namespace"
 	"github.com/m3db/m3/src/dbnode/persist/fs"
+	"github.com/m3db/m3/src/dbnode/sharding"
 	"github.com/m3db/m3/src/dbnode/storage"
 	"github.com/m3db/m3/src/dbnode/storage/index"
 	"github.com/m3db/m3/src/m3ninx/idx"
 	"github.com/m3db/m3/src/x/context"
 	"github.com/m3db/m3/src/x/ident"
 	xtest "github.com/m3db/m3/src/x/test"
+	xtime "github.com/m3db/m3/src/x/time"
+	"go.uber.org/zap"
 
 	"github.com/stretchr/testify/require"
 )
+
+type shardedIndexChecksum struct {
+	shard     uint32
+	checksums []ident.IndexChecksum
+}
+
+func buildExpectedChecksumsByShard(
+	ids []string,
+	allowedShards []uint32,
+	shardSet sharding.ShardSet,
+) []ident.IndexChecksum {
+	shardedChecksums := make([]shardedIndexChecksum, 0, len(ids))
+	count := 0
+	for i, id := range ids {
+		checksum := ident.IndexChecksum{ID: []byte(id), Checksum: int64(i)}
+		shard := shardSet.Lookup(ident.BytesID([]byte(id)))
+
+		if len(allowedShards) > 0 {
+			shardInUse := false
+			for _, allowed := range allowedShards {
+				if allowed == shard {
+					shardInUse = true
+					break
+				}
+			}
+
+			if !shardInUse {
+				continue
+			}
+		}
+
+		found := false
+		for idx, sharded := range shardedChecksums {
+			if shard != sharded.shard {
+				continue
+			}
+
+			found = true
+			shardedChecksums[idx].checksums = append(sharded.checksums, checksum)
+			break
+		}
+
+		if found {
+			continue
+		}
+
+		count++
+		shardedChecksums = append(shardedChecksums, shardedIndexChecksum{
+			shard:     shard,
+			checksums: []ident.IndexChecksum{checksum},
+		})
+	}
+
+	sort.Slice(shardedChecksums, func(i, j int) bool {
+		return shardedChecksums[i].shard < shardedChecksums[j].shard
+	})
+
+	checksums := make([]ident.IndexChecksum, 0, count)
+	for _, sharded := range shardedChecksums {
+		checksums = append(checksums, sharded.checksums...)
+	}
+
+	return checksums
+}
+
+func writeData(t *testing.T, testSetup TestSetup) {
+
+}
 
 func TestWideFetch(t *testing.T) {
 	if testing.Short() {
@@ -43,149 +117,127 @@ func TestWideFetch(t *testing.T) {
 	}
 
 	var (
-		blockSize = 2 * time.Hour
-		batchSize = 15
-		// seriesCount = 15
+		batchSize   = 15
+		seriesCount = 15
+		blockSize   = time.Hour * 2
 	)
 
-	nsOpts := namespace.NewOptions()
-	retOpts := nsOpts.RetentionOptions().
-		SetRetentionPeriod(2 * time.Hour).SetBlockSize(blockSize)
+	// Test setup
 	idxOpts := namespace.NewIndexOptions().
 		SetEnabled(true).
 		SetBlockSize(time.Hour * 2)
-	nsOpts = nsOpts.
-		SetRetentionOptions(retOpts).
-		SetIndexOptions(idxOpts)
+	nsOpts := namespace.NewOptions().
+		SetIndexOptions(idxOpts).
+		SetRepairEnabled(false).
+		SetRetentionOptions(defaultIntegrationTestRetentionOpts)
 
-	ns1, err := namespace.NewMetadata(testNamespaces[0], nsOpts)
+	nsID := testNamespaces[0]
+	nsMetadata, err := namespace.NewMetadata(nsID, nsOpts)
 	require.NoError(t, err)
 
-	opts := NewTestOptions(t).
-		SetNamespaces([]namespace.Metadata{ns1})
+	testOpts := NewTestOptions(t).
+		SetTickMinimumInterval(time.Second).
+		SetNamespaces([]namespace.Metadata{nsMetadata})
 
-	fsOpts := fs.NewOptions()
+	filePathPrefix, err := ioutil.TempDir("", fmt.Sprintf("integration-test-11"))
+	require.NoError(t, err)
+
+	fsOpts := fs.NewOptions().SetFilePathPrefix(filePathPrefix)
 	decOpts := fsOpts.DecodingOptions().SetIndexEntryHasher(xtest.NewParsedIndexHasher(t))
 	fsOpts = fsOpts.SetDecodingOptions(decOpts)
 
-	// Test setup
-	setup, err := NewTestSetup(t, opts, fsOpts,
+	testSetup, err := NewTestSetup(t, testOpts, fsOpts,
 		func(opt storage.Options) storage.Options {
 			return opt.SetWideBatchSize(batchSize)
 		})
 	require.NoError(t, err)
-	defer setup.Close()
-
-	require.NoError(t, setup.InitializeBootstrappers(InitializeBootstrappersOptions{
-		WithFileSystem: true,
-	}))
+	defer testSetup.Close()
 
 	// Write test data
-	now := setup.NowFn()()
-	blockTime := now //.Truncate(time.Hour).Add(-blockSize)
-	// ids := make([]string, seriesCount)
-	// for i := range ids {
-	// 	// Keep in lex order.
-	// 	padCount := i / 10
-	// 	pad := ""
-	// 	for i := 0; i < padCount; i++ {
-	// 		pad = fmt.Sprintf("%so", pad)
-	// 	}
+	ids := make([]string, seriesCount)
+	for i := range ids {
+		// Keep in lex order.
+		padCount := i / 10
+		pad := ""
+		for i := 0; i < padCount; i++ {
+			pad = fmt.Sprintf("%so", pad)
+		}
 
-	// 	ids[i] = fmt.Sprintf("foo%s-%d", pad, i)
-	// }
-	ids := []string{
-		// "a-1", "b-2", "c-3", "d-4", "e-5", "f-6", "g-7", "h-8", "i-9", "j-10", "k-11", "l-12", "m-13", "n-14", "o-15",
-		"z-1", "y-2", "x-3", "w-4", "v-5", "u-6", "t-7", "s-8", "r-9", "q-10", "p-11", "o-12", "n-13", "m-14", "l-15",
+		ids[i] = fmt.Sprintf("foo%s-%d", pad, i)
 	}
-	tags := ident.NewTags(ident.StringTag("abc", "def"))
-	inputData := []generate.BlockConfig{
-		{IDs: ids, NumPoints: 1, Start: blockTime, Tags: tags},
-	}
-
-	seriesMaps := generate.BlocksByStart(inputData)
-	require.NoError(t, writeTestDataToDisk(ns1, setup, seriesMaps, 0))
 
 	// Start the server with filesystem bootstrapper
-	log := setup.StorageOpts().InstrumentOptions().Logger()
-	log.Debug("filesystem bootstrap test")
-	require.NoError(t, setup.StartServer())
+	log := testSetup.StorageOpts().InstrumentOptions().Logger()
+	log.Debug("wide fetch test")
+	require.NoError(t, testSetup.StartServer())
 	log.Debug("server is now up")
 
 	// Stop the server
 	defer func() {
-		require.NoError(t, setup.StopServer())
+		require.NoError(t, testSetup.StopServer())
 		log.Debug("server is now down")
 	}()
 
+	now := testSetup.NowFn()()
+	// Write the data.
+	tags := ident.NewTags(ident.StringTag("abc", "def"))
+	inputData := []generate.BlockConfig{
+		{IDs: ids, NumPoints: 1, Start: now, Tags: tags},
+	}
+
+	seriesMaps := make(map[xtime.UnixNano]generate.SeriesBlock)
+	for _, input := range inputData {
+		testSetup.SetNowFn(input.Start)
+		testData := generate.Block(input)
+		seriesMaps[xtime.ToUnixNano(input.Start)] = testData
+		fmt.Println("Writing points at", input.Start)
+		require.NoError(t, testSetup.WriteBatch(nsID, testData))
+	}
+
+	log.Debug("test data written")
+	// Advance time to make sure all data are flushed. Because data
+	// are flushed to disk asynchronously, need to poll to check
+	// when data are written.
+	testSetup.SetNowFn(testSetup.NowFn()().Add(blockSize * 2))
+	maxWaitTime := time.Minute
+	require.NoError(t, waitUntilDataFilesFlushed(filePathPrefix, testSetup.ShardSet(), nsID, seriesMaps, maxWaitTime))
+	// Verify on-disk data match what we expect
+	verifyFlushedDataFiles(t, testSetup.ShardSet(), testSetup.StorageOpts(), nsID, seriesMaps)
+	log.Debug("test data has been flushed")
+
+	require.True(t, indexed)
+	log.Info("verified data is indexed", zap.Duration("took", time.Since(start)))
+
 	var (
 		ctx      = context.NewContext()
-		query    = index.Query{Query: idx.NewTermQuery([]byte("abc"), []byte("def"))}
+		query    = index.Query{Query: idx.NewAllQuery()} // idx.NewTermQuery([]byte("abc"), []byte("def"))}
 		iterOpts = index.IterationOptions{}
 	)
 
-	// for i := range ids {
-	// 	id := ids[len(ids)-i-1]
-	// 	require.NoError(t, setup.DB().WriteTagged(context.NewContext(), ns1.ID(),
-	// 		ident.StringID(id), ident.MustNewTagStringsIterator("abc", "def"), now, 1,
-	// 		xtime.Second, nil))
-	// }
-
-	_, err = setup.DB().WideQuery(ctx, ns1.ID(), query, blockTime, nil, iterOpts)
-	// Verify in-memory data match what we expect
+	fmt.Println("Wide querying at", now)
+	chk, err := testSetup.DB().WideQuery(ctx, nsMetadata.ID(), query, now, nil, iterOpts)
+	// Verify data match what we expect
 	require.NoError(t, err)
 
-	// type shardedIndexChecksum struct {
-	// 	shard     uint32
-	// 	checksums []ident.IndexChecksum
-	// }
+	fmt.Println("COMPLETED.", len(chk))
+	for i, c := range chk {
+		fmt.Println(i, c.Checksum, string(c.ID), "Shard:", testSetup.ShardSet().Lookup(ident.BytesID(c.ID)))
+	}
 
-	// shardedChecksums := make([]shardedIndexChecksum, 0, len(ids))
-	// for i, id := range ids {
-	// 	checksum := ident.IndexChecksum{ID: []byte(id), Checksum: int64(i)}
-	// 	shard := setup.ShardSet().Lookup(ident.BytesID([]byte(id)))
-	// 	found := false
-	// 	for idx, sharded := range shardedChecksums {
-	// 		if shard != sharded.shard {
-	// 			continue
-	// 		}
+	qr, err := testSetup.DB().QueryIDs(ctx, nsMetadata.ID(), query, index.QueryOptions{
+		StartInclusive: now.Add(time.Hour * -10),
+		EndExclusive:   now.Add(time.Hour * 10),
+	})
 
-	// 		found = true
-	// 		shardedChecksums[idx].checksums = append(sharded.checksums, checksum)
-	// 		break
-	// 	}
-
-	// 	if found {
-	// 		continue
-	// 	}
-
-	// 	shardedChecksums = append(shardedChecksums, shardedIndexChecksum{
-	// 		shard:     shard,
-	// 		checksums: []ident.IndexChecksum{checksum},
-	// 	})
-	// }
-
-	// for _, s := range shardedChecksums {
-	// 	fmt.Println(" ", s.shard)
-	// 	for _, cs := range s.checksums {
-	// 		fmt.Println("   ", cs.Checksum, string(cs.ID))
-	// 	}
-	// }
-
-	// sort.Slice(shardedChecksums, func(i, j int) bool {
-	// 	return shardedChecksums[i].shard < shardedChecksums[j].shard
-	// })
-
-	// fmt.Println("===============")
-
-	// for _, s := range shardedChecksums {
-	// 	fmt.Println(" ", s.shard)
-	// 	for _, cs := range s.checksums {
-	// 		fmt.Println("   ", cs.Checksum, string(cs.ID))
-	// 	}
-	// }
-
-	// for i, c := range chk {
-	// 	fmt.Println(i, c.Checksum, string(c.ID), "Shard:", setup.ShardSet().Lookup(ident.BytesID(c.ID)))
-	// }
+	require.NoError(t, err)
+	fmt.Println("Querying IDs")
+	for _, v := range qr.Results.Map().Iter() {
+		fmt.Println(v.Key().String())
+		for v.Value().Next() {
+			c := v.Value().Current()
+			fmt.Println(c.Name.String(), ":", c.Value.String())
+		}
+		fmt.Println(v.Value().Err())
+	}
+	fmt.Println("QueryIDs done")
 }
