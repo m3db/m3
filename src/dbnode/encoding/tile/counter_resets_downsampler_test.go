4314727b9b3ba9ac6c781398d3eb0eeadbd48801
Linas Medziunas <linas.medziunas@gmail.com>
2020-10-09 13:25:27 +0300

    Separate counter_resets_downsampler_prop_test

--- src/dbnode/encoding/tile/counter_resets_downsampler_test.go
+++ src/dbnode/encoding/tile/counter_resets_downsampler_test.go
@@ -24,9 +24,6 @@ import (
 	"math"
 	"testing"
 
-	"github.com/leanovate/gopter"
-	"github.com/leanovate/gopter/gen"
-	"github.com/leanovate/gopter/prop"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -255,137 +252,8 @@ func TestDownsampleCounterResetsInvariantsWithPrevFrameLastValue(t *testing.T) {
 	testDownsampleCounterResetsInvariants(t, true)
 }
 
-func testDownsampleCounterResetsInvariants(t *testing.T, usePrevFrameLastValue bool) {
-	params := gopter.DefaultTestParameters()
-	params.MinSize = 1
-	if usePrevFrameLastValue {
-		params.MinSize++
-	}
-	params.MinSuccessfulTests = 10000
-	params.MaxShrinkCount = 0
-	properties := gopter.NewProperties(params)
-	generator := gen.SliceOf(gen.Float64Range(0, 100))
-
-	epsilon := 0.00001
-
-	properties.Property("return consistent indices", prop.ForAll(
-		func(v []float64) bool {
-			results := downsampleFromSlice(v, usePrevFrameLastValue)
-
-			for i := 1; i < len(results); i++ {
-				if results[i].FrameIndex <= results[i-1].FrameIndex {
-					return false
-				}
-			}
-
-			return true
-		},
-		generator,
-	))
-
-	properties.Property("shrink the result", prop.ForAll(
-		func(v []float64) bool {
-			maxResultLength := len(v)
-			if maxResultLength > 4 {
-				maxResultLength = 4
-			}
-
-			results := downsampleFromSlice(v, usePrevFrameLastValue)
-
-			return len(results) <= maxResultLength
-		},
-		generator,
-	))
-
-	properties.Property("preserve the last value", prop.ForAll(
-		func(v []float64) bool {
-			results := downsampleFromSlice(v, usePrevFrameLastValue)
-
-			lastIndex := len(v) - 1
-			lastFrameValue := v[lastIndex]
-			if usePrevFrameLastValue {
-				lastIndex--
-			}
-
-			lastResult := results[len(results)-1]
-
-			return lastResult.Value == lastFrameValue && lastResult.FrameIndex == lastIndex
-		},
-		generator,
-	))
-
-	properties.Property("preserve values after adjusting for counter resets", prop.ForAll(
-		func(v []float64) bool {
-			results := downsampleFromSlice(v, usePrevFrameLastValue)
-
-			downsampledValues := make([]float64, 0, len(results))
-			for _, result := range results {
-				downsampledValues = append(downsampledValues, result.Value)
-			}
-
-			prevFrameLastValue := 0.0
-			input := v
-			if usePrevFrameLastValue {
-				prevFrameLastValue = v[0]
-				input = input[1:]
-			}
-
-			adjustedInput := applyCounterResetAdjustment(prevFrameLastValue, input)
-			adjustedResults := applyCounterResetAdjustment(prevFrameLastValue, downsampledValues)
-
-			for i, result := range results {
-				if math.Abs(adjustedResults[i]-adjustedInput[result.FrameIndex]) > epsilon {
-					return false
-				}
-			}
-
-			return true
-		},
-		generator,
-	))
-
-	properties.TestingRun(t)
-}
-
 func downsample(prevFrameLastValue float64, vals []float64) []DownsampledValue {
 	results := make([]DownsampledValue, 0, 4)
 
 	return DownsampleCounterResets(prevFrameLastValue, vals, results)
-}
-
-func downsampleFromSlice(vals []float64, usePrevFrameLastValue bool) []DownsampledValue {
-	prevFrameLastValue := math.NaN()
-
-	if usePrevFrameLastValue {
-		prevFrameLastValue = vals[0]
-		vals = vals[1:]
-	}
-
-	return downsample(prevFrameLastValue, vals)
-}
-
-func applyCounterResetAdjustment(previousVal float64, vals []float64) []float64 {
-	transformed := make([]float64, 0, len(vals))
-	if len(vals) == 0 {
-		return transformed
-	}
-
-	var (
-		previous    = previousVal
-		accumulated float64
-	)
-
-	for i := 0; i < len(vals); i++ {
-		current := vals[i]
-		delta := current - previous
-		if delta >= 0 {
-			accumulated += delta
-		} else { // a reset
-			accumulated += current
-		}
-		transformed = append(transformed, accumulated)
-		previous = current
-	}
-
-	return transformed
 }
