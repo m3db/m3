a7496e1018cd212aff31f223f5d10242f5c4025d
Linas Naginionis <linas.n@chronosphere.io>
2020-10-21 14:59:16 +0300

    initial implementation

--- src/query/functions/linear/sort.go
+++ src/query/functions/linear/sort.go
@@ -20,13 +20,182 @@
 
 package linear
 
-const (
-	// NB: Because Prometheus's sort and sort_desc only look at the last value,
-	// these functions are essentially noops in M3 as we don't support instant queries.
+import (
+	"fmt"
+	"math"
+	"sort"
 
+	"github.com/m3db/m3/src/query/block"
+	"github.com/m3db/m3/src/query/executor/transform"
+	"github.com/m3db/m3/src/query/functions/utils"
+	"github.com/m3db/m3/src/query/models"
+	"github.com/m3db/m3/src/query/parser"
+)
+
+const (
 	// SortType returns timeseries elements sorted by their values, in ascending order.
 	SortType = "sort"
 
 	// SortDescType is the same as sort, but sorts in descending order.
 	SortDescType = "sort_desc"
 )
+
+type sortOp struct {
+	opType string
+	lessFn lessFn
+}
+
+// OpType for the operator
+func (o sortOp) OpType() string {
+	return o.opType
+}
+
+// String representation
+func (o sortOp) String() string {
+	return fmt.Sprintf("type: %s", o.opType)
+}
+
+type sortNode struct {
+	op         sortOp
+	controller *transform.Controller
+}
+
+type valueAndMeta struct {
+	val        float64
+	seriesMeta block.SeriesMeta
+}
+
+type lessFn func (i, j float64) bool
+
+func asc(i, j float64) bool {
+	return i < j || math.IsNaN(j) && !math.IsNaN(i)
+}
+
+func desc(i, j float64) bool {
+	return i > j || math.IsNaN(j) && !math.IsNaN(i)
+}
+
+// Node creates an execution node
+func (o sortOp) Node(
+	controller *transform.Controller,
+	_ transform.Options,
+) transform.OpNode {
+	return &sortNode{
+		op:         o,
+		controller: controller,
+	}
+}
+
+func (n *sortNode) Params() parser.Params {
+	return n.op
+}
+
+func (n *sortNode) Process(queryCtx *models.QueryContext, ID parser.NodeID, b block.Block) error {
+	return transform.ProcessSimpleBlock(n, n.controller, queryCtx, ID, b)
+}
+
+func (n *sortNode) ProcessBlock(queryCtx *models.QueryContext, ID parser.NodeID, b block.Block) (block.Block, error) {
+	stepIter, err := b.StepIter()
+	if err != nil {
+		return nil, err
+	}
+
+	instantaneous := queryCtx.Options.Instantaneous
+
+	meta := b.Meta()
+	seriesMetas := utils.FlattenMetadata(meta, stepIter.SeriesMeta())
+	stepCount := stepIter.StepCount()
+
+	if !instantaneous {
+		builder, err := n.controller.BlockBuilder(queryCtx, meta, seriesMetas)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := builder.AddCols(stepCount); err != nil {
+			return nil, err
+		}
+
+		for index := 0; stepIter.Next(); index++ {
+			if err := builder.AppendValues(index, stepIter.Current().Values()); err != nil {
+				return nil, err
+			}
+		}
+
+		if err = stepIter.Err(); err != nil {
+			return nil, err
+		}
+		return builder.Build(), nil
+	}
+
+	for index := 0; stepIter.Next(); index++ {
+		if isLastStep(index, stepCount) {
+			//we only care for the last step values for the instant query
+			values := stepIter.Current().Values()
+			valuesToSort := make([]valueAndMeta, len(values))
+			for i, value := range values {
+				valuesToSort[i] = valueAndMeta{
+					val:        value,
+					seriesMeta: seriesMetas[i],
+				}
+			}
+
+			sort.Slice(valuesToSort, func(i, j int) bool {
+				return n.op.lessFn(valuesToSort[i].val, valuesToSort[j].val)
+			})
+
+			for i := range valuesToSort {
+				values[i] = valuesToSort[i].val
+				seriesMetas[i] = valuesToSort[i].seriesMeta
+			}
+
+			//adjust bounds to contain single step
+			time, err := meta.Bounds.TimeForIndex(index)
+			if err != nil {
+				return nil, err
+			}
+			meta.Bounds = models.Bounds{
+				Start:    time,
+				Duration: meta.Bounds.StepSize,
+				StepSize: meta.Bounds.StepSize,
+			}
+
+			blockBuilder, err := n.controller.BlockBuilder(queryCtx, meta, seriesMetas)
+			if err != nil {
+				return nil, err
+			}
+			if err = blockBuilder.AddCols(1); err != nil {
+				return nil, err
+			}
+			if err := blockBuilder.AppendValues(0, values); err != nil {
+				return nil, err
+			}
+			if err = stepIter.Err(); err != nil {
+				return nil, err
+			}
+			return blockBuilder.Build(), nil
+		}
+	}
+
+	return nil, fmt.Errorf("no data to return: %s", n.op.opType)
+}
+
+func isLastStep(stepIndex int, stepCount int) bool {
+	return stepIndex == stepCount-1
+}
+
+func NewSortOp(opType string) (parser.Params, error) {
+	ascending := opType == SortType
+	if !ascending && opType != SortDescType {
+		return nil, fmt.Errorf("operator not supported: %s", opType)
+	}
+
+	var lessFn lessFn
+	if ascending {
+		lessFn = asc
+	} else {
+		lessFn = desc
+	}
+
+	return sortOp{opType, lessFn}, nil
+}
---
+++ src/query/functions/linear/sort_test.go
@@ -0,0 +1,38 @@
+package linear
+
+import (
+	"math"
+	"sort"
+	"testing"
+
+	"github.com/m3db/m3/src/query/test"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestShouldFailWhenOpTypeIsInvalid(t *testing.T) {
+	_, err := NewSortOp("sortAsc")
+	require.Error(t, err)
+}
+
+func TestAscSort(t *testing.T) {
+	actual := []float64{ 5.0, 4.1, math.NaN(), 8.6, 0.1 }
+	expected := []float64{ 0.1, 4.1, 5.0, 8.6, math.NaN() }
+
+	sort.Slice(actual, func(i, j int) bool {
+		return asc(actual[i], actual[j])
+	})
+
+	test.EqualsWithNans(t, expected, actual)
+}
+
+func TestDescSort(t *testing.T) {
+	actual := []float64{ 5.0, 4.1, math.NaN(), 8.6, 0.1 }
+	expected := []float64{ 8.6, 5.0, 4.1, 0.1, math.NaN() }
+
+	sort.Slice(actual, func(i, j int) bool {
+		return desc(actual[i], actual[j])
+	})
+
+	test.EqualsWithNans(t, expected, actual)
+}
