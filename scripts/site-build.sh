e977d94dc41832b8003f17f7948d4a207dd65d4f
ChrisChinchilla <chris@chronosphere.io>
2020-10-21 16:34:01 +0200

    Add openapi generation to build process
    Signed-off-by: ChrisChinchilla <chris@chronosphere.io>

--- scripts/site-build.sh
+++ scripts/site-build.sh
@@ -10,3 +10,6 @@ rsync -a --exclude=*.go "$ASSET_DIR"/* site/static/openapi
 # Create .bak file and then delete it to make sed work for both GNU and Mac versions
 sed -i.bak "s#spec-url='.*'#spec-url='spec.yml'#g" site/static/openapi/index.html
 rm site/static/openapi/index.html.bak
+
+# Now run hugo
+hugo
