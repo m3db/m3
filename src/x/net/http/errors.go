15cfbc4ab7ba2dcf56f9065d6e61aaeadbfa6fc1
Vilius Pranckaitis <vpranckaitis@gmail.com>
2020-11-18 14:42:43 +0200

    fix incorrect content type for m3query/ error responses

--- src/x/net/http/errors.go
+++ src/x/net/http/errors.go
@@ -92,25 +92,30 @@ func WriteError(w http.ResponseWriter, err error, opts ...WriteErrorOption) {
 		fn(&o)
 	}
 
+	statusCode := getStatusCode(err)
+	if o.response == nil {
+		w.Header().Set(HeaderContentType, ContentTypeJSON)
+		w.WriteHeader(statusCode)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
+	} else {
+		w.WriteHeader(statusCode)
+		w.Write(o.response)
+	}
+}
+
+func getStatusCode(err error) int {
 	switch v := err.(type) {
 	case Error:
-		w.WriteHeader(v.Code())
+		return v.Code()
 	case error:
 		if xerrors.IsInvalidParams(v) {
-			w.WriteHeader(http.StatusBadRequest)
+			return http.StatusBadRequest
 		} else if errors.Is(err, context.DeadlineExceeded) {
-			w.WriteHeader(http.StatusGatewayTimeout)
+			return http.StatusGatewayTimeout
 		} else {
-			w.WriteHeader(http.StatusInternalServerError)
+			return http.StatusInternalServerError
 		}
 	default:
-		w.WriteHeader(http.StatusInternalServerError)
+		return http.StatusInternalServerError
 	}
-
-	if o.response != nil {
-		w.Write(o.response)
-		return
-	}
-
-	json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
 }
