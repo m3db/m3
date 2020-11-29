41e3e6c8cebf3c59dbba5397c044bb02fb8d9a11
Linas Medziunas <linas.medziunas@gmail.com>
2020-11-29 22:43:41 +0200

    Update benchmark result

--- src/dbnode/encoding/m3tsz/decoder_benchmark_test.go
+++ src/dbnode/encoding/m3tsz/decoder_benchmark_test.go
@@ -31,7 +31,7 @@ import (
 	"github.com/m3db/m3/src/dbnode/x/xio"
 )
 
-// BenchmarkM3TSZDecode-12    	   10000	    108797 ns/op
+// BenchmarkM3TSZDecode-12    	   16813	     71793 ns/op
 func BenchmarkM3TSZDecode(b *testing.B) {
 	var (
 		encodingOpts = encoding.NewOptions()
