fda143a453be98a8faabf3977929e825e38afb43
Rob Skillington <rob@chronosphere.io>
2020-12-28 13:13:51 -0500

    Fix tests

--- scripts/docker-integration-tests/carbon/test.sh
+++ scripts/docker-integration-tests/carbon/test.sh
@@ -35,8 +35,8 @@ function find_carbon {
   query=$1
   expected_file=$2
   RESPONSE=$(curl -sSg "http://localhost:7201/api/v1/graphite/metrics/find?query=$query")
-  ACTUAL=$(echo $RESPONSE | jq . | sort)
-  EXPECTED=$(cat $EXPECTED_PATH/$expected_file | jq . | sort)
+  ACTUAL=$(echo $RESPONSE | jq '. | sort')
+  EXPECTED=$(cat $EXPECTED_PATH/$expected_file | jq '. | sort')
   if [ "$ACTUAL" == "$EXPECTED" ]
   then
     return 0
@@ -53,18 +53,18 @@ t=$(date +%s)
 echo "foo.min.aggregate.baz 41 $t" | nc 0.0.0.0 7204
 echo "foo.min.aggregate.baz 42 $t" | nc 0.0.0.0 7204
 echo "Attempting to read min aggregated carbon metric"
-ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff read_carbon foo.min.aggregate.baz 41
+ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff "read_carbon 'foo.min.aggregate.baz' 41"
 
 echo "Writing out a carbon metric that should not be aggregated"
 t=$(date +%s)
-# 43 should win out here because of M3DBs upsert semantics. While M3DBs upsert
+# 43 should win out here because of M3DB's upsert semantics. While M3DB's upsert
 # semantics are not always guaranteed, it is guaranteed for a minimum time window
 # that is as large as bufferPast/bufferFuture which should be much more than enough
 # time for these two commands to complete.
 echo "foo.min.already-aggregated.baz 42 $t" | nc 0.0.0.0 7204
 echo "foo.min.already-aggregated.baz 43 $t" | nc 0.0.0.0 7204
 echo "Attempting to read unaggregated carbon metric"
-ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff read_carbon foo.min.already-aggregated.baz 43
+ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff "read_carbon 'foo.min.already-aggregated.baz' 43"
 
 echo "Writing out a carbon metric that should should use the default mean aggregation"
 t=$(date +%s)
@@ -72,17 +72,17 @@ t=$(date +%s)
 echo "foo.min.catch-all.baz 10 $t" | nc 0.0.0.0 7204
 echo "foo.min.catch-all.baz 20 $t" | nc 0.0.0.0 7204
 echo "Attempting to read mean aggregated carbon metric"
-ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff read_carbon foo.min.catch-all.baz 15
+ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff "read_carbon 'foo.min.catch-all.baz' 15"
 
 # Test writing and reading IDs with colons in them.
 t=$(date +%s)
 echo "foo.bar:baz.qux 42 $t" | nc 0.0.0.0 7204
-ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff read_carbon foo.bar:*.* 42
+ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff "read_carbon 'foo.bar:*.*' 42"
 
 # Test writing and reading IDs with a single element.
 t=$(date +%s)
 echo "quail 42 $t" | nc 0.0.0.0 7204
-ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff read_carbon quail 42
+ATTEMPTS=20 MAX_TIMEOUT=4 TIMEOUT=1 retry_with_backoff "read_carbon 'quail' 42"
 
 # Test using "**" in queries
 t=$(date +%s)
