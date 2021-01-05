c41b10ce7fb3ab1b11338ea39f5619440913238c
Vilius Pranckaitis <vpranckaitis@gmail.com>
2021-01-05 13:52:40 +0200

    [dtest] Add a method for Prom remote writes

--- src/cmd/tools/dtest/docker/harness/query_api_test.go
+++ src/cmd/tools/dtest/docker/harness/query_api_test.go
@@ -73,7 +73,9 @@ func testInvalidQueryReturns400(t *testing.T, tests []urlTest) {
 	coord := singleDBNodeDockerResources.Coordinator()
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			assert.NoError(t, coord.RunQuery(verifyResponse(400), tt.url), "for query '%v'", tt.url)
 		})
 	}
