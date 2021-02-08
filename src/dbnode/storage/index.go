9a930dba5db284060b62c71a4982c7c05b2ee5b3
Ryan Hall <ryanhall07@gmail.com>
2021-02-07 19:35:11 -0800

    typo

--- src/dbnode/storage/index.go
+++ src/dbnode/storage/index.go
@@ -1605,12 +1605,12 @@ func (i *nsIndex) queryWithSpan(
 		var timedOut bool
 		if timeLeft := deadline.Sub(i.nowFn()); timeLeft > 0 {
 			wg.Add(1)
-			schedulResult := i.queryWorkersPool.GoWithTimeoutInstrument(func() {
+			scheduleResult := i.queryWorkersPool.GoWithTimeoutInstrument(func() {
 				execBlockFn(ctx, cancellable, block, query, opts, &state, results, logFields)
 				wg.Done()
 			}, timeLeft)
-			totalWaitTime += schedulResult.WaitTime
-			timedOut = !schedulResult.Available
+			totalWaitTime += scheduleResult.WaitTime
+			timedOut = !scheduleResult.Available
 
 			if timedOut {
 				// Did not launch task, need to ensure don't wait for it.
