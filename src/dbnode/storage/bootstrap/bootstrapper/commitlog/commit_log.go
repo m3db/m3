a03007064c9697308dd38322a996b4c274981bd7
λinas <soundvibe@users.noreply.github.com>
2021-02-10 11:33:05 +0200

    [dbnode] Long-term profiling and metrics for bootstrappers (#3076)
    * cpu and heap profiling could be enabled during bootstrapping.
    
    * Added ProfilerOptions to peers options.
    Renamed some methods.
    
    * Move heap profiling to peers bootstrap
    
    * temporary testing gcp upload
    
    * Defined new Profiler interface with FileProfiler implementation.
    
    * tidy up go.sum
    
    * fixed linter
    
    * instrumentations added for uninitialized and commit log bootstrappers.
    refactored where profiler options are set.
    
    * no need to profile uninitialized bootstrapper.
    
    * changes after review.
    
    * implemented default NoOpProfiler.
    
    * use profiler directly.
    
    * Moved profiler to src/x/instrument.
    Removed ProfilerOptions.
    
    * fixed copyright blockls
    
    * no need for old package comment

--- src/dbnode/storage/bootstrap/bootstrapper/commitlog/commit_log.go
+++ src/dbnode/storage/bootstrap/bootstrapper/commitlog/commit_log.go
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+// Package commitlog implements commit log bootstrapping.
 package commitlog
 
 import (
