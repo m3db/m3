c4cd58bb8cee302abfb5dfa76a78b8d83a91caf1
Ryan Allen <rallen090@gmail.com>
2021-02-18 12:36:06 -0500

    Fix test 2

--- src/query/api/v1/handler/prometheus/native/read_test.go
+++ src/query/api/v1/handler/prometheus/native/read_test.go
@@ -126,10 +126,6 @@ func TestPromReadHandlerWithTimeout(t *testing.T) {
 		err.Error())
 }
 
-func TestWriteReturnedDataLimitedHeader(t *testing.T) {
-
-}
-
 func testPromReadHandlerRead(
 	t *testing.T,
 	resultMeta block.ResultMetadata,
