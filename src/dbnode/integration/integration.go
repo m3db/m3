e2b3c6d7326a3a9128c2fe845a325aed783ee21a
Vilius Pranckaitis <vpranckaitis@gmail.com>
2021-02-24 16:18:59 +0200

    integration test for bootstrapping with missing blocks on disk

--- src/dbnode/integration/integration.go
+++ src/dbnode/integration/integration.go
@@ -353,14 +353,21 @@ func NewDefaultBootstrappableTestSetups( // nolint:gocyclo
 	}
 }
 
+type generatorOptionsFn = func(generate.Options) generate.Options
+
 func writeTestDataToDisk(
 	metadata namespace.Metadata,
 	setup TestSetup,
 	seriesMaps generate.SeriesBlocksByStart,
 	volume int,
+	generatorOptionsFns ...generatorOptionsFn,
 ) error {
 	ropts := metadata.Options().RetentionOptions()
-	writer := generate.NewWriter(setup.GeneratorOptions(ropts))
+	gOpts := setup.GeneratorOptions(ropts)
+	for _, fn := range generatorOptionsFns {
+		gOpts = fn(gOpts)
+	}
+	writer := generate.NewWriter(gOpts)
 	return writer.WriteData(namespace.NewContextFrom(metadata), setup.ShardSet(), seriesMaps, volume)
 }
 
