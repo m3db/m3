9c8ddaedc0ae0ed094a676c0bcd47bf4a085afa6
ChrisChinchilla <chris@chronosphere.io>
2021-02-26 16:42:48 +0100

    Add docs version switch
    Signed-off-by: ChrisChinchilla <chris@chronosphere.io>

--- scripts/site-build.sh
+++ scripts/site-build.sh
@@ -2,6 +2,30 @@
 set -e
 HUGO_DOCKER_IMAGE="klakegg/hugo:ext-alpine"
 ASSET_DIR=${1:-src/query/generated/assets/openapi}
+
+# First generate old versions
+docsVersions=$(git tag -l 'docs/*')
+for docVersion in $docsVersions
+do
+        echo "Building $docVersion"
+        git checkout -q "tags/$docVersion"
+        # Copy over OpenAPI doc.
+        mkdir -p site/static/openapi
+        rsync -a --exclude=*.go "$ASSET_DIR"/* site/static/openapi
+        # Create .bak file and then delete it to make sed work for both GNU and Mac versions
+        sed -i.bak "s#spec-url='.*'#spec-url='spec.yml'#g" site/static/openapi/index.html
+        rm -f site/static/openapi/index.html.bak
+        # Now run hugo
+        if [[ -n "${HUGO_DOCKER:-}" ]]; then
+                docker run --rm -it -v "$PWD"/site:/src "${HUGO_DOCKER_IMAGE}"
+        else
+                cd site
+                hugo -d "public/${docVersion//docs\/}"
+        fi    
+        cd ..    
+done
+
+# Now generate latest
 # Copy over OpenAPI doc.
 mkdir -p site/static/openapi
 rsync -a --exclude=*.go "$ASSET_DIR"/* site/static/openapi
--- site/go.mod
+++ site/go.mod
@@ -2,4 +2,4 @@ module m3-site
 
 go 1.15
 
-require github.com/chronosphereio/victor v0.0.0-20210204163248-3548c3638c6d // indirect
+require github.com/chronosphereio/victor v0.0.0-20210226154119-14f3b8e637c9 // indirect
