e5d2ed1dd8ffd9af3900b9522e6201aa081e92f5
ChrisChinchilla <chris@chronosphere.io>
2021-03-09 15:24:28 +0100

    Store current branch
    Signed-off-by: ChrisChinchilla <chris@chronosphere.io>

--- scripts/site-build.sh
+++ scripts/site-build.sh
@@ -2,28 +2,8 @@
 set -e
 HUGO_DOCKER_IMAGE="klakegg/hugo:ext-alpine"
 ASSET_DIR=${1:-src/query/generated/assets/openapi}
-
 # First generate old versions
-docsVersions=$(git tag -l 'docs/*')
-for docVersion in $docsVersions
-do
-        echo "Building $docVersion"
-        git checkout -q "tags/$docVersion"
-        # Copy over OpenAPI doc.
-        mkdir -p site/static/openapi
-        rsync -a --exclude=*.go "$ASSET_DIR"/* site/static/openapi
-        # Create .bak file and then delete it to make sed work for both GNU and Mac versions
-        sed -i.bak "s#spec-url='.*'#spec-url='spec.yml'#g" site/static/openapi/index.html
-        rm -f site/static/openapi/index.html.bak
-        # Now run hugo
-        if [[ -n "${HUGO_DOCKER:-}" ]]; then
-                docker run --rm -it -v "$PWD"/site:/src "${HUGO_DOCKER_IMAGE}"
-        else
-                cd site
-                hugo -d "public/${docVersion//docs\/}"
-        fi    
-        cd ..    
-done
+DOCS_VERSIONS=$(git tag -l 'docs/*')
 
 # Now generate latest
 # Copy over OpenAPI doc.
@@ -37,5 +17,25 @@ if [[ -n "${HUGO_DOCKER:-}" ]]; then
         docker run --rm -it -v "$PWD"/site:/src "${HUGO_DOCKER_IMAGE}"
 else
         cd site
-        hugo
+        hugo -v
+        cd ..
 fi
+
+for docVersion in $DOCS_VERSIONS
+do
+        IFS='/'
+        read -ra version <<< "$docVersion"
+        echo "Building $docVersion"
+        git archive --output "site/${version[1]}.zip" "$docVersion:site/" && unzip -d "site/${version[1]}" "site/${version[1]}.zip" && rm -f "site/${version[1]}.zip"
+
+        # Copy over OpenAPI doc.
+        # mkdir -p site/static/openapi
+        # rsync -a --exclude=*.go "$ASSET_DIR"/* site/static/openapi
+        # # Create .bak file and then delete it to make sed work for both GNU and Mac versions
+        # sed -i.bak "s#spec-url='.*'#spec-url='spec.yml'#g" site/static/openapi/index.html
+        # rm -f site/static/openapi/index.html.bak
+        # echo "public/${docVersion//docs\/}"
+        # Now run hugo
+        cd "site/${version[1]}"
+        hugo -v -d "../public/${version[1]}"   
+done
