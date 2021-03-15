fb216c9b77c08f9054a0f6005d99795e98f1eea4
ChrisChinchilla <chris@chronosphere.io>
2021-03-15 11:28:48 +0100

    Tidy build script
    Signed-off-by: ChrisChinchilla <chris@chronosphere.io>

--- scripts/site-build.sh
+++ scripts/site-build.sh
@@ -2,6 +2,7 @@
 set -e
 HUGO_DOCKER_IMAGE="klakegg/hugo:ext-alpine"
 ASSET_DIR=${1:-src/query/generated/assets/openapi}
+
 # First generate old versions
 DOCS_VERSIONS=$(git tag -l 'docs/*')
 
@@ -12,6 +13,7 @@ rsync -a --exclude=*.go "$ASSET_DIR"/* site/static/openapi
 # Create .bak file and then delete it to make sed work for both GNU and Mac versions
 sed -i.bak "s#spec-url='.*'#spec-url='spec.yml'#g" site/static/openapi/index.html
 rm -f site/static/openapi/index.html.bak
+
 # Now run hugo
 if [[ -n "${HUGO_DOCKER:-}" ]]; then
         docker run -e HUGO_ENV=production -it -v "$PWD"/site:/src "${HUGO_DOCKER_IMAGE}"
@@ -23,18 +25,11 @@ fi
 
 for docVersion in $DOCS_VERSIONS
 do
+        # Add copies of all docs tags
         IFS='/'
         read -ra version <<< "$docVersion"
         echo "Building $docVersion"
         git archive --output "site/${version[1]}.zip" "$docVersion:site/" && unzip -d "site/${version[1]}" "site/${version[1]}.zip" && rm -f "site/${version[1]}.zip"
-
-        # Copy over OpenAPI doc.
-        # mkdir -p site/static/openapi
-        # rsync -a --exclude=*.go "$ASSET_DIR"/* site/static/openapi
-        # # Create .bak file and then delete it to make sed work for both GNU and Mac versions
-        # sed -i.bak "s#spec-url='.*'#spec-url='spec.yml'#g" site/static/openapi/index.html
-        # rm -f site/static/openapi/index.html.bak
-        # echo "public/${docVersion//docs\/}"
 
         # Now run hugo
         if [[ -n "${HUGO_DOCKER:-}" ]]; then
