121215170df208711d4aaabd4e1b7a8158f8c7e6
Linas Medžiūnas <linasm@users.noreply.github.com>
2021-03-16 16:32:48 +0200

    [tools] read_commitlog util (#3359)

--- src/dbnode/persist/fs/commitlog/iterator.go
+++ src/dbnode/persist/fs/commitlog/iterator.go
@@ -47,7 +47,7 @@ type iterator struct {
 	metrics  iteratorMetrics
 	log      *zap.Logger
 	files    []persist.CommitLogFile
-	reader   commitLogReader
+	reader   Reader
 	read     LogEntry
 	err      error
 	setRead  bool
@@ -163,7 +163,7 @@ func (i *iterator) nextReader() bool {
 	file := i.files[0]
 	i.files = i.files[1:]
 
-	reader := newCommitLogReader(commitLogReaderOptions{
+	reader := NewReader(ReaderOptions{
 		commitLogOptions:    i.opts,
 		returnMetadataAsRef: i.iterOpts.ReturnMetadataAsRef,
 	})
