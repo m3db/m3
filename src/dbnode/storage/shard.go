4b1c457972fe5ffbfc277dc68fee6a4987109a10
default <bo@chronosphere.io>
2021-03-18 18:57:02 -0400

    [dbnode] Skip snapshot/flush if shard is not bootstrapped. (#3357)

--- src/dbnode/storage/shard.go
+++ src/dbnode/storage/shard.go
@@ -1322,7 +1322,6 @@ func (s *dbShard) insertSeriesForIndexingAsyncBatched(
 			releaseEntryRef: true,
 		},
 	})
-
 	// i.e. unable to enqueue into shard insert queue
 	if err != nil {
 		entry.OnIndexFinalize(indexBlockStart) // release any reference's we've held for indexing
