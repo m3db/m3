0e5a0e7076764cb1253b4822bc1ee20fb0ef5350
Vilius Pranckaitis <vpranckaitis@gmail.com>
2021-07-15 09:42:00 +0300

    revert comment

--- src/metrics/encoding/protobuf/aggregated_decoder.go
+++ src/metrics/encoding/protobuf/aggregated_decoder.go
@@ -62,8 +62,7 @@ func (d AggregatedDecoder) Value() float64 {
 	return d.pb.Metric.TimedMetric.Value
 }
 
-// Annotation returns the decoded annotation. Note that it might get invalidated when
-// decoders are pooled and thus should be copied on access.
+// Annotation returns the decoded annotation.
 func (d *AggregatedDecoder) Annotation() []byte {
 	return d.pb.Metric.TimedMetric.Annotation
 }
