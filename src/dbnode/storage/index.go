2bc04a678321b77976ba1898ec20dcf6e5e505f1
Rob Skillington <rob@chronosphere.io>
2021-07-27 16:07:04 -0400

    Add blockTickResult changes

--- src/dbnode/storage/index.go
+++ src/dbnode/storage/index.go
@@ -947,7 +947,7 @@ func (i *nsIndex) Tick(
 		result.NumTotalDocs += blockTickResult.NumDocs
 		result.FreeMmap += blockTickResult.FreeMmap
 
-		if tickErr == nil && result.NumSegmentsBootstrapped != 0 {
+		if tickErr == nil && blockTickResult.NumSegmentsBootstrapped != 0 {
 			flushedBlocks = append(flushedBlocks, block.StartTime())
 		}
 	}
