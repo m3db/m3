4ef5da2f3642a00803006940c69af129f26da92a
Nate Broyles <nate@chronosphere.io>
2021-08-26 16:56:19 -0400

    Ensure dbnode debug server shutdown on server shutdown
    This commit calls server.Shutdown for the debug server
    once the main dbnode server is shutdown. This change makes
    it easier to restart a DB node process in-memory without
    causing errors.

--- src/dbnode/server/server.go
+++ src/dbnode/server/server.go
@@ -107,6 +107,7 @@ import (
 const (
 	bootstrapConfigInitTimeout       = 10 * time.Second
 	serverGracefulCloseTimeout       = 10 * time.Second
+	debugServerGracefulCloseTimeout  = 2 * time.Second
 	bgProcessLimitInterval           = 10 * time.Second
 	maxBgProcessLimitMonitorDuration = 5 * time.Minute
 	cpuProfileDuration               = 5 * time.Second
@@ -864,23 +865,8 @@ func Run(runOpts RunOptions) {
 			}
 		}
 
-		go func() {
-			mux := http.DefaultServeMux
-			if debugWriter != nil {
-				if err := debugWriter.RegisterHandler(xdebug.DebugURL, mux); err != nil {
-					logger.Error("unable to register debug writer endpoint", zap.Error(err))
-				}
-			}
-
-			if err := http.ListenAndServe(debugListenAddress, mux); err != nil {
-				logger.Error("debug server could not listen",
-					zap.String("address", debugListenAddress), zap.Error(err))
-			} else {
-				logger.Info("debug server listening",
-					zap.String("address", debugListenAddress),
-				)
-			}
-		}()
+		debugClose := startDebugServer(debugWriter, logger, debugListenAddress)
+		defer debugClose()
 	}
 
 	topo, err := syncCfg.TopologyInitializer.Init()
@@ -1114,7 +1100,36 @@ func Run(runOpts RunOptions) {
 	case <-time.After(closeTimeout):
 		logger.Error("server closed after timeout", zap.Duration("timeout", closeTimeout))
 	}
+}
 
+func startDebugServer(
+	debugWriter xdebug.ZipWriter,
+	logger *zap.Logger,
+	debugListenAddress string,
+) func() {
+	mux := http.DefaultServeMux
+	server := http.Server{Addr: debugListenAddress, Handler: mux}
+
+	if debugWriter != nil {
+		if err := debugWriter.RegisterHandler(xdebug.DebugURL, mux); err != nil {
+			logger.Error("unable to register debug writer endpoint", zap.Error(err))
+		}
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("debug server could not listen",
+				zap.String("address", debugListenAddress), zap.Error(err))
+		}
+	}()
+
+	return func() {
+		ctx, cancel := context.WithTimeout(context.Background(), debugServerGracefulCloseTimeout)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Warn("debug server failed to shutdown gracefully")
+		} else {
+			logger.Info("debug server closed")
 		}
 	}
 }
