c0c1d1d998cb56e6b6571bbdcdb0e335ee396e69
Ryan Allen <rallen090@gmail.com>
2021-09-02 14:58:40 -0400

    Test source 5

--- src/dbnode/storage/index.go
+++ src/dbnode/storage/index.go
@@ -1815,6 +1815,7 @@ func (i *nsIndex) queryWithSpan(
 
 		sp.SetTag("allowedQuota", acquireResult.Permit.AllowedQuota())
 		sp.SetTag("quotaRemaining", acquireResult.Permit.QuotaRemaining())
+		sp.SetTag("waited", acquireResult.Waited)
 		sp.SetTag("source", acquireResult.Source)
 
 		return acquireResult.Permit, waitTime
