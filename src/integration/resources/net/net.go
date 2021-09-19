0d83096cdf38ad77a6f780624c16b448edcb785c
Nate Broyles <nate@chronosphere.io>
2021-09-19 17:20:28 -0400

    [tests] Add coordinator capable of being started in-process.
    This commit adds an implementation of resources.Coordinator
    that is capable of being started in-process as opposed to
    being started via a Docker container.
    
    Additionally, adds helper functions for getting an open
    port and updating a host:port string where the port is 0
    with an actual usuable port.

---
+++ src/integration/resources/net/net.go
@@ -0,0 +1,79 @@
+// Copyright (c) 2021  Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// Package net contains network-related helpers for integration testing.
+package net
+
+import (
+	"net"
+	"strconv"
+)
+
+// GetAvailablePort retrieves an available port on the host machine. This
+// delegates the port selection to the golang net library by starting a server and
+// then checking the port that the server is using.
+func GetAvailablePort() (int, error) {
+	l, err := net.Listen("tcp", ":0") // nolint: gosec
+	if err != nil {
+		return 0, nil
+	}
+	defer l.Close() // nolint: errcheck
+
+	_, p, err := net.SplitHostPort(l.Addr().String())
+	if err != nil {
+		return 0, err
+	}
+	port, err := strconv.Atoi(p)
+	if err != nil {
+		return 0, err
+	}
+	return port, nil
+}
+
+// MaybeGeneratePort takes an address and generates a new version of
+// that address with the port replaced with an open port if the original
+// port was 0. Otherwise, the address is returned unchanged.
+// This method returns the potentially updated address, the potentially
+// updated port, a boolean indicating if the address was changed, and
+// an error in case the method failed
+func MaybeGeneratePort(address string) (string, int, bool, error) {
+	h, p, err := net.SplitHostPort(address)
+	if err != nil {
+		return "", 0, false, err
+	}
+
+	port, err := strconv.Atoi(p)
+	if err != nil {
+		return "", 0, false, err
+	}
+
+	if port == 0 {
+		newPort, err := GetAvailablePort()
+		if err != nil {
+			return address, 0, false, err
+		}
+
+		newAddr := net.JoinHostPort(h, strconv.Itoa(newPort))
+
+		return newAddr, newPort, true, nil
+	}
+
+	return address, port, false, nil
+}
