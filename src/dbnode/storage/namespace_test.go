b7daa74adbc5f87bca76bc1ee0970d22eafab439
Vilius Pranckaitis <vpranckaitis@gmail.com>
2021-10-12 19:34:49 +1100

    [dbnode] Check if namespace is read-only in namespace::FlushIndex (#3833)

--- src/dbnode/storage/namespace_test.go
+++ src/dbnode/storage/namespace_test.go
@@ -100,9 +100,10 @@ func newTestNamespaceWithIDOpts(
 
 func newTestNamespaceWithOpts(
 	t *testing.T,
+	opts namespace.Options,
 	dopts Options,
 ) (*dbNamespace, closerFn) {
-	nsID, opts := defaultTestNs1ID, defaultTestNs1Opts
+	nsID := defaultTestNs1ID
 	metadata := newTestNamespaceMetadataWithIDOpts(t, nsID, opts)
 	hashFn := func(identifier ident.ID) uint32 { return testShardIDs[0].ID() }
 	shardSet, err := sharding.NewShardSet(testShardIDs, hashFn)
@@ -135,7 +136,7 @@ func newTestNamespaceWithTruncateType(
 		SetRuntimeOptionsManager(runtime.NewOptionsManager()).
 		SetTruncateType(truncateType)
 
-	ns, closer := newTestNamespaceWithOpts(t, opts)
+	ns, closer := newTestNamespaceWithOpts(t, defaultTestNs1Opts, opts)
 	ns.reverseIndex = index
 	return ns, closer
 }
@@ -624,14 +625,30 @@ func TestNamespaceFlushDontNeedFlush(t *testing.T) {
 }
 
 func TestNamespaceSkipFlushIfReadOnly(t *testing.T) {
-	ns, closer := newTestNamespace(t)
+	ctrl := xtest.NewController(t)
+	defer ctrl.Finish()
+
+	// Configure the namespace so that flushing would only be enabled/disabled by read-only property.
+	indexOpts := namespace.NewIndexOptions().
+		SetEnabled(true)
+	nsOpts := namespace.NewOptions().
+		SetFlushEnabled(true).
+		SetColdWritesEnabled(true).
+		SetIndexOptions(indexOpts)
+
+	// Set mocked 'OnColdFlush' so that the test would fail if cold flush would happen.
+	opts := DefaultTestOptions().
+		SetOnColdFlush(NewMockOnColdFlush(ctrl)).
+		SetRuntimeOptionsManager(runtime.NewOptionsManager())
+
+	ns, closer := newTestNamespaceWithOpts(t, nsOpts, opts)
 	defer closer()
 
 	ns.bootstrapState = Bootstrapped
 	ns.SetReadOnly(true)
-	err := ns.WarmFlush(xtime.Now(), nil)
-	require.NoError(t, err)
+	require.NoError(t, ns.WarmFlush(xtime.Now(), nil))
 	require.NoError(t, ns.ColdFlush(nil))
+	require.NoError(t, ns.FlushIndex(nil))
 }
 
 func TestNamespaceFlushSkipFlushed(t *testing.T) {
