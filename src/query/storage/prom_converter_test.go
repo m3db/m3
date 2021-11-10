1b949083eaff2cb91bbbff678f14d42a5ee0150c
Linas Medžiūnas <linasm@users.noreply.github.com>
2021-11-10 08:34:49 +0200

    [query] Normalize prom datapoints for low resolution counters (#3909)

--- src/query/storage/prom_converter_test.go
+++ src/query/storage/prom_converter_test.go
@@ -26,6 +26,7 @@ import (
 	"time"
 
 	"github.com/m3db/m3/src/dbnode/encoding"
+	"github.com/m3db/m3/src/dbnode/generated/proto/annotation"
 	dts "github.com/m3db/m3/src/dbnode/ts"
 	"github.com/m3db/m3/src/query/block"
 	"github.com/m3db/m3/src/query/generated/proto/prompb"
@@ -33,9 +34,7 @@ import (
 	"github.com/m3db/m3/src/query/storage/m3/consolidators"
 	"github.com/m3db/m3/src/query/test/seriesiter"
 	"github.com/m3db/m3/src/query/ts"
-	"github.com/m3db/m3/src/x/checked"
 	"github.com/m3db/m3/src/x/ident"
-	"github.com/m3db/m3/src/x/pool"
 	xsync "github.com/m3db/m3/src/x/sync"
 	xtest "github.com/m3db/m3/src/x/test"
 	xtime "github.com/m3db/m3/src/x/time"
@@ -81,7 +80,8 @@ func verifyExpandPromSeries(
 		Warnings:   []block.Warning{{Name: "foo", Message: "bar"}},
 	}
 
-	results, err := SeriesIteratorsToPromResult(context.Background(), fetchResult, pools, nil)
+	results, err := SeriesIteratorsToPromResult(
+		context.Background(), fetchResult, pools, nil, NewPromConvertOptions())
 	assert.NoError(t, err)
 
 	require.NotNil(t, results)
@@ -126,7 +126,8 @@ func TestContextCanceled(t *testing.T) {
 
 	iters := seriesiter.NewMockSeriesIters(ctrl, ident.Tag{}, 1, 2)
 	fetchResult := fr(t, iters, makeTag("foo", "bar", 1)...)
-	_, err = SeriesIteratorsToPromResult(ctx, fetchResult, pool, nil)
+	_, err = SeriesIteratorsToPromResult(
+		ctx, fetchResult, pool, nil, NewPromConvertOptions())
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "context canceled")
 }
@@ -211,40 +212,6 @@ func TestIteratorsToPromResult(t *testing.T) {
 	assert.Equal(t, expected, result)
 }
 
-// overwrite overwrites existing tags with `!!!` literals.
-type overwrite func()
-
-func setupTags(name, value string) (ident.Tags, overwrite) {
-	buckets := []pool.Bucket{{Capacity: 100, Count: 2}}
-	bytesPool := pool.NewCheckedBytesPool(buckets, nil,
-		func(sizes []pool.Bucket) pool.BytesPool {
-			return pool.NewBytesPool(sizes, nil)
-		})
-
-	bytesPool.Init()
-	getFromPool := func(id string) checked.Bytes {
-		pID := bytesPool.Get(len(id))
-		pID.IncRef()
-		pID.AppendAll([]byte(id))
-		pID.DecRef()
-		return pID
-	}
-
-	idPool := ident.NewPool(bytesPool, ident.PoolOptions{})
-	tags := idPool.Tags()
-	tags.Append(idPool.BinaryTag(getFromPool(name), getFromPool(value)))
-	tags.Append(idPool.BinaryTag(getFromPool(name), getFromPool("")))
-	tags.Append(idPool.BinaryTag(getFromPool(""), getFromPool(value)))
-
-	overwrite := func() {
-		tags.Finalize()
-		getFromPool("!!!")
-		getFromPool("!!!")
-	}
-
-	return tags, overwrite
-}
-
 func TestDecodeIteratorsWithEmptySeries(t *testing.T) {
 	ctrl := xtest.NewController(t)
 	defer ctrl.Finish()
@@ -321,7 +288,8 @@ func TestDecodeIteratorsWithEmptySeries(t *testing.T) {
 	}
 
 	opts := models.NewTagOptions()
-	res, err := SeriesIteratorsToPromResult(context.Background(), buildIters(), nil, opts)
+	res, err := SeriesIteratorsToPromResult(
+		context.Background(), buildIters(), nil, opts, NewPromConvertOptions())
 	require.NoError(t, err)
 	verifyResult(t, res)
 
@@ -329,7 +297,205 @@ func TestDecodeIteratorsWithEmptySeries(t *testing.T) {
 	require.NoError(t, err)
 	pool.Init()
 
-	res, err = SeriesIteratorsToPromResult(context.Background(), buildIters(), pool, opts)
+	res, err = SeriesIteratorsToPromResult(
+		context.Background(), buildIters(), pool, opts, NewPromConvertOptions())
 	require.NoError(t, err)
 	verifyResult(t, res)
+}
+
+func TestSeriesIteratorsToPromResultNormalizeLowResCounters(t *testing.T) {
+	t0 := xtime.Now().Truncate(time.Hour)
+
+	tests := []struct {
+		name          string
+		isCounter     bool
+		maxResolution time.Duration
+		given         []dts.Datapoint
+		want          []prompb.Sample
+	}{
+		{
+			name:          "low resolution gauge",
+			isCounter:     false,
+			maxResolution: time.Hour,
+			given: []dts.Datapoint{
+				{TimestampNanos: t0, Value: 1},
+				{TimestampNanos: t0.Add(time.Hour), Value: 2},
+			},
+			want: []prompb.Sample{
+				{Value: 1, Timestamp: ms(t0)},
+				{Value: 2, Timestamp: ms(t0.Add(time.Hour))},
+			},
+		},
+		{
+			name:          "high resolution gauge",
+			isCounter:     false,
+			maxResolution: time.Minute,
+			given: []dts.Datapoint{
+				{TimestampNanos: t0, Value: 1},
+				{TimestampNanos: t0.Add(time.Minute), Value: 2},
+			},
+			want: []prompb.Sample{
+				{Value: 1, Timestamp: ms(t0)},
+				{Value: 2, Timestamp: ms(t0.Add(time.Minute))},
+			},
+		},
+		{
+			name:          "low resolution counter, no datapoints",
+			isCounter:     true,
+			maxResolution: time.Hour,
+		},
+		{
+			name:          "low resolution counter, one datapoint",
+			isCounter:     true,
+			maxResolution: time.Hour,
+			given:         []dts.Datapoint{{TimestampNanos: t0, Value: 1}},
+			want:          []prompb.Sample{{Value: 1, Timestamp: ms(t0)}},
+		},
+		{ // nolint: dupl
+			name:          "high resolution counter with no resets",
+			isCounter:     true,
+			maxResolution: time.Minute,
+			given: []dts.Datapoint{
+				{TimestampNanos: t0, Value: 1},
+				{TimestampNanos: t0.Add(time.Minute), Value: 2},
+				{TimestampNanos: t0.Add(2 * time.Minute), Value: 2},
+				{TimestampNanos: t0.Add(3 * time.Minute), Value: 3},
+			},
+			want: []prompb.Sample{
+				{Value: 1, Timestamp: ms(t0)},
+				{Value: 2, Timestamp: ms(t0.Add(time.Minute))},
+				{Value: 2, Timestamp: ms(t0.Add(2 * time.Minute))},
+				{Value: 3, Timestamp: ms(t0.Add(3 * time.Minute))},
+			},
+		},
+		{ // nolint: dupl
+			name:          "high resolution counter with resets",
+			isCounter:     true,
+			maxResolution: time.Minute,
+			given: []dts.Datapoint{
+				{TimestampNanos: t0, Value: 10},
+				{TimestampNanos: t0.Add(time.Minute), Value: 3},
+				{TimestampNanos: t0.Add(2 * time.Minute), Value: 5},
+				{TimestampNanos: t0.Add(3 * time.Minute), Value: 8},
+			},
+			want: []prompb.Sample{
+				{Value: 10, Timestamp: ms(t0)},
+				{Value: 3, Timestamp: ms(t0.Add(time.Minute))},
+				{Value: 5, Timestamp: ms(t0.Add(2 * time.Minute))},
+				{Value: 8, Timestamp: ms(t0.Add(3 * time.Minute))},
+			},
+		},
+		{ // nolint: dupl
+			name:          "low resolution counter with no resets",
+			isCounter:     true,
+			maxResolution: time.Hour,
+			given: []dts.Datapoint{
+				{TimestampNanos: t0, Value: 1},
+				{TimestampNanos: t0.Add(time.Minute), Value: 2},
+				{TimestampNanos: t0.Add(time.Hour), Value: 2},
+				{TimestampNanos: t0.Add(time.Hour + time.Minute), Value: 3},
+			},
+			want: []prompb.Sample{
+				{Value: 2, Timestamp: ms(t0.Add(time.Minute))},
+				{Value: 3, Timestamp: ms(t0.Add(time.Hour + time.Minute))},
+			},
+		},
+		{ // nolint: dupl
+			name:          "low resolution counter with resets",
+			isCounter:     true,
+			maxResolution: time.Hour,
+			given: []dts.Datapoint{
+				{TimestampNanos: t0, Value: 10},
+				{TimestampNanos: t0.Add(time.Minute), Value: 3},
+				{TimestampNanos: t0.Add(time.Hour), Value: 5},
+				{TimestampNanos: t0.Add(time.Hour + time.Minute), Value: 8},
+			},
+			want: []prompb.Sample{
+				{Value: 13, Timestamp: ms(t0.Add(time.Minute))},
+				{Value: 18, Timestamp: ms(t0.Add(time.Hour + time.Minute))},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testSeriesIteratorsToPromResultNormalize(
+				t, tt.isCounter, tt.maxResolution, tt.given, tt.want)
+		})
+	}
+}
+
+func testSeriesIteratorsToPromResultNormalize(
+	t *testing.T,
+	isCounter bool,
+	maxResolution time.Duration,
+	given []dts.Datapoint,
+	want []prompb.Sample,
+) {
+	ctrl := xtest.NewController(t)
+	defer ctrl.Finish()
+
+	var (
+		gaugePayload   = &annotation.Payload{MetricType: annotation.MetricType_GAUGE}
+		counterPayload = &annotation.Payload{MetricType: annotation.MetricType_COUNTER, HandleValueResets: true}
+	)
+
+	firstAnnotation := annotationBytes(t, gaugePayload)
+	if isCounter {
+		firstAnnotation = annotationBytes(t, counterPayload)
+	}
+
+	iter := encoding.NewMockSeriesIterator(ctrl)
+
+	iter.EXPECT().FirstAnnotation().Return(firstAnnotation).MaxTimes(1)
+	for _, dp := range given {
+		iter.EXPECT().Next().Return(true)
+		iter.EXPECT().Current().Return(dp, xtime.Second, nil)
+	}
+
+	iter.EXPECT().Err().Return(nil)
+	iter.EXPECT().Next().Return(false)
+
+	iter.EXPECT().Tags().Return(ident.EmptyTagIterator)
+
+	verifyResult := func(t *testing.T, expected []prompb.Sample, res PromResult) {
+		timeSeries := res.PromResult.GetTimeseries()
+		if len(expected) == 0 {
+			require.Equal(t, 0, len(timeSeries))
+		} else {
+			require.Equal(t, 1, len(timeSeries))
+			samples := timeSeries[0].Samples
+			require.Equal(t, expected, samples)
+		}
+	}
+
+	iters := []encoding.SeriesIterator{iter}
+
+	it := encoding.NewMockSeriesIterators(ctrl)
+	it.EXPECT().Iters().Return(iters).AnyTimes()
+	it.EXPECT().Len().Return(len(iters)).AnyTimes()
+
+	fetchResultMetadata := block.NewResultMetadata()
+	fetchResultMetadata.Resolutions = []time.Duration{maxResolution, maxResolution / 2}
+	fetchResult, err := consolidators.NewSeriesFetchResult(it, nil, fetchResultMetadata)
+	assert.NoError(t, err)
+
+	opts := models.NewTagOptions()
+	res, err := SeriesIteratorsToPromResult(
+		context.Background(), fetchResult, nil, opts, NewPromConvertOptions())
+	require.NoError(t, err)
+	verifyResult(t, want, res)
+}
+
+func ms(t xtime.UnixNano) int64 {
+	return t.ToNormalizedTime(time.Millisecond)
+}
+
+func annotationBytes(t *testing.T, payload *annotation.Payload) dts.Annotation {
+	if payload != nil {
+		annotationBytes, err := payload.Marshal()
+		require.NoError(t, err)
+		return annotationBytes
+	}
+	return nil
 }
