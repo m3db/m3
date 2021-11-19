m3db/m3

commit 13e4c4563a2db669bd55527de42385259ed59a5f

[m3msg] Remove unnecessary ConsumeHandler interface (#3918)
The ConsumerHandler is redundant and unnecessary. Use the
MessageProcessor interface for the aggregator so it's consistent with
the coordinator.

This will make it easier to add some base metrics to both consumers in
the future.

src/aggregator/server/m3msg/server.go
@@ -21,9 +21,7 @@
 package m3msg
 
 import (
-	"errors"
 	"fmt"
-	"io"
 
 	"github.com/m3db/m3/src/aggregator/aggregator"
 	"github.com/m3db/m3/src/metrics/encoding"
@@ -35,11 +33,6 @@ import (
 	"go.uber.org/zap"
 )
 
-type server struct {
-	aggregator aggregator.Aggregator
-	logger     *zap.Logger
-}
-
 // NewServer creates a new M3Msg server.
 func NewServer(
 	address string,
@@ -49,49 +42,43 @@ func NewServer(
 	if err := opts.Validate(); err != nil {
 		return nil, err
 	}
-
-	s := &server{
-		aggregator: aggregator,
-		logger:     opts.InstrumentOptions().Logger(),
+	newMessageProcessor := func() consumer.MessageProcessor {
+		// construct a new messageProcessor per consumer so the internal protos can be reused across messages on the
+		// same connection.
+		return &messageProcessor{
+			aggregator: aggregator,
+			logger:     opts.InstrumentOptions().Logger(),
+		}
 	}
-
-	handler := consumer.NewConsumerHandler(s.Consume, opts.ConsumerOptions())
+	handler := consumer.NewMessageHandler(newMessageProcessor, opts.ConsumerOptions())
 	return xserver.NewServer(address, handler, opts.ServerOptions()), nil
 }
 
-func (s *server) Consume(c consumer.Consumer) {
-	var (
-		pb    = &metricpb.MetricWithMetadatas{}
-		union = &encoding.UnaggregatedMessageUnion{}
-	)
-	for {
-		msg, err := c.Message()
-		if err != nil {
-			if !errors.Is(err, io.EOF) {
-				s.logger.Error("could not read message", zap.Error(err))
-			}
-			break
-		}
-
-		// Reset and reuse the protobuf message for unpacking.
-		protobuf.ReuseMetricWithMetadatasProto(pb)
-		if err = s.handleMessage(pb, union, msg); err != nil {
-			s.logger.Error("could not process message",
-				zap.Error(err),
-				zap.Uint64("shard", msg.ShardID()),
-				zap.String("proto", pb.String()))
-		}
-	}
-	c.Close()
+type messageProcessor struct {
+	pb         metricpb.MetricWithMetadatas
+	union      encoding.UnaggregatedMessageUnion
+	aggregator aggregator.Aggregator
+	logger     *zap.Logger
 }
 
-func (s *server) handleMessage(
+func (m *messageProcessor) Process(msg consumer.Message) {
+	if err := m.handleMessage(&m.pb, &m.union, msg); err != nil {
+		m.logger.Error("could not process message",
+			zap.Error(err),
+			zap.Uint64("shard", msg.ShardID()),
+			zap.String("proto", m.pb.String()))
+	}
+}
+
+func (m *messageProcessor) handleMessage(
 	pb *metricpb.MetricWithMetadatas,
 	union *encoding.UnaggregatedMessageUnion,
 	msg consumer.Message,
 ) error {
 	defer msg.Ack()
 
+	// Reset and reuse the protobuf message for unpacking.
+	protobuf.ReuseMetricWithMetadatasProto(&m.pb)
 	// Unmarshal the message.
 	if err := pb.Unmarshal(msg.Bytes()); err != nil {
 		return err
@@ -104,27 +91,27 @@ func (s *server) handleMessage(
 			return err
 		}
 		u := union.CounterWithMetadatas.ToUnion()
-		return s.aggregator.AddUntimed(u, union.CounterWithMetadatas.StagedMetadatas)
+		return m.aggregator.AddUntimed(u, union.CounterWithMetadatas.StagedMetadatas)
 	case metricpb.MetricWithMetadatas_BATCH_TIMER_WITH_METADATAS:
 		err := union.BatchTimerWithMetadatas.FromProto(pb.BatchTimerWithMetadatas)
 		if err != nil {
 			return err
 		}
 		u := union.BatchTimerWithMetadatas.ToUnion()
-		return s.aggregator.AddUntimed(u, union.BatchTimerWithMetadatas.StagedMetadatas)
+		return m.aggregator.AddUntimed(u, union.BatchTimerWithMetadatas.StagedMetadatas)
 	case metricpb.MetricWithMetadatas_GAUGE_WITH_METADATAS:
 		err := union.GaugeWithMetadatas.FromProto(pb.GaugeWithMetadatas)
 		if err != nil {
 			return err
 		}
 		u := union.GaugeWithMetadatas.ToUnion()
-		return s.aggregator.AddUntimed(u, union.GaugeWithMetadatas.StagedMetadatas)
+		return m.aggregator.AddUntimed(u, union.GaugeWithMetadatas.StagedMetadatas)
 	case metricpb.MetricWithMetadatas_FORWARDED_METRIC_WITH_METADATA:
 		err := union.ForwardedMetricWithMetadata.FromProto(pb.ForwardedMetricWithMetadata)
 		if err != nil {
 			return err
 		}
-		return s.aggregator.AddForwarded(
+		return m.aggregator.AddForwarded(
 			union.ForwardedMetricWithMetadata.ForwardedMetric,
 			union.ForwardedMetricWithMetadata.ForwardMetadata)
 	case metricpb.MetricWithMetadatas_TIMED_METRIC_WITH_METADATA:
@@ -132,7 +119,7 @@ func (s *server) handleMessage(
 		if err != nil {
 			return err
 		}
-		return s.aggregator.AddTimed(
+		return m.aggregator.AddTimed(
 			union.TimedMetricWithMetadata.Metric,
 			union.TimedMetricWithMetadata.TimedMetadata)
 	case metricpb.MetricWithMetadatas_TIMED_METRIC_WITH_METADATAS:
@@ -140,10 +127,12 @@ func (s *server) handleMessage(
 		if err != nil {
 			return err
 		}
-		return s.aggregator.AddTimedWithStagedMetadatas(
+		return m.aggregator.AddTimedWithStagedMetadatas(
 			union.TimedMetricWithMetadatas.Metric,
 			union.TimedMetricWithMetadatas.StagedMetadatas)
 	default:
 		return fmt.Errorf("unrecognized message type: %v", pb.Type)
 	}
 }
+
+func (m *messageProcessor) Close() {}

src/msg/consumer/consumer.go
@@ -66,7 +66,7 @@ func (l *listener) Accept() (Consumer, error) {
 		return nil, err
 	}
 
-	return newConsumer(conn, l.msgPool, l.opts, l.m), nil
+	return newConsumer(conn, l.msgPool, l.opts, l.m, NewNoOpMessageProcessor), nil
 }
 
 type metrics struct {
@@ -97,11 +97,12 @@ type consumer struct {
 	w       xio.ResettableWriter
 	conn    net.Conn
 
-	ackPb  msgpb.Ack
-	closed bool
-	doneCh chan struct{}
-	wg     sync.WaitGroup
-	m      metrics
+	ackPb            msgpb.Ack
+	closed           bool
+	doneCh           chan struct{}
+	wg               sync.WaitGroup
+	m                metrics
+	messageProcessor MessageProcessor
 }
 
 func newConsumer(
@@ -109,6 +110,7 @@ func newConsumer(
 	mPool *messagePool,
 	opts Options,
 	m metrics,
+	newMessageProcessorFn NewMessageProcessorFn,
 ) *consumer {
 	var (
 		wOpts = xio.ResettableWriterOptions{
@@ -126,11 +128,12 @@ func newConsumer(
 		decoder: proto.NewDecoder(
 			conn, opts.DecoderOptions(), opts.ConnectionReadBufferSize(),
 		),
-		w:      writerFn(newConnWithTimeout(conn, opts.ConnectionWriteTimeout(), time.Now), wOpts),
-		conn:   conn,
-		closed: false,
-		doneCh: make(chan struct{}),
-		m:      m,
+		w:                writerFn(newConnWithTimeout(conn, opts.ConnectionWriteTimeout(), time.Now), wOpts),
+		conn:             conn,
+		closed:           false,
+		doneCh:           make(chan struct{}),
+		m:                m,
+		messageProcessor: newMessageProcessorFn(),
 	}
 }
 
@@ -140,6 +143,9 @@ func (c *consumer) Init() {
 		c.ackUntilClose()
 		c.wg.Done()
 	}()
+}
+func (c *consumer) process(m Message) {
+	c.messageProcessor.Process(m)
 }
 
 func (c *consumer) Message() (Message, error) {
@@ -230,6 +236,7 @@ func (c *consumer) Close() {
 	close(c.doneCh)
 	c.wg.Wait()
 	c.conn.Close()
+	c.messageProcessor.Close()
 }
 
 type message struct {

src/msg/consumer/handlers.go
@@ -29,54 +29,27 @@ import (
 	"go.uber.org/zap"
 )
 
-type consumerHandler struct {
-	opts      Options
-	mPool     *messagePool
-	consumeFn ConsumeFn
-	m         metrics
-}
-
-// NewConsumerHandler creates a new server handler with consumerFn.
-func NewConsumerHandler(consumeFn ConsumeFn, opts Options) server.Handler {
-	mPool := newMessagePool(opts.MessagePoolOptions())
-	mPool.Init()
-	return &consumerHandler{
-		consumeFn: consumeFn,
-		opts:      opts,
-		mPool:     mPool,
-		m:         newConsumerMetrics(opts.InstrumentOptions().MetricsScope()),
-	}
-}
-
-func (h *consumerHandler) Handle(conn net.Conn) {
-	c := newConsumer(conn, h.mPool, h.opts, h.m)
-	c.Init()
-	h.consumeFn(c)
-}
-
-func (h *consumerHandler) Close() {}
-
 type messageHandler struct {
-	opts  Options
-	mPool *messagePool
-	mp    MessageProcessor
-	m     metrics
+	opts                  Options
+	mPool                 *messagePool
+	newMessageProcessorFn NewMessageProcessorFn
+	m                     metrics
 }
 
 // NewMessageHandler creates a new server handler with messageFn.
-func NewMessageHandler(mp MessageProcessor, opts Options) server.Handler {
+func NewMessageHandler(newMessageProcessorFn NewMessageProcessorFn, opts Options) server.Handler {
 	mPool := newMessagePool(opts.MessagePoolOptions())
 	mPool.Init()
 	return &messageHandler{
-		mp:    mp,
-		opts:  opts,
-		mPool: mPool,
-		m:     newConsumerMetrics(opts.InstrumentOptions().MetricsScope()),
+		newMessageProcessorFn: newMessageProcessorFn,
+		opts:                  opts,
+		mPool:                 mPool,
+		m:                     newConsumerMetrics(opts.InstrumentOptions().MetricsScope()),
 	}
 }
 
 func (h *messageHandler) Handle(conn net.Conn) {
-	c := newConsumer(conn, h.mPool, h.opts, h.m)
+	c := newConsumer(conn, h.mPool, h.opts, h.m, h.newMessageProcessorFn)
 	c.Init()
 	var (
 		msgErr error
@@ -87,7 +60,7 @@ func (h *messageHandler) Handle(conn net.Conn) {
 		if msgErr != nil {
 			break
 		}
-		h.mp.Process(msg)
+		c.process(msg)
 	}
 	if msgErr != nil && msgErr != io.EOF {
 		h.opts.InstrumentOptions().Logger().With(zap.Error(msgErr)).Error("could not read message from consumer")
@@ -95,4 +68,4 @@ func (h *messageHandler) Handle(conn net.Conn) {
 	c.Close()
 }
 
-func (h *messageHandler) Close() { h.mp.Close() }
+func (h *messageHandler) Close() {}

src/msg/consumer/types.go
@@ -129,6 +129,28 @@ type MessageProcessor interface {
 	Close()
 }
 
+// NewMessageProcessorFn creates a new MessageProcessor scoped to a single connection. Messages are processed serially
+// in a connection.
+type NewMessageProcessorFn func() MessageProcessor
+
+// SingletonMessageProcessor uses the same MessageProcessor for all connections.
+func SingletonMessageProcessor(p MessageProcessor) NewMessageProcessorFn {
+	return func() MessageProcessor {
+		return p
+	}
+}
+
+// NewNoOpMessageProcessor creates a new MessageProcessor that does nothing.
+func NewNoOpMessageProcessor() MessageProcessor {
+	return &noOpMessageProcessor{}
+}
+
+type noOpMessageProcessor struct{}
+
+func (n noOpMessageProcessor) Process(Message) {}
+
+func (n noOpMessageProcessor) Close() {}
+
 // ConsumeFn processes the consumer. This is useful when user want to reuse
 // resource across messages received on the same consumer or have finer level
 // control on how to read messages from consumer.

src/msg/consumer/handlers_test.go
@@ -28,6 +28,7 @@ import (
 	"github.com/m3db/m3/src/msg/generated/proto/msgpb"
 	"github.com/m3db/m3/src/msg/protocol/proto"
 	"github.com/m3db/m3/src/x/server"
+	xtest "github.com/m3db/m3/src/x/test"
 
 	"github.com/fortytw2/leaktest"
 	"github.com/golang/mock/gomock"
@@ -59,7 +60,7 @@ func TestServerWithMessageFn(t *testing.T) {
 	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
-	s := server.NewServer("a", NewMessageHandler(p, opts), server.NewOptions())
+	s := server.NewServer("a", NewMessageHandler(SingletonMessageProcessor(p), opts), server.NewOptions())
 	s.Serve(l)
 
 	conn, err := net.Dial("tcp", l.Addr().String())
@@ -88,57 +89,57 @@ func TestServerWithMessageFn(t *testing.T) {
 	s.Close()
 }
 
-func TestServerWithConsumeFn(t *testing.T) {
+func TestServerMessageDifferentConnections(t *testing.T) {
 	defer leaktest.Check(t)()
 
-	var (
-		count  = 0
-		bytes  []byte
-		closed bool
-		wg     sync.WaitGroup
-	)
-	consumeFn := func(c Consumer) {
-		for {
-			count++
-			m, err := c.Message()
-			if err != nil {
-				break
-			}
-			bytes = m.Bytes()
-			m.Ack()
-			wg.Done()
-		}
-		c.Close()
-		closed = true
-	}
+	ctrl := xtest.NewController(t)
+	defer ctrl.Finish()
 
 	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
+	var wg sync.WaitGroup
+	wg.Add(2)
+	handleMessage := func(m Message) {
+		wg.Done()
+	}
+
+	mp1 := NewMockMessageProcessor(ctrl)
+	mp2 := NewMockMessageProcessor(ctrl)
+	mp1.EXPECT().Process(gomock.Any()).Do(handleMessage)
+	mp1.EXPECT().Close()
+	mp2.EXPECT().Process(gomock.Any()).Do(handleMessage)
+	mp2.EXPECT().Close()
+
 	// Set a large ack buffer size to make sure the background go routine
 	// can flush it.
 	opts := testOptions().SetAckBufferSize(100)
-	s := server.NewServer("a", NewConsumerHandler(consumeFn, opts), server.NewOptions())
-	require.NoError(t, err)
-	s.Serve(l)
+	first := true
+	var mu sync.Mutex
+	newMessageProcessor := func() MessageProcessor {
+		mu.Lock()
+		defer mu.Unlock()
+		if first {
+			first = false
+			return mp1
+		}
+		return mp2
+	}
 
-	conn, err := net.Dial("tcp", l.Addr().String())
+	s := server.NewServer("a", NewMessageHandler(newMessageProcessor, opts), server.NewOptions())
+	require.NoError(t, err)
+	require.NoError(t, s.Serve(l))
+
+	conn1, err := net.Dial("tcp", l.Addr().String())
+	require.NoError(t, err)
+	conn2, err := net.Dial("tcp", l.Addr().String())
 	require.NoError(t, err)
 
-	wg.Add(1)
-	err = produce(conn, &testMsg1)
+	err = produce(conn1, &testMsg1)
+	require.NoError(t, err)
+	err = produce(conn2, &testMsg1)
 	require.NoError(t, err)
 
 	wg.Wait()
-	require.Equal(t, testMsg1.Value, bytes)
-
-	var ack msgpb.Ack
-	testDecoder := proto.NewDecoder(conn, opts.DecoderOptions(), 10)
-	err = testDecoder.Decode(&ack)
-	require.NoError(t, err)
-	require.Equal(t, 1, len(ack.Metadata))
-	require.Equal(t, testMsg1.Metadata, ack.Metadata[0])
-
 	s.Close()
-	require.True(t, closed)
 }

src/cmd/services/m3coordinator/server/m3msg/config.go
@@ -86,7 +86,7 @@ func (c handlerConfiguration) newHandler(
 		ProtobufDecoderPoolOptions: c.ProtobufDecoderPool.NewObjectPoolOptions(iOpts),
 		BlockholePolicies:          c.BlackholePolicies,
 	})
-	return consumer.NewMessageHandler(p, cOpts), nil
+	return consumer.NewMessageHandler(consumer.SingletonMessageProcessor(p), cOpts), nil
 }
 
 // NewOptions creates handler options.

src/cmd/services/m3coordinator/server/m3msg/protobuf_handler_test.go
@@ -67,7 +67,7 @@ func TestM3MsgServerWithProtobufHandler(t *testing.T) {
 
 	s := server.NewServer(
 		"a",
-		consumer.NewMessageHandler(newProtobufProcessor(hOpts), opts),
+		consumer.NewMessageHandler(consumer.SingletonMessageProcessor(newProtobufProcessor(hOpts)), opts),
 		server.NewOptions(),
 	)
 	s.Serve(l)
@@ -150,7 +150,7 @@ func TestM3MsgServerWithProtobufHandler_Blackhole(t *testing.T) {
 
 	s := server.NewServer(
 		"a",
-		consumer.NewMessageHandler(newProtobufProcessor(hOpts), opts),
+		consumer.NewMessageHandler(consumer.SingletonMessageProcessor(newProtobufProcessor(hOpts)), opts),
 		server.NewOptions(),
 	)
 	s.Serve(l)

commit 5e4e5bf7fd0bc2c198dad44e9aabb06547dba22a

[msg] Do not Close singleton MessageProcessors when closing connections
This fixes a regression that was introduced in https://github.com/m3db/m3/pull/3918

The shared singleton MessageProcessor was being closed when a Connection
was being closed.

Now a Pool interface is introduced. A MessageProcessor is fetched when a
Connection is created and returned when a Connection is closed. It is up
to the pool impl to decide if it should Close the MessageProcessor on
each return or only when the pool is closed.

src/aggregator/server/m3msg/server.go
@@ -50,7 +50,7 @@ func NewServer(
 			logger:     opts.InstrumentOptions().Logger(),
 		}
 	}
-	handler := consumer.NewMessageHandler(newMessageProcessor, opts.ConsumerOptions())
+	handler := consumer.NewMessageHandler(consumer.NewMessageProcessorPool(newMessageProcessor), opts.ConsumerOptions())
 	return xserver.NewServer(address, handler, opts.ServerOptions()), nil
 }
 

src/msg/consumer/consumer.go
@@ -69,7 +69,7 @@ func (l *listener) Accept() (Consumer, error) {
 		return nil, err
 	}
 
-	return newConsumer(conn, l.msgPool, l.opts, l.m, NewNoOpMessageProcessor), nil
+	return newConsumer(conn, l.msgPool, l.opts, l.m, NewNoOpMessageProcessor()), nil
 }
 
 type metrics struct {
@@ -123,7 +123,7 @@ func newConsumer(
 	mPool *messagePool,
 	opts Options,
 	m metrics,
-	newMessageProcessorFn NewMessageProcessorFn,
+	mp MessageProcessor,
 ) *consumer {
 	var (
 		wOpts = xio.ResettableWriterOptions{
@@ -146,7 +146,7 @@ func newConsumer(
 		closed:           false,
 		doneCh:           make(chan struct{}),
 		m:                m,
-		messageProcessor: newMessageProcessorFn(),
+		messageProcessor: mp,
 	}
 }
 
@@ -262,7 +262,6 @@ func (c *consumer) Close() {
 	close(c.doneCh)
 	c.wg.Wait()
 	c.conn.Close()
-	c.messageProcessor.Close()
 }
 
 type message struct {

src/msg/consumer/handlers.go
@@ -31,26 +31,27 @@ import (
 )
 
 type messageHandler struct {
-	opts                  Options
-	mPool                 *messagePool
-	newMessageProcessorFn NewMessageProcessorFn
-	m                     metrics
+	opts   Options
+	mPool  *messagePool
+	mpPool MessageProcessorPool
+	m      metrics
 }
 
 // NewMessageHandler creates a new server handler with messageFn.
-func NewMessageHandler(newMessageProcessorFn NewMessageProcessorFn, opts Options) server.Handler {
+func NewMessageHandler(mpPool MessageProcessorPool, opts Options) server.Handler {
 	mPool := newMessagePool(opts.MessagePoolOptions())
 	mPool.Init()
 	return &messageHandler{
-		newMessageProcessorFn: newMessageProcessorFn,
-		opts:                  opts,
-		mPool:                 mPool,
-		m:                     newConsumerMetrics(opts.InstrumentOptions().MetricsScope()),
+		mpPool: mpPool,
+		opts:   opts,
+		mPool:  mPool,
+		m:      newConsumerMetrics(opts.InstrumentOptions().MetricsScope()),
 	}
 }
 
 func (h *messageHandler) Handle(conn net.Conn) {
-	c := newConsumer(conn, h.mPool, h.opts, h.m, h.newMessageProcessorFn)
+	mp := h.mpPool.Get()
+	c := newConsumer(conn, h.mPool, h.opts, h.m, mp)
 	c.Init()
 	var (
 		msgErr error
@@ -68,7 +69,10 @@ func (h *messageHandler) Handle(conn net.Conn) {
 	if msgErr != nil && msgErr != io.EOF {
 		h.opts.InstrumentOptions().Logger().With(zap.Error(msgErr)).Error("could not read message from consumer")
 	}
+	h.mpPool.Put(mp)
 	c.Close()
 }
 
-func (h *messageHandler) Close() {}
+func (h *messageHandler) Close() {
+	h.mpPool.Close()
+}

src/msg/consumer/types.go
@@ -132,16 +132,57 @@ type MessageProcessor interface {
 	Close()
 }
 
-// NewMessageProcessorFn creates a new MessageProcessor scoped to a single connection. Messages are processed serially
-// in a connection.
-type NewMessageProcessorFn func() MessageProcessor
-
-// SingletonMessageProcessor uses the same MessageProcessor for all connections.
-func SingletonMessageProcessor(p MessageProcessor) NewMessageProcessorFn {
-	return func() MessageProcessor {
-		return p
-	}
+// MessageProcessorPool returns MessageProcessors.
+type MessageProcessorPool interface {
+	// Get returns a MessageProcessor.
+	Get() MessageProcessor
+	// Put returns the MessageProcessor.
+	Put(mp MessageProcessor)
+	// Close the pool.
+	Close()
 }
+
+// SingletonMessageProcessor returns a MessageProcessorPool that shares the same MessageProcessor for all users. The
+// MessageProcessor is closed when the pool is closed.
+func SingletonMessageProcessor(mp MessageProcessor) MessageProcessorPool {
+	return &singletonMessageProcessorPool{mp: mp}
+}
+
+type singletonMessageProcessorPool struct {
+	mp MessageProcessor
+}
+
+func (s singletonMessageProcessorPool) Get() MessageProcessor {
+	return s.mp
+}
+
+func (s singletonMessageProcessorPool) Put(MessageProcessor) {
+	// mp is shared by all users, nothing to do.
+}
+
+func (s singletonMessageProcessorPool) Close() {
+	s.mp.Close()
+}
+
+// NewMessageProcessorPool returns a MessageProcessorPool that creates a new MessageProcessor for every call to Get
+// and closes the MessageProcessor for every call to Put.
+func NewMessageProcessorPool(fn func() MessageProcessor) MessageProcessorPool {
+	return &messageProcessorPool{fn: fn}
+}
+
+type messageProcessorPool struct {
+	fn func() MessageProcessor
+}
+
+func (m messageProcessorPool) Get() MessageProcessor {
+	return m.fn()
+}
+
+func (m messageProcessorPool) Put(mp MessageProcessor) {
+	mp.Close()
+}
+
+func (m messageProcessorPool) Close() {}
 
 // NewNoOpMessageProcessor creates a new MessageProcessor that does nothing.
 func NewNoOpMessageProcessor() MessageProcessor {

src/msg/consumer/handlers_test.go
@@ -35,7 +35,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestServerWithMessageFn(t *testing.T) {
+func TestServerWithSingletonMessageProcessor(t *testing.T) {
 	defer leaktest.Check(t)()
 
 	var (
@@ -53,7 +53,7 @@ func TestServerWithMessageFn(t *testing.T) {
 			m.Ack()
 			wg.Done()
 		},
-	).Times(2)
+	).Times(3)
 	// Set a large ack buffer size to make sure the background go routine
 	// can flush it.
 	opts := testOptions().SetAckBufferSize(100)
@@ -63,27 +63,35 @@ func TestServerWithMessageFn(t *testing.T) {
 	s := server.NewServer("a", NewMessageHandler(SingletonMessageProcessor(p), opts), server.NewOptions())
 	s.Serve(l)
 
-	conn, err := net.Dial("tcp", l.Addr().String())
+	conn1, err := net.Dial("tcp", l.Addr().String())
+	require.NoError(t, err)
+	conn2, err := net.Dial("tcp", l.Addr().String())
 	require.NoError(t, err)
 
-	wg.Add(1)
-	err = produce(conn, &testMsg1)
+	wg.Add(3)
+	err = produce(conn1, &testMsg1)
 	require.NoError(t, err)
-	wg.Add(1)
-	err = produce(conn, &testMsg2)
+	err = produce(conn1, &testMsg2)
+	require.NoError(t, err)
+	err = produce(conn2, &testMsg2)
 	require.NoError(t, err)
 
 	wg.Wait()
 	require.Equal(t, string(testMsg1.Value), data[0])
 	require.Equal(t, string(testMsg2.Value), data[1])
+	require.Equal(t, string(testMsg2.Value), data[2])
 
 	var ack msgpb.Ack
-	testDecoder := proto.NewDecoder(conn, opts.DecoderOptions(), 10)
+	testDecoder := proto.NewDecoder(conn1, opts.DecoderOptions(), 10)
 	err = testDecoder.Decode(&ack)
 	require.NoError(t, err)
-	require.Equal(t, 2, len(ack.Metadata))
+	testDecoder = proto.NewDecoder(conn2, opts.DecoderOptions(), 10)
+	err = testDecoder.Decode(&ack)
+	require.NoError(t, err)
+	require.Equal(t, 3, len(ack.Metadata))
 	require.Equal(t, testMsg1.Metadata, ack.Metadata[0])
 	require.Equal(t, testMsg2.Metadata, ack.Metadata[1])
+	require.Equal(t, testMsg2.Metadata, ack.Metadata[2])
 
 	p.EXPECT().Close()
 	s.Close()
@@ -126,7 +134,7 @@ func TestServerMessageDifferentConnections(t *testing.T) {
 		return mp2
 	}
 
-	s := server.NewServer("a", NewMessageHandler(newMessageProcessor, opts), server.NewOptions())
+	s := server.NewServer("a", NewMessageHandler(NewMessageProcessorPool(newMessageProcessor), opts), server.NewOptions())
 	require.NoError(t, err)
 	require.NoError(t, s.Serve(l))
 