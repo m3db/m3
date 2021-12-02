977cab6e1b8dde70fef02053f569407243cf89ff
Jerome Froelich <jeromefroelich@hotmail.com>
2021-12-02 17:27:39 -0500

    [instrument] Optionally log build info to stdout (#3955)

--- src/x/instrument/build.go
+++ src/x/instrument/build.go
@@ -23,6 +23,7 @@ package instrument
 import (
 	"errors"
 	"log"
+	"os"
 	"runtime"
 	"strconv"
 	"sync"
@@ -52,6 +53,12 @@ var (
 	// set to a non-empty string, we log the build information at process startup.
 	LogBuildInfoAtStartup string
 
+	// LogBuildInfoToStdout controls whether we log build information to stdout or stderr.
+	// If it is set to a non-empty string then the build info will be logged to stdout,
+	// otherwise it will be logged to stderr (assuming LogBuildInfoAtStartup is also
+	// non-empty).
+	LogBuildInfoToStdout string
+
 	// goVersion is the current runtime version.
 	goVersion = runtime.Version()
 
@@ -68,19 +75,28 @@ var (
 	errBuildTimeNegative = errors.New("reporter build time must be non-negative")
 )
 
-// LogBuildInfo logs the build information to the provided logger.
+// LogBuildInfo logs the build information using the default logger.
 func LogBuildInfo() {
-	log.Printf("Go Runtime version: %s\n", goVersion)
-	log.Printf("Build Version:      %s\n", Version)
-	log.Printf("Build Revision:     %s\n", Revision)
-	log.Printf("Build Branch:       %s\n", Branch)
-	log.Printf("Build Date:         %s\n", BuildDate)
-	log.Printf("Build TimeUnix:     %s\n", BuildTimeUnix)
+	LogBuildInfoWithLogger(log.Default())
+}
+
+// LogBuildInfoWithLogger logs the build information using the provided logger.
+func LogBuildInfoWithLogger(logger *log.Logger) {
+	logger.Printf("Go Runtime version: %s\n", goVersion)
+	logger.Printf("Build Version:      %s\n", Version)
+	logger.Printf("Build Revision:     %s\n", Revision)
+	logger.Printf("Build Branch:       %s\n", Branch)
+	logger.Printf("Build Date:         %s\n", BuildDate)
+	logger.Printf("Build TimeUnix:     %s\n", BuildTimeUnix)
 }
 
 func init() {
 	if LogBuildInfoAtStartup != "" {
-		LogBuildInfo()
+		logger := log.Default()
+		if LogBuildInfoToStdout != "" {
+			logger = log.New(os.Stdout, "", log.LstdFlags)
+		}
+		LogBuildInfoWithLogger(logger)
 	}
 }
 
