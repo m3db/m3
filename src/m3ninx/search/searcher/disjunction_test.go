982c6d7793540701bb85b38c3cd4615a586d95b2
Linas Medžiūnas <linasm@users.noreply.github.com>
2021-12-08 10:19:30 +0200

    [m3ninx] Avoid cloning of roaring bitmap in conjunctionSearcher (#3948)

--- src/m3ninx/search/searcher/disjunction_test.go
+++ src/m3ninx/search/searcher/disjunction_test.go
@@ -81,17 +81,17 @@ func TestDisjunctionSearcher(t *testing.T) {
 	require.NoError(t, err)
 
 	// Test the postings list from the first Reader.
-	expected := firstPL1.Clone()
-	expected.Union(secondPL1)
-	expected.Union(thirdPL1)
+	expected := firstPL1.CloneAsMutable()
+	require.NoError(t, expected.UnionInPlace(secondPL1))
+	require.NoError(t, expected.UnionInPlace(thirdPL1))
 	pl, err := s.Search(firstReader)
 	require.NoError(t, err)
 	require.True(t, pl.Equal(expected))
 
 	// Test the postings list from the second Reader.
-	expected = firstPL2.Clone()
-	expected.Union(secondPL2)
-	expected.Union(thirdPL2)
+	expected = firstPL2.CloneAsMutable()
+	require.NoError(t, expected.UnionInPlace(secondPL2))
+	require.NoError(t, expected.UnionInPlace(thirdPL2))
 	pl, err = s.Search(secondReader)
 	require.NoError(t, err)
 	require.True(t, pl.Equal(expected))
