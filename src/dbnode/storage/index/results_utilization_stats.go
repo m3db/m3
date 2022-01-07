e6b46c729aeb44fd9058552f72baa8c672894eb3
Linas Medžiūnas <linasm@users.noreply.github.com>
2022-01-07 07:43:56 +0200

    [dbnode] Avoid resultsMap memory leak during query execution (#4032)

--- src/dbnode/storage/index/results_utilization_stats.go
+++ src/dbnode/storage/index/results_utilization_stats.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Uber Technologies, Inc.
+// Copyright (c) 2022 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,28 +20,28 @@
 
 package index
 
-import (
-	"bytes"
-
-	"github.com/cespare/xxhash/v2"
-)
+// resultsUtilizationStats tracks utilization of results object in order to detect and prevent
+// unbounded growth of reusable object which may otherwise stay in the object pool indefinitely.
+type resultsUtilizationStats struct {
+	resultsMapCapacity            int
+	consecutiveTimesUnderCapacity int
+}
 
 const (
-	defaultInitialResultsMapSize = 64
+	consecutiveTimesUnderCapacityThreshold = 10
 )
 
-func newResultsMap() *ResultsMap {
-	return _ResultsMapAlloc(_ResultsMapOptions{
-		hash: func(k []byte) ResultsMapHash {
-			return ResultsMapHash(xxhash.Sum64(k))
-		},
-		equals: bytes.Equal,
-		copy: func(k []byte) []byte {
-			return append(make([]byte, 0, len(k)), k...)
-		},
-		finalize: func(k []byte) {
-			// NB(nate): no-op for bytes IDs
-		},
-		initialSize: defaultInitialResultsMapSize,
-	})
+// updateAndCheck returns true iff the underlying object is supposed to be returned
+// to the appropriate object pool for reuse.
+func (r *resultsUtilizationStats) updateAndCheck(totalDocsCount int) bool {
+	if totalDocsCount < r.resultsMapCapacity {
+		r.consecutiveTimesUnderCapacity++
+	} else {
+		r.resultsMapCapacity = totalDocsCount
+		r.consecutiveTimesUnderCapacity = 0
+	}
+
+	// If the size of reusable result object is not fully utilized for a long enough time,
+	// we want to drop it from the pool and prevent holding on to excessive memory indefinitely.
+	return r.consecutiveTimesUnderCapacity < consecutiveTimesUnderCapacityThreshold
 }
