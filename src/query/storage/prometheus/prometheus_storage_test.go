c3bc6b9586e339de2089fe6b005e15c7002a93d7
Andrew Ma <andrew.ma@databricks.com>
2022-08-08 15:54:03 -0700

    Add further testing for BucketWindowGet

--- src/query/storage/prometheus/prometheus_storage_test.go
+++ src/query/storage/prometheus/prometheus_storage_test.go
@@ -164,7 +164,9 @@ func TestSelectWithMetaInContext(t *testing.T) {
 	assert.Equal(t, []string{"warn_warning"}, res.WarningStrings())
 }
 
+// Test to check WindowGetOrFetch() function for Redis cache
 // Will need local Redis server on port 6379 for this test
+// Based off of initial prometheus_storage test (mostly same code, just modified to end up using cache)
 func TestWindowGet(t *testing.T) {
 	ctrl := xtest.NewController(t)
 	defer ctrl.Finish()
@@ -195,8 +197,10 @@ func TestWindowGet(t *testing.T) {
 		check.cache.FlushAll()
 	}
 
+	// Size must be < BucketSize to ensure we do end up using the WindowGetOrFetch() function
+	// Otherwise we end up using BucketWindowGetOrFetch() without calling that subfunction
 	start := int64(100)
-	end := int64(1200)
+	end := int64(400)
 	step := int64(time.Second)
 
 	hints := &promstorage.SelectHints{
@@ -286,6 +290,188 @@ func TestWindowGet(t *testing.T) {
 	assert.NoError(t, series.Err())
 
 	exDp := [][]dp{{{v: 100, t: 200}}, {{v: 1, t: 100}}}
+	exTags := []string{`{foo="bar", qux="qaz"}`, `{foo="bar", qux="qzz"}`}
+	assert.Equal(t, exTags, acTags)
+	assert.Equal(t, exDp, acDp)
+	require.NoError(t, q.Close())
+}
+
+// Test to check BucketWindowGetOrFetch() function for Redis cache
+// Will need local Redis server on port 6379 for this test
+// Based off of initial prometheus_storage test (mostly same code, just modified to end up using cache)
+func TestBucketWindowGet(t *testing.T) {
+	ctrl := xtest.NewController(t)
+	defer ctrl.Finish()
+
+	var resMutex sync.Mutex
+	res := block.NewResultMetadata()
+	resultMetadataReceiveFn := func(m block.ResultMetadata) {
+		resMutex.Lock()
+		defer resMutex.Unlock()
+		res = res.CombineMetadata(m)
+	}
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, FetchOptionsContextKey, storage.NewFetchOptions())
+	ctx = context.WithValue(ctx, BlockResultMetadataFnKey, resultMetadataReceiveFn)
+
+	store := storage.NewMockStorage(ctrl)
+	opts := PrometheusOptions{
+		Storage:           store,
+		InstrumentOptions: instrument.NewOptions(),
+		RedisCacheAddress: "127.0.0.1:6379",
+	}
+
+	queryable := NewPrometheusQueryable(opts)
+	q, err := queryable.Querier(ctx, 0, 0)
+	require.NoError(t, err)
+
+	if check, ok := q.(*querier); ok {
+		check.cache.FlushAll()
+	}
+
+	start := int64(100)
+	last_start := int64(3600)
+	end := int64(3700)
+	step := int64(time.Second)
+
+	hints := &promstorage.SelectHints{
+		Start: start * 1000,
+		End:   end * 1000,
+
+		Step: step / int64(time.Millisecond),
+	}
+
+	matchers := []*labels.Matcher{
+		labels.MustNewMatcher(labels.MatchEqual, "foo", "bar"),
+		labels.MustNewMatcher(labels.MatchRegexp, "qux", "q.z"),
+	}
+
+	m, err := models.NewMatcher(models.MatchEqual, []byte("foo"), []byte("bar"))
+	require.NoError(t, err)
+	m2, err := models.NewMatcher(models.MatchRegexp, []byte("qux"), []byte("q.z"))
+	require.NoError(t, err)
+
+	exQuery := &storage.FetchQuery{
+		TagMatchers: models.Matchers{m, m2},
+		Start:       time.Unix(start, 0),
+		End:         time.Unix(end, 0),
+		Interval:    time.Duration(step),
+	}
+
+	meta := block.NewResultMetadata()
+	meta.AddWarning("warn", "warning")
+	store.EXPECT().FetchProm(ctx, exQuery, gomock.Any()).DoAndReturn(
+		func(_ context.Context, arg1 *storage.FetchQuery, _ *storage.FetchOptions) (storage.PromResult, error) {
+			return storage.PromResult{
+				Metadata: meta,
+				PromResult: &prompb.QueryResult{
+					Timeseries: []*prompb.TimeSeries{
+						{
+							Labels: []prompb.Label{
+								{Name: []byte("foo"), Value: []byte("bar")},
+								{Name: []byte("qux"), Value: []byte("qzz")},
+							},
+							Samples: []prompb.Sample{
+								{Value: 0, Timestamp: 100000}, {Value: 1, Timestamp: 101000}, {Value: 2, Timestamp: 102000},
+								{Value: 0, Timestamp: 2400000}, {Value: 1, Timestamp: 2401000}, {Value: 2, Timestamp: 2402000},
+								{Value: 0, Timestamp: 3600000}, {Value: 1, Timestamp: 3601000},
+							},
+						},
+						{
+							Labels: []prompb.Label{
+								{Name: []byte("foo"), Value: []byte("bar")},
+								{Name: []byte("qux"), Value: []byte("qaz")},
+							},
+							Samples: []prompb.Sample{
+								{Value: 1, Timestamp: 100000}, {Value: 0, Timestamp: 300000}, {Value: 0, Timestamp: 700000},
+								{Value: 0, Timestamp: 1000000}, {Value: 1, Timestamp: 1300000}, {Value: 2, Timestamp: 1600000},
+								{Value: 0, Timestamp: 3600000}, {Value: 1, Timestamp: 3601000},
+							},
+						},
+					},
+				},
+			}, nil
+		}).MaxTimes(1)
+
+	series := q.Select(true, hints, matchers...)
+	assert.NoError(t, series.Err())
+
+	lastQuery := &storage.FetchQuery{
+		TagMatchers: models.Matchers{m, m2},
+		Start:       time.Unix(last_start, 0),
+		End:         time.Unix(end, 0),
+		Interval:    time.Duration(step),
+	}
+
+	store.EXPECT().FetchProm(ctx, lastQuery, gomock.Any()).DoAndReturn(
+		func(_ context.Context, arg1 *storage.FetchQuery, _ *storage.FetchOptions) (storage.PromResult, error) {
+			return storage.PromResult{
+				Metadata: meta,
+				PromResult: &prompb.QueryResult{
+					Timeseries: []*prompb.TimeSeries{
+						{
+							Labels: []prompb.Label{
+								{Name: []byte("foo"), Value: []byte("bar")},
+								{Name: []byte("qux"), Value: []byte("qzz")},
+							},
+							Samples: []prompb.Sample{
+								{Value: 0, Timestamp: 3600000},
+								{Value: 1, Timestamp: 3601000},
+							},
+						},
+						{
+							Labels: []prompb.Label{
+								{Name: []byte("foo"), Value: []byte("bar")},
+								{Name: []byte("qux"), Value: []byte("qaz")},
+							},
+							Samples: []prompb.Sample{
+								{Value: 0, Timestamp: 3600000},
+								{Value: 1, Timestamp: 3601000},
+							},
+						},
+					},
+				},
+			}, nil
+		}).MaxTimes(1)
+
+	// Rerun, result should be cached so we should not have to FetchProm again
+	// If we do, since Max is 1, we will hit error
+	series = q.Select(true, hints, matchers...)
+	assert.NoError(t, series.Err())
+
+	type dp struct {
+		v float64
+		t int64
+	}
+
+	acDp := make([][]dp, 0, 2)
+	acTags := make([]string, 0, 2)
+	for series.Next() {
+		curr := series.At()
+		acTags = append(acTags, curr.Labels().String())
+		it := curr.Iterator()
+		ac := make([]dp, 0, 1)
+		for it.Next() {
+			t, v := it.At()
+			ac = append(ac, dp{t: t, v: v})
+		}
+
+		assert.NoError(t, it.Err())
+		acDp = append(acDp, ac)
+	}
+
+	assert.NoError(t, series.Err())
+
+	exDp := [][]dp{
+		{
+			{v: 1, t: 100000}, {v: 0, t: 300000}, {v: 0, t: 700000}, {v: 0, t: 1000000}, {v: 1, t: 1300000},
+			{v: 2, t: 1600000}, {v: 0, t: 3600000}, {v: 1, t: 3601000},
+		},
+		{
+			{v: 0, t: 100000}, {v: 1, t: 101000}, {v: 2, t: 102000}, {v: 0, t: 2400000}, {v: 1, t: 2401000},
+			{v: 2, t: 2402000}, {v: 0, t: 3600000}, {v: 1, t: 3601000},
+		},
+	}
 	exTags := []string{`{foo="bar", qux="qaz"}`, `{foo="bar", qux="qzz"}`}
 	assert.Equal(t, exTags, acTags)
 	assert.Equal(t, exDp, acDp)
