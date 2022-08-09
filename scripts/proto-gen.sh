fdce009486d104ee6692265b1e6e099347adf2da
Andrew Mains <amains@uber.com>
2022-08-09 17:55:45 -0400

    Make proto-gen targets not rely on GOPATH
    Similar to https://github.com/m3db/m3/pull/4135
    
    Since m3 is now a go.mod project, it's no longer necessary to be checked out under GOPATH.
    
    This PR makes our code generation reflect that fact. I've switched to use of relative paths in generate.go, which as I understand
    it is the preferred way of doing things generally.
    
    Our proto-gen script now takes in an actual path to proto files vs a logical path. It infers the logical repo path from the actual path.
    
    Fixes #4134

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# include poratable readlink
-source $(dirname $0)/realpath.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT="${DIR}/.."
+
+# include portable readlink
+source "${DIR}/realpath.sh"
 
 # paranoia, ftw
 set -e
-
 PROTOC_IMAGE_VERSION=${PROTOC_IMAGE_VERSION:-"znly/protoc:0.2.0"}
 
 # ensure docker is running
@@ -17,21 +19,42 @@ if [[ -n "$BUILDKITE" ]]; then
 fi
 
 PROTO_SRC=$1
-for i in "${GOPATH}/src/${PROTO_SRC}"/*; do
+# Find all protobuf files under ${PROTO_SRC}
+for i in "${PROTO_SRC}"/*; do
 	if ! [ -d $i ]; then
 		continue
 	fi
 
 	if ls $i/*.proto > /dev/null 2>&1; then
-		proto_files=$(ls $i/*.proto | sed -e "s@${GOPATH}@@g")
+
+    # Find all .proto files in this subdirectory, and return the full import path to them. The import path is:
+    # github.com/m3db/m3/<path_from_root>
+    # compute their path relative to the repository root.
+    proto_files="$(find $i -name \*.proto | while read -r filename; do
+      full_path="$(realpath ${filename})"
+
+      # Relativize it to the root
+      root_full_path="$(realpath "${ROOT}")"
+
+      # Bash substitution:
+      # https://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
+      path_from_root="${full_path/${root_full_path}}"
+      # Finally
+      echo "github.com/m3db/m3${path_from_root}"
+    done)"
+
 		echo "generating from ${proto_files}"
 		# need the additional m3db_path mount in docker because it's a symlink on the CI.
-		m3db_path=$(realpath $GOPATH/src/github.com/m3db/m3)
+		m3db_path=$(realpath "${ROOT}")
     resolve_protos="Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types"
 
-		docker run --rm -w /src -v $GOPATH/src:/src -v ${m3db_path}:/src/github.com/m3db/m3 \
+		docker run --rm -w /src \
+		-v "${ROOT}/src:/src" \
+		-v ${m3db_path}:/src/github.com/m3db/m3 \
 		$UID_FLAGS $PROTOC_IMAGE_VERSION \
 			 --gogofaster_out=${resolve_protos},plugins=grpc:/src \
-			 -I/src -I/src/github.com/m3db/m3/vendor ${proto_files}
+			 -I/src \
+			 ${proto_files}
+
 	fi
 done
