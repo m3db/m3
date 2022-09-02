88d1ef09907716edf493d00ada1a0769ee1e41fc
Andrew Mains <amains@uber.com>
2022-09-02 12:35:16 -0400

    etcd_docker 6: Remove etcd embed from M3DB node (#4150)
    * etcd_docker 5: Incorporate docker based etcd approach into docker integration tests.
    
    PR 5 for https://github.com/m3db/m3/issues/4144
    
    This PR makes the docker integration tests use containerized etcd.
    Previously, these relied on M3DB running an embbeded etcd server.
    
    There's no inherent need for this, and it opens us up to dependency issues as described
    in the linked github issue.
    
    Note: there are a handful that require multiple servers; these are currently "skipped" (commented). I intend to bring those
    back at a later date..
    
    commit-id:e67a5172
    
    * etcd_docker 6: Remove etcd embed from M3DB node
    
    PR 6 for https://github.com/m3db/m3/issues/4144
    
    Now that all test usages of the embed package had been removed, this PR removes
    the usage of the embed package from the m3db server itself.
    
    N.B.: this PR currently makes no attempt to fix up any "quick start" flows relying on embedded etcd; that will
    come later.
    
    commit-id:2870a75c

--- scripts/docker-integration-tests/run.sh
+++ scripts/docker-integration-tests/run.sh
@@ -3,22 +3,55 @@
 set -ex
 
 TESTS=(
-	scripts/docker-integration-tests/cold_writes_simple/test.sh
-	scripts/docker-integration-tests/prometheus_replication/test.sh
-	scripts/docker-integration-tests/carbon/test.sh
-	scripts/docker-integration-tests/aggregator/test.sh
-	scripts/docker-integration-tests/aggregator_legacy/test.sh
-	scripts/docker-integration-tests/query_fanout/test.sh
-	scripts/docker-integration-tests/repair/test.sh
-	scripts/docker-integration-tests/replication/test.sh
-	scripts/docker-integration-tests/multi_cluster_write/test.sh
-	scripts/docker-integration-tests/coordinator_config_rules/test.sh
-	scripts/docker-integration-tests/coordinator_noop/test.sh
-	scripts/docker-integration-tests/prom_remote_write_backend/test.sh
+  scripts/docker-integration-tests/cold_writes_simple/test.sh
+  # TODO (amains): This test requires two *separate* etcd clusters, which is a bit harder to setup.
+  #  scripts/docker-integration-tests/prometheus_replication/test.sh
+
+  scripts/docker-integration-tests/carbon/test.sh
+  scripts/docker-integration-tests/aggregator/test.sh
+  scripts/docker-integration-tests/aggregator_legacy/test.sh
+
+  # TODO (amains): This test requires two *separate* etcd clusters, which is a bit harder to setup.
+  #  scripts/docker-integration-tests/query_fanout/test.sh
+
+  scripts/docker-integration-tests/repair/test.sh
+
+  # TODO (amains): This test requires two *separate* etcd clusters, which is a bit harder to setup.
+  #  scripts/docker-integration-tests/replication/test.sh
+
+  # TODO (amains): This test requires two *separate* etcd clusters, which is a bit harder to setup.
+  #  scripts/docker-integration-tests/multi_cluster_write/test.sh
+
+  scripts/docker-integration-tests/coordinator_config_rules/test.sh
+  scripts/docker-integration-tests/coordinator_noop/test.sh
+  scripts/docker-integration-tests/prom_remote_write_backend/test.sh
 )
 
+# Some systems, including our default Buildkite hosts, don't come with netcat
+# installed and we may not have perms to install it. "Install" it in the worst
+# possible way.
+if ! command -v nc && [[ "$BUILDKITE" == "true" ]]; then
+  echo "installing netcat"
+  NCDIR="$(mktemp -d)"
+
+  yumdownloader -y --destdir "$NCDIR" --resolve nc
+  (
+    cd "$NCDIR"
+    RPM=$(find . -maxdepth 1 -name '*.rpm' | tail -n1)
+    rpm2cpio "$RPM" | cpio -id
+  )
+
+  export PATH="$PATH:$NCDIR/usr/bin"
+
+  function cleanup_nc() {
+    rm -rf "$NCDIR"
+  }
+
+  trap cleanup_nc EXIT
+fi
+
 if [[ -z "$SKIP_SETUP" ]] || [[ "$SKIP_SETUP" == "false" ]]; then
-	scripts/docker-integration-tests/setup.sh
+  scripts/docker-integration-tests/setup.sh
 fi
 
 NUM_TESTS=${#TESTS[@]}
@@ -27,16 +60,16 @@ MAX_IDX=$(((NUM_TESTS*(BUILDKITE_PARALLEL_JOB+1)/BUILDKITE_PARALLEL_JOB_COUNT)-1
 
 ITER=0
 for test in "${TESTS[@]}"; do
-	if [[ $ITER -ge $MIN_IDX && $ITER -le $MAX_IDX ]]; then
-		# Ensure all docker containers have been stopped so we don't run into issues
-		# trying to bind ports.
-		docker rm -f $(docker ps -aq) 2>/dev/null || true
-		echo "----------------------------------------------"
-		echo "running $test"
-		if ! (export M3_PATH=$(pwd) && $test); then
-			echo "--- :bk-status-failed: $test FAILED"
-			exit 1
-		fi
-	fi
-	ITER="$((ITER+1))"
+  if [[ $ITER -ge $MIN_IDX && $ITER -le $MAX_IDX ]]; then
+    # Ensure all docker containers have been stopped so we don't run into issues
+    # trying to bind ports.
+    docker rm -f $(docker ps -aq) 2>/dev/null || true
+    echo "----------------------------------------------"
+    echo "running $test"
+    if ! (export M3_PATH=$(pwd) && $test); then
+      echo "--- :bk-status-failed: $test FAILED"
+      exit 1
+    fi
+  fi
+  ITER="$((ITER+1))"
 done
