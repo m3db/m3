135cdd8d34db3883b0b3ef9d2cd14537b222e5da
Andrew Mains <amains@uber.com>
2022-09-02 10:50:29 -0400

    etcd_docker 4: Incorporate docker based etcd into Go integration tests (#4148)
    PR 4 for https://github.com/m3db/m3/issues/4144
    
    High level approach is as described in https://github.com/m3db/m3/issues/4144 .
    
    This PR integrates docker based etcd into our Go integration tests. It removes the need to have the embed package running
    in m3db for them, but doesn't yet touch that functionality.
    
    commit-id:3ae12ffd

--- src/integration/resources/options.go
+++ src/integration/resources/options.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2022 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package resources
 
 import (
@@ -8,6 +28,9 @@ import (
 // ClusterOptions contains options for spinning up a new M3 cluster
 // composed of in-process components.
 type ClusterOptions struct {
+	// EtcdEndpoints if provided, will be used directly instead of spinning up a dedicated etcd node for the cluster.
+	// By default, NewClusterFromSpecification will spin up and manage an etcd node itself.
+	EtcdEndpoints []string
 	// DBNode contains cluster options for spinning up dbnodes.
 	DBNode *DBNodeClusterOptions
 	// Aggregator is the optional cluster options for spinning up aggregators.
