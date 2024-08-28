5c5dbe5b70d766e9f2d2974bf559bcce2f72d0e8
zengk <zengk@uber.com>
2024-08-28 11:39:03 -0700

    add host.docker.internal to queryaddress

--- scripts/comparator/compare.go
+++ scripts/comparator/compare.go
@@ -53,7 +53,7 @@ func main() {
 
 		pQueryFile    = flag.String("input", "", "the query file")
 		pPromAddress  = flag.String("promAdress", "host.docker.internal:9090", "prom address")
-		pQueryAddress = flag.String("queryAddress", "0.0.0.0:7201/m3query", "M3 query address")
+		pQueryAddress = flag.String("queryAddress", "host.docker.internal:7201/m3query", "M3 query address")
 
 		pComparatorAddress = flag.String("comparator", "", "comparator address")
 		pRegressionDir     = flag.String("regressionDir", "", "optional directory for regression tests")
