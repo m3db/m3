a873a01a6064a4b5a2538aba4421bb13b8de986c
elliotjberman <elliotjberman@gmail.com>
2024-10-08 16:47:38 -0400

    Revert "Close Iterator in Active Ruleset (#4299)" (#4301)
    This reverts commit 1a881a097e5b324a1604633d7ccecbe8b653c361.

--- src/metrics/rules/active_ruleset.go
+++ src/metrics/rules/active_ruleset.go
@@ -528,13 +528,12 @@ func (as *activeRuleSet) matchRollupTarget(
 
 	var (
 		rollupTags      = rollupOp.Tags
+		sortedTagIter   = matchOpts.SortedTagIteratorFn(sortedTagPairBytes)
 		matchTagIdx     = 0
 		nameTagName     = as.tagsFilterOpts.NameTagKey
 		nameTagValue    []byte
 		includeTagNames = as.includeTagKeys
 	)
-
-	sortedTagIter := matchOpts.SortedTagIteratorFn(sortedTagPairBytes)
 
 	switch rollupOp.Type {
 	case mpipeline.GroupByRollupType:
@@ -580,7 +579,6 @@ func (as *activeRuleSet) matchRollupTarget(
 
 			// If one of the target tags is not found in the ID, this is considered  a non-match so return immediately.
 			if res > 0 {
-				sortedTagIter.Close()
 				return nil, false, nil
 			}
 		}
@@ -631,13 +629,11 @@ func (as *activeRuleSet) matchRollupTarget(
 		}
 	}
 
-	if err := sortedTagIter.Err(); err != nil {
-		sortedTagIter.Close()
-		return nil, false, err
+	if sortedTagIter.Err() != nil {
+		return nil, false, sortedTagIter.Err()
 	}
 
 	if !targetOpts.generateRollupID {
-		sortedTagIter.Close()
 		return nil, true, nil
 	}
 
@@ -649,7 +645,6 @@ func (as *activeRuleSet) matchRollupTarget(
 	}
 
 	newName := rollupOp.NewName(nameTagValue)
-	sortedTagIter.Close()
 	return as.newRollupIDFn(newName, tagPairs), true, nil
 }
 
