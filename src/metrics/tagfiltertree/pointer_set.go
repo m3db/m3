9aa6613dadc4e9bc1361576bf9353e5a2b636cdf
Shankar Nair <shankar.nair@uber.com>
2024-10-15 08:49:43 -0700

    reduce memory footprint using bitsets to sparse-compress pointer slice

---
+++ src/metrics/tagfiltertree/pointer_set.go
@@ -0,0 +1,58 @@
+package tagfiltertree
+
+import "math/bits"
+
+type PointerSet struct {
+	bits [2]uint64 // Using 2 uint64 gives us 128 bits (0 to 127).
+}
+
+// Set adds a pointer at index i (0 <= i < 127).
+func (ps *PointerSet) Set(i byte) {
+	if i < 64 {
+		ps.bits[0] |= (1 << i)
+	} else {
+		ps.bits[1] |= (1 << (i - 64))
+	}
+}
+
+// Clear removes a pointer at index i.
+func (ps *PointerSet) Clear(i byte) {
+	if i < 64 {
+		ps.bits[0] &= ^(1 << i)
+	} else {
+		ps.bits[1] &= ^(1 << (i - 64))
+	}
+}
+
+// IsSet checks if a pointer is present at index i.
+func (ps *PointerSet) IsSet(i byte) bool {
+	if i < 64 {
+		return ps.bits[0]&(1<<i) != 0
+	}
+	return ps.bits[1]&(1<<(i-64)) != 0
+}
+
+// CountSetBitsUntil counts how many bits are set to 1 up to index i (inclusive).
+func (ps *PointerSet) CountSetBitsUntil(i byte) int {
+	if i < 64 {
+		// Count bits in the first uint64 up to index i.
+		return bits.OnesCount64(ps.bits[0] & ((1 << (i + 1)) - 1))
+	} else {
+		// Count all bits in the first uint64.
+		count := bits.OnesCount64(ps.bits[0])
+		// Count bits in the second uint64 up to index i - 64.
+		count += bits.OnesCount64(ps.bits[1] & ((1 << (i - 64 + 1)) - 1))
+		return count
+	}
+}
+
+// Bits returns a slice of bytes representing the indices of set pointers.
+func (ps *PointerSet) Bits() []byte {
+	bits := make([]byte, 0, 128)
+	for i := byte(0); i < 128; i++ {
+		if ps.IsSet(i) {
+			bits = append(bits, i)
+		}
+	}
+	return bits
+}
---
+++ src/metrics/tagfiltertree/trie_char.go
@@ -0,0 +1,27 @@
+package tagfiltertree
+
+type trieChar struct {
+	ch byte
+}
+
+func (c *trieChar) String() string {
+	return string(c.ch)
+}
+
+func (c *trieChar) SetEnd() {
+	c.ch |= 0x01
+}
+
+func (c *trieChar) IsEnd() bool {
+	return c.ch&0x01 == 0x01
+}
+
+func NewTrieChar(ch byte) trieChar {
+	return trieChar{
+		ch: ch << 1,
+	}
+}
+
+func (c *trieChar) Char() byte {
+	return c.ch >> 1
+}
