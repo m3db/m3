85c69e3c0ca36267e68bb61060756a3d0ab013b4
zengk <zengk@uber.com>
2024-10-24 11:46:05 -0700

    echo path

--- src/dbnode/generated/thrift/generate.sh
+++ src/dbnode/generated/thrift/generate.sh
@@ -3,19 +3,29 @@
 # paranoia, ftw
 set -ex
 
-# ensure docker is running
-docker run --rm hello-world >/dev/null
+echo "Current PATH: $PATH"
 
-# generate files using dockerized thrift-gen
-THRIFT_IMAGE_VERSION=${THRIFT_IMAGE_VERSION:-"quay.io/m3db/thrift-gen:latest"}
-echo "Generating thrift files with image: $THRIFT_IMAGE_VERSION"
-
-UID_FLAGS="-u $(id -u)"
-if [[ -n "$BUILDKITE" ]]; then
-	UID_FLAGS="-u root"
+echo "Checking if thrift-gen is in PATH..."
+if command -v thrift-gen >/dev/null 2>&1; then
+    echo "thrift-gen found at: $(command -v thrift-gen)"
+else
+    echo "thrift-gen not found in PATH. Exiting."
+    exit 1
 fi
 
 thrift-gen --generateThrift --inputFile rpc.thrift --outputDir ../rpc
+
+# ensure docker is running
+#docker run --rm hello-world >/dev/null
+
+# generate files using dockerized thrift-gen
+#THRIFT_IMAGE_VERSION=${THRIFT_IMAGE_VERSION:-"quay.io/m3db/thrift-gen:latest"}
+#echo "Generating thrift files with image: $THRIFT_IMAGE_VERSION"
+
+#UID_FLAGS="-u $(id -u)"
+#if [[ -n "$BUILDKITE" ]]; then
+#	UID_FLAGS="-u root"
+#fi
 
 #docker run --rm -v "$(pwd):/data" $UID_FLAGS    \
 #  "$THRIFT_IMAGE_VERSION" --generateThrift      \
