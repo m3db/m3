b3f0d2b98b02d449e12fe3b5f15dd55fa9892001
zengk <zengk@uber.com>
2024-11-06 14:55:21 -0800

    directly run thrift in generate.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:1.22-bullseye
 
-RUN apt-get update && apt-get install -y lsof netcat-openbsd docker.io jq protobuf-compiler
+RUN apt-get update && apt-get install -y lsof netcat-openbsd docker.io jq protobuf-compiler thrift-compiler
 
 RUN go install github.com/gogo/protobuf/protoc-gen-gogofaster@latest
 
--- src/dbnode/generated/thrift/generate.sh
+++ src/dbnode/generated/thrift/generate.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
 # paranoia, ftw
-set -e
+set -ex
 
-# ensure docker is running
-docker run --rm hello-world >/dev/null
-
-# generate files using dockerized thrift-gen
-THRIFT_IMAGE_VERSION=${THRIFT_IMAGE_VERSION:-"quay.io/m3db/thrift-gen:latest"}
-echo "Generating thrift files with image: $THRIFT_IMAGE_VERSION"
-
-UID_FLAGS="-u $(id -u)"
-if [[ -n "$BUILDKITE" ]]; then
-	UID_FLAGS="-u root"
-fi
-
-docker run --rm -v "$(pwd):/data" $UID_FLAGS    \
-  "$THRIFT_IMAGE_VERSION" --generateThrift      \
-  --inputFile /data/rpc.thrift --outputDir /data
+thrift --gen go rpc.thrift
 
 # ensure formatting is correct
 go fmt github.com/m3db/m3/src/dbnode/generated/thrift/rpc
