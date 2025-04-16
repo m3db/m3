2b6c98d30817a0337d1b5adcae4458387a62df9f
ggaurav08 <110825097+ggaurav08@users.noreply.github.com>
2025-04-16 11:47:07 +0530

    adding circuit breaker default config

--- src/dbnode/circuitbreaker/internal/circuitbreaker/config.go
+++ src/dbnode/circuitbreaker/internal/circuitbreaker/config.go
@@ -8,10 +8,10 @@ import (
 const (
 	_defaultWindowSize           = 15
 	_defaultBucketDuration       = time.Second
-	_defaultRecoveryTime         = time.Second * 1800
+	_defaultRecoveryTime         = time.Second * 2
 	_defaultJitter               = time.Second * 1
 	_defaultMaxProbeTime         = time.Second * 25
-	_defaultMinimumRequests      = int64(20)
+	_defaultMinimumRequests      = int64(100)
 	_defaultFailureRatio         = 0.50
 	_defaultMinimumProbeRequests = 25
 )
