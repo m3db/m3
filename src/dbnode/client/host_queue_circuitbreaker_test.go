b1e9fa91205f84654d6351fe939fe0c2d9f475fa
ggaurav08 <110825097+ggaurav08@users.noreply.github.com>
2025-05-22 22:18:32 +0530

    review change

--- src/dbnode/client/host_queue_circuitbreaker_test.go
+++ src/dbnode/client/host_queue_circuitbreaker_test.go
@@ -170,6 +170,7 @@ func TestHostQueueCircuitBreakerIntegration(t *testing.T) {
 			var wg sync.WaitGroup
 			wg.Add(test.numCalls)
 			var actualErrs []error
+			var mu sync.Mutex // Add mutex to protect actualErrs
 
 			for i := 0; i < test.numCalls; i++ {
 				writeOp := testWriteOp(
@@ -179,7 +180,9 @@ func TestHostQueueCircuitBreakerIntegration(t *testing.T) {
 					1234567890,
 					rpc.TimeType_UNIX_SECONDS,
 					func(result interface{}, err error) {
+						mu.Lock()
 						actualErrs = append(actualErrs, err)
+						mu.Unlock()
 						wg.Done()
 					},
 				)
