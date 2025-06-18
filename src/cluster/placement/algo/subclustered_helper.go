7cff5a013e79c2902fa936bf3fb7a59f61e77bf4
Riya Tyagi <riya.tyagi@uber.com>
2025-06-18 12:39:47 +0530

    Added the subclustered helper file with placemnt helper methods placeholders

---
+++ src/cluster/placement/algo/subclustered_helper.go
@@ -0,0 +1,141 @@
+// Copyright (c) 2025 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package algo
+
+import (
+	"errors"
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/m3db/m3/src/cluster/placement"
+	"github.com/m3db/m3/src/cluster/shard"
+)
+
+var (
+	// nolint: unused
+	errSubclusteredHelperNotImplemented = errors.New("subclustered helper methods not yet implemented")
+)
+
+// nolint
+type subclusteredHelper struct {
+	targetLoad          map[string]int
+	shardToInstanceMap  map[uint32]map[placement.Instance]struct{}
+	groupToInstancesMap map[string]map[placement.Instance]struct{}
+	groupToWeightMap    map[string]uint32
+	subClusters         map[uint32]*subcluster
+	rf                  int
+	uniqueShards        []uint32
+	instances           map[string]placement.Instance
+	log                 *zap.Logger
+	opts                placement.Options
+	totalWeight         uint32
+}
+
+// subcluster is a subcluster in the placement.
+// nolint
+type subcluster struct {
+	id                  uint32
+	targetShardCount    int
+	instances           map[string]placement.Instance
+	shardMap            map[uint32]int
+	instanceShardCounts map[string]int
+}
+
+// nolint
+func newSubclusteredHelper(p placement.Placement, targetRF int, opts placement.Options, subClusterToExclude uint32) (placementHelper, error) {
+	ph := &subclusteredHelper{
+		rf:           targetRF,
+		instances:    make(map[string]placement.Instance, p.NumInstances()),
+		uniqueShards: p.Shards(),
+		log:          opts.InstrumentOptions().Logger(),
+		opts:         opts,
+	}
+
+	for _, instance := range p.Instances() {
+		ph.instances[instance.ID()] = instance
+	}
+
+	// TODO: Implement subclustered helper logic to scan current load and build target load. Also add the validations.
+
+	return ph, nil
+}
+
+// nolint
+// Instances returns the list of instances managed by the PlacementHelper.
+func (ph *subclusteredHelper) Instances() []placement.Instance {
+	// TODO: Implement subclustered instances logic
+	return nil
+}
+
+// CanMoveShard checks if the shard can be moved from the instance to the target isolation group.
+// nolint: unused
+func (ph *subclusteredHelper) CanMoveShard(shard uint32, from placement.Instance, toIsolationGroup string) bool {
+	// TODO: Implement subclustered shard movement logic
+	return false
+}
+
+// placeShards distributes shards to the instances in the helper, with aware of where are the shards coming from.
+// nolint: unused
+func (ph *subclusteredHelper) placeShards(
+	shards []shard.Shard,
+	from placement.Instance,
+	candidates []placement.Instance,
+) error {
+	// TODO: Implement subclustered shard placement logic
+	return fmt.Errorf("subclustered placeShards not yet implemented: %w",
+		errSubclusteredHelperNotImplemented)
+}
+
+// addInstance adds an instance to the placement.
+// nolint: unused
+func (ph *subclusteredHelper) addInstance(addingInstance placement.Instance) error {
+	// TODO: Implement subclustered add instance logic
+	return fmt.Errorf("subclustered addInstance not yet implemented: %w", errSubclusteredHelperNotImplemented)
+}
+
+// optimize rebalances the load distribution in the cluster.
+// nolint: unused
+func (ph *subclusteredHelper) optimize(t optimizeType) error {
+	// TODO: Implement subclustered optimization logic
+	return fmt.Errorf("subclustered optimize not yet implemented: %w", errSubclusteredHelperNotImplemented)
+}
+
+// generatePlacement generates a placement.
+// nolint: unused
+func (ph *subclusteredHelper) generatePlacement() placement.Placement {
+	// TODO: Implement subclustered placement generation logic
+	return nil
+}
+
+// reclaimLeavingShards reclaims all the leaving shards on the given instance
+// by pulling them back from the rest of the cluster.
+// nolint: unused
+func (ph *subclusteredHelper) reclaimLeavingShards(instance placement.Instance) {
+	// TODO: Implement subclustered reclaim leaving shards logic
+}
+
+// returnInitializingShards returns all the initializing shards on the given instance
+// by returning them back to the original owners.
+// nolint: unused
+func (ph *subclusteredHelper) returnInitializingShards(instance placement.Instance) {
+	// TODO: Implement subclustered return initializing shards logic
+}
